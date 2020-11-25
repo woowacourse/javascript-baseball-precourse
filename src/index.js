37b955f0e944eeace33d14ba973687ab6df3d5e5
Daniel <elrion018@gmail.com>
2020-11-25 19:40:45 +0900

    chore: eslint, prettier 적용 [문제시 삭제]

--- src/index.js
+++ src/index.js
@@ -3,11 +3,4 @@ export default function BaseballGame() {
     return "결과 값 String";
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
 new BaseballGame();
