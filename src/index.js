40e7f5c3d5676e56dc4dcd4c0db5ad381df0e75d
hyperflow <hyperflow@kakao.com>
2020-11-25 17:01:20 +0900

    feat: Implement method 'computingNumber'

--- src/index.js
+++ src/index.js
@@ -8,4 +8,24 @@ const getRandomNumber = function () {
   return Math.floor(Math.random() * Math.floor(9)) + 1;
 }
 
+const computingNumber = function () {
+  const numberArray = []
+  let number = 0
+  let counter = 0
+
+  while (counter < 3) {
+    const randomNumber = getRandomNumber()
+    if (numberArray.indexOf(randomNumber) !== -1) {
+      continue
+    }
+    else {
+      numberArray.push(randomNumber)
+      number += Math.pow(10, counter) * randomNumber
+      counter++
+    }
+  }
+
+  return number
+}
+
 new BaseballGame();
