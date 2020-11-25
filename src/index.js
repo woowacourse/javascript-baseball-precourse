d49ae93db5065eeb8ebf27bd761932d9f80b1245
ehddnr301 <dy950328@gmail.com>
2020-11-25 21:36:26 +0900

    1st - Add generateRandomNum function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+// * 1부터 9사이의 임의의 수 생성
+const generateRandomNum = () => {
+  let ranNum = Math.floor(Math.random() * 10);
+
+  while (ranNum === 0) {
+    ranNum = Math.floor(Math.random() * 10);
+  }
+
+  return ranNum;
+};
+
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
 new BaseballGame();
