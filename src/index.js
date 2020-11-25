34b913f17f645ea5902100ff6e6be9ee5709e60f
dawit jeong <wjdekdnlt1215@gmail.com>
2020-11-25 15:43:10 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    if (isExist(userInputNumbers, numbers)) return "결과 값 String";
   };
 }
+
+const isExist = (aNumber, numbers) => {
+  let result = false;
+  for (let i = 0; i < numbers.length; i++) {
+    if (aNumber === numbers[i]) {
+      result = true;
+    }
+  }
+  return result;
+};
 
 // export default class BaseballGame {
 //   play(computerInputNumbers, userInputNumbers) {
