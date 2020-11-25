cd2da4d6f212241974b5ad9685d6ef31a536474e
rjwu95 <yugun000@gmail.com>
2020-11-25 17:57:31 +0900

    플레이어가 입력한 숫자 유효성 검사 후 alert 띄워주고 새로 입력할 수 있게 하는 메소드 생성
     - validate 메소드 생성
     - 입력 값 유무, 길이, 1-9 까지 범위, 중복 검사

--- src/index.js
+++ src/index.js
@@ -19,8 +19,26 @@ export default class BaseballGame {
   clickEventListener() {
     const submitButton = document.getElementById("submit")
     submitButton.addEventListener('click', () => {
-      const inputValue = document.getElementById("user-input").value
+      const userInput = document.getElementById("user-input")
+      const inputValue = userInput.value
+      const validation = this.validate(inputValue)
+      if (!validation) {
+        alert("잘못된 값을 입력했습니다.")
+        userInput.value = ''
+        return
+      }
     })
+  }
+  validate(value) {
+    if (!value) return false
+    if (value.length !== 3) return false
+    if (value.match(/[^1-9]/)) return false
+    const hash = {}
+    for (let i = 0; i < value.length; i++) {
+      if (hash[value[i]]) return false
+      hash[value[i]] = 1
+    }
+    return true
   }
 }
 new BaseballGame();
