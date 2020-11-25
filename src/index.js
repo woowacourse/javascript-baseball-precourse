5b011912711d0474704b35fa2cfe3f04e41f44e1
loquemedalagana <nadamasquetu012@gmail.com>
2020-11-25 21:09:25 +0900

    test first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
+import createRandomNumber from './assets/computer';
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
+// export default function BaseballGame() {
+//   this.play = function (computerInputNumbers, userInputNumbers) {
 //     return "결과 값 String";
-//   }
+//   };
 // }
 
+export default class BaseballGame {
+  play(computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  }
+}
+
 new BaseballGame();
+console.log(createRandomNumber());
+
+//구조는 main class안에 두 개의 매개변수를 받은 후 play시킨다.
