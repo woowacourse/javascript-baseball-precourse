b963343629839c76b890b225b29f6906f41c2c8f
jehong <jehong@student.42seoul.com>
2020-11-26 01:24:26 +0900

    feat: generate answer and empty result

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ function StartGame() {
   if (result.value !== '')
     result.value = '';
   
-
+  let answer = [];
+  let candidate = [];
+  for (let i = 1; i <= 9; i++)
+    candidate.push(i);
+  for (let i = 0; i <= 2; i++) {
+    let picked = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    answer.push(picked);
+  }
+  CountBallStrike(answer, GetInput());
 }
 
 function GetInput() {
