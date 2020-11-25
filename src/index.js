6f974eb7f6ac5fb4728abdad2556af66a929a140
rjwu95 <yugun000@gmail.com>
2020-11-25 17:25:39 +0900

    1-9 중에 서로 다른 임의의 수 3개 선택하는 메소드 생성
     - createRandomNumber 라는 이름으로 메소드 생성
     - class 내에 randomNumber 변수 만들어서 저장

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
+// export default function BaseballGame() {
+//   this.play = function (computerInputNumbers, userInputNumbers) {
 //     return "결과 값 String";
-//   }
+//   };
 // }
 
+export default class BaseballGame {
+  randomNumber = ''
+  play(computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  }
+  createRandomNumber() {
+    const numbers = Array.from({ length: 9 }, (_, i) => i + 1)
+    while (this.randomNumber.length < 3) {
+      const selectedNumber = numbers.splice(Math.floor(Math.random() * numbers.length), 1)
+      this.randomNumber += selectedNumber[0].toString()
+    }
+  }
+}
 new BaseballGame();
