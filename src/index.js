24f9d169e7de8d89b1f9e8f098b548ce489d8670
Sun Jung Kim <blackmilk274@gmail.com>
2020-11-25 22:25:43 +0900

    feat: 랜덤 정답 생성 함수, 정답 유효성 검사 함수 추가

--- src/index.js
+++ src/index.js
@@ -6,6 +6,29 @@ export default class BaseballGame {
   static getUserInput() {
     return document.querySelector('#user-input').value;
   }
+
+  static isValidAnswer(answer) {
+    return (
+      answer.length === 3 &&
+      answer[0] !== answer[1] &&
+      answer[1] !== answer[2] &&
+      answer[0] !== answer[2]
+    );
+  }
+
+  static generateAnswer() {
+    let answer = Math.floor(Math.random() * 1000).toString();
+    while (!this.isValidAnswer(answer)) {
+      answer = Math.floor(Math.random() * 1000).toString();
+    }
+    return answer;
+  }
 }
 
 const game = new BaseballGame();
+
+// 메소드 테스트용 실행함수
+const testMethods = () => {
+  console.log(`generateAnswer: ${BaseballGame.generateAnswer()}`);
+};
+testMethods();
