847f0caaa07dfb4997b48495e99d739eace29f65
dawit jeong <wjdekdnlt1215@gmail.com>
2020-11-25 21:25:51 +0900

    complete checking usre input validity

--- src/index.js
+++ src/index.js
@@ -3,24 +3,43 @@ const submitButton = document.getElementById("submit");
 
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
-    if (isExist(userInputNumbers, numbers)) return "결과 값 String";
+    console.log(computerInputNumbers, userInputNumbers);
+    // if (isExist(userInputNumbers, numbers)) return "결과 값 String";
   };
+  this.done = false;
 }
 
 // Math.floor(Math.random() * 9 + 1);
 
-const isExist = (aNumber, numbers) => {
-  let result = false;
-  for (let i = 0; i < numbers.length; i++) {
-    if (aNumber === numbers[i]) {
-      result = true;
+let CurrentBaseballGame = new BaseballGame();
+
+const getAllIndexsByValue = (array, value) => {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      result.push(i);
     }
   }
   return result;
 };
 
-const done = false;
-const CurrentBaseballGame = new BaseballGame();
+const checkUserInput = (value) => {
+  if (value.length !== 3) {
+    return { isValid: false, errorMessage: "Input must have 3 length." };
+  }
+  for (let i = 0; i < value.length; i++) {
+    if (JSON.stringify(parseInt(value[i], 10)) === JSON.stringify(NaN)) {
+      return { isValid: false, errorMessage: "Input must be number only." };
+    }
+    if (getAllIndexsByValue(value, value[i]).length !== 1) {
+      return {
+        isValid: false,
+        errorMessage: "Input must have unrepeated numbers only.",
+      };
+    }
+  }
+  return { isValid: true, errorMessage: "Perfect" };
+};
 
 const isAlreadyExist = (numbers, aNumber) => {
   if (numbers === []) return false;
@@ -47,7 +66,10 @@ const getComputerInput = () => {
   return result.join("");
 };
 submitButton.onclick = () => {
+  const resultOfCheck = checkUserInput(userInput.value);
+  if (!resultOfCheck.isValid) {
+    alert(resultOfCheck.errorMessage);
+    return;
+  }
   CurrentBaseballGame.play(getComputerInput(), userInput.value);
 };
-
-new BaseballGame();
