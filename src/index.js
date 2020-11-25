12bf57b929215647dcd4ac9469bc290926f44fa3
shinsehantan <shinsehantan@users.noreply.github.com>
2020-11-26 00:43:34 +0900

    make a randomNumber

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 export default function BaseballGame() {
+  this.randomNumber = makeRandomNumber();
+
+  console.log(this.randomNumber);
   this.play = function (computerInputNumbers, userInputNumbers) {
+    // console.log(computerInputNumbers, userInputNumbers);
     return "결과 값 String";
   };
+}
+
+function makeRandomNumber() {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let randomNumber = [];
+
+  while (randomNumber.length <= 2) {
+    let a = Math.floor(Math.random() * candidates.length + 1);
+
+    if (randomNumber.indexOf(a) === -1) {
+      randomNumber.push(a);
+    }
+  }
+
+  return randomNumber;
 }
 
 // export default class BaseballGame {
@@ -10,4 +29,5 @@ export default function BaseballGame() {
 //   }
 // }
 
-new BaseballGame();
+let game = new BaseballGame();
+// game.play(123, 22);
