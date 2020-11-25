43323e037b4b0685edec60ce385fea0991da8f79
unknown <dudtjr913@naver.com>
2020-11-25 18:48:23 +0900

    Feat : 정답인 3개의 수 선택하는 함수 구현 완료 <description> makeOnAnswer 함수에 1 ~ 9까지의 배열을 할당한후 랜덤하게 selectRandonNumber 함수로 숫자를 뽑고, 뽑힌 숫자를 1 ~ 9까지의 배열에서 삭제해준 뒤 뽑힌 숫자를 정답인 answer에 추가해주었다.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  const makeOnAnswer = () => {
+    const ALL_NUMBERS = 9;
+    const numberArray = Array(ALL_NUMBERS)
+      .fill()
+      .map((v, i) => i + 1);
+    let answer = "";
+
+    for (let i = 1; i <= 3; i++) {
+      const selectedNumber = numberArray.splice(
+        selectRandomNumber(numberArray.length),
+        1
+      );
+      answer += selectedNumber;
+    }
+    console.log(answer);
+    return answer;
+  };
+
+  const selectRandomNumber = (length) => {
+    const randomNumber = Math.floor(Math.random() * length);
+
+    return randomNumber;
+  };
+}
 
 new BaseballGame();
