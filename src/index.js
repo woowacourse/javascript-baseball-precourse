9dae463f17b80b0dbf8a8d26eaf8fe83309d48b4
Hyun Cheol <mlch1603@naver.com>
2020-11-26 02:26:21 +0900

    feat: getting computer's number

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
+// export default function BaseballGame() {
+//   this.play = function (computerInputNumbers, userInputNumbers) {
 //     return "결과 값 String";
-//   }
+//   };
 // }
 
-new BaseballGame();
+export default class BaseballGame {
+  constructor() {
+    this.computerNumber = [];
+  }
+
+  getComputerNumber() {
+    let index = 0;
+    
+    while(index < 3) {
+      const randomNumber = Math.floor(Math.random() * 10);
+      
+      if(randomNumber !== 0 && this.computerNumber.indexOf(randomNumber) === -1) {
+        this.computerNumber[index] = randomNumber;
+        index++;
+      }
+    }
+    console.log(this.computerNumber);
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  }
+}
+
+new BaseballGame().getComputerNumber();
