e83b8fd6506faf9da8141cb1d0c092adb3c3eeca
Teo_Hwang <taeung2008@gmail.com>
2020-11-25 23:30:54 +0900

    feat: 컴퓨터가 임의의 수 3개를 생성하는 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
+export default class BaseballGame {
+  play(computerInputNumbers, userInputNumbers) {
+    return `${computerInputNumbers} ${userInputNumbers}`;
+  }
+
+  // 컴퓨터 입력 값 가져오기 메소드
+  getComputerNumber() {
+    let computerInputNumbers = '';
+
+    while (computerInputNumbers.length < 3) {
+      const randomNumber = Math.floor(Math.random() * 9 + 1);
+
+      if (this.isDuplicate(computerInputNumbers, randomNumber)) {
+        computerInputNumbers += randomNumber.toString();
+      }
+    }
+
+    return computerInputNumbers;
+  }
+
+  // 중복 검사 함수
+  isDuplicate(text1, text2) {
+    let resultData = false;
+
+    // 중복이 없으면 -1을 출력함
+    if (text1.indexOf(text2) === -1) {
+      resultData = true;
+    }
+
+    return resultData;
+  }
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+// 테스트 케이스
+const game = new BaseballGame();
+// 컴퓨터 입력 값 테스트
+const testCase1 = game.getComputerNumber();
+// 유저 입력 값 테스트
+const testCase2 = 456;
 
-new BaseballGame();
+// 콘솔 창에 테스트
+console.log(game.play(testCase1, testCase2));
