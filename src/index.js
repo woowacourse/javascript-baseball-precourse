fb0a929aaa024e9649be7b46dfec2e1ff33b1db2
dawit jeong <wjdekdnlt1215@gmail.com>
2020-11-25 21:01:32 +0900

    complete making computerInput

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const userInput = document.getElementById("user-input");
+const submitButton = document.getElementById("submit");
+
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     if (isExist(userInputNumbers, numbers)) return "결과 값 String";
@@ -16,10 +19,35 @@ const isExist = (aNumber, numbers) => {
   return result;
 };
 
-const userInput = document.getElementById("user-input");
-const submitButton = document.getElementById("submit");
+const done = false;
+const CurrentBaseballGame = new BaseballGame();
+
+const isAlreadyExist = (numbers, aNumber) => {
+  if (numbers === []) return false;
+  let result = false;
+
+  numbers.forEach((val) => {
+    if (val === aNumber) result = true;
+  });
+  return result;
+};
+const generateRandomNumber = (existingNumber) => {
+  let result = Math.floor(Math.random() * 9 + 1);
+  while (isAlreadyExist(existingNumber, result)) {
+    result = Math.floor(Math.random() * 9 + 1);
+  }
+  return result;
+};
+const getComputerInput = () => {
+  const firstNumber = generateRandomNumber([]);
+  const secondNumber = generateRandomNumber([firstNumber]);
+  const thirdNumber = generateRandomNumber([firstNumber, secondNumber]);
+  const result = [firstNumber, secondNumber, thirdNumber];
+
+  return result.join("");
+};
 submitButton.onclick = () => {
-  console.log(userInput.value);
+  CurrentBaseballGame.play(getComputerInput(), userInput.value);
 };
 
 new BaseballGame();
