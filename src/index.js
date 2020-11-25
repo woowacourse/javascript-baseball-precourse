0067da84ac0c9dd6cc7b664b9d117cf9408acd23
Hyun Cheol <mlch1603@naver.com>
2020-11-26 03:31:39 +0900

    feat: compare numbers & return result

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@
 export default class BaseballGame {
   constructor() {
     this.computerNumber = [];
+    this.balls = 0;
+    this.strikes = 0;
+  }
+
+  resetBallsAndStrikes() {
+    this.balls = 0;
+    this.strikes = 0;
   }
 
   getComputerNumber() {
@@ -23,9 +30,31 @@ export default class BaseballGame {
     console.log(this.computerNumber);
   }
 
+  getReturnString() {
+    if(this.balls && this.strikes) {
+      return `${this.balls}볼 ${this.strikes}스트라이크`;
+    } else if(!this.balls && this.strikes) {
+      return `${this.strikes}스트라이크`;
+    } else if(this.balls && !this.strikes) {
+      return `${this.balls}볼`;
+    } else {
+      return "낫싱";
+    }
+  }
+
   play(computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    computerInputNumbers.forEach((number, idx) => {
+      const indexOfNumber = userInputNumbers.indexOf(number);
+      if(indexOfNumber === -1) {
+      } else if(indexOfNumber === idx) {
+        this.strikes++;
+      } else {
+        this.balls++;
+      }
+    });
+
+    const result = this.getReturnString();
+    this.resetBallsAndStrikes();
+    return result;
   }
 }
-
-new BaseballGame().getComputerNumber();
