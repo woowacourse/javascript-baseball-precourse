705445e79d852eb0ce595b895c1313d866347132
SEONBIN <tjsqls2003@gmail.com>
2020-11-26 19:05:40 +0900

    create setTargetNumber

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  this.setTargetNumber = function () {
+    const TargetNumber = [];
+
+    while (TargetNumber.length < 3) {
+      let randomNumber = Math.floor(Math.random() * 9) + 1;
+
+      if (TargetNumber.indexOf(randomNumber) == -1) {
+        TargetNumber.push(randomNumber);
+      }
+    }
+
+    return TargetNumber;
+  };
+}
 
 new BaseballGame();
