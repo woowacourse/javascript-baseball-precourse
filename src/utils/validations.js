ff6945f093ace3d01bca021a0c7ca56b8743d9c3
2SOOY <1401660@naver.com>
2020-11-26 17:22:07 +0900

    Feat - input 입력길이 3으로 제한, 입력된 데이터 검증 함수 구현 - input태그의 입력 길이를 3으로 제한하여 애초에 3자리 입력만 받도록 설정 - 검증 1 : 숫자로만 입력했는지 검증 - 검증 2 : 중복된 숫자가 없는지 검증

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,1 +1,12 @@
-export const isValidInputData = (data) => {};
+export const isValidInputData = (data) => {
+  if (isNaN(data)) {
+    return false;
+  }
+
+  const numbers = new Set(data.split(''));
+  if (numbers.size !== 3) {
+    return false;
+  }
+
+  return true;
+};
