7c100949e20fbef6927d7662d377bdc8aabf1a17
365kim <365listener@gmail.com>
2020-11-26 14:10:45 +0900

    feat: 랜덤숫자 만들어주는 유틸함수 추가

---
+++ src/utils.js
@@ -0,0 +1,12 @@
+export default function Utils() {
+  this.generateRandomNumbers = (N) => {
+    let numbers = [];
+    while (numbers.length < N) {
+      let r = Math.floor(Math.random() * 9) + 1;
+      if (numbers.indexOf(r) == -1) {
+        numbers.push(r);
+      }
+    }
+    return numbers.join('');
+  };
+}
