248cc2f6aebe464410b16af1e847092d0b267cd9
sunhpark42 <sunhpark42@gmail.com>
2020-11-26 13:53:49 +0900

    Feat : 게임시작 시 컴퓨터 답울  설정하는  기능 구현
    1 - 9 사이의 임의의 숫자를 중복없이 뽑아, 정답 배열에 할당함.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export default function BaseballGame() {
+  let answer = setAnswer();
+
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
@@ -9,5 +11,24 @@ export default function BaseballGame() {
 //     return "결과 값 String";
 //   }
 // }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * Math.floor(max - min + 1) + min);
+}
+
+function setAnswer() {
+  let answer = [];
+
+  while (answer.length < 3) {
+    let num = getRandomInt(1, 9);
+    
+    if (answer.indexOf(num) > -1) {
+      continue;
+    } else {
+      answer.push(num);
+    }
+  }
+  return answer;
+}
 
 new BaseballGame();
