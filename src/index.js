00b21f6fae6b4baacb1b0827f1df64047ec4ea59
Byeonguk Yoo <bwyoo1229@gmail.com>
2020-11-26 16:43:29 +0900

    feat: 중복없는 컴퓨터 난수 배열 생성 함수 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 export default class BaseballGame {
+  constructor() {
+    this.computerInputNumbers = this.getComputerInputNumbers();
+  }
+
   play(computerInputNumbers, userInputNumbers) {
     return printGameResult(computerInputNumbers, userInputNumbers);
   }
+
+  getRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 9 + 1);
+
+    return randomNumber;
+  }
+
+  getComputerInputNumbers() {
+    const PITCH_COUNT = 3;
+    let computerInputNumbers = [];
+    let pitch = 0;
+    let randomNumber;
+
+    while (pitch < PITCH_COUNT) {
+      randomNumber = this.getRandomNumber();
+      if (!computerInputNumbers.includes(randomNumber)) {
+        computerInputNumbers.push(randomNumber);
+        pitch++;
+      }
+    }
+
+    return computerInputNumbers;
+  }
 }
 
-const getRandomNumber = () => {
-  const randomNumber = Math.floor(Math.random() * 9 + 1);
+const game = new BaseballGame();
+console.log(game);
 
-  return randomNumber;
-};
-
-new BaseballGame();
+// Event Handler
