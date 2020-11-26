7290bbb00b27f2c6ecac3413e9bf98c6b3c86ec3
임광열 <mkitigy@gmail.com>
2020-11-26 20:27:51 +0900

    Modify logic in play function

--- src/baseballgame.js
+++ src/baseballgame.js
@@ -1,11 +1,11 @@
-import Util, { DIGITS } from "./util.js";
+import { DIGITS } from "./util.js";
 
 export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
     let strike = 0;
     let ball = 0;
 
-    for (let i = 0; i < computerInputNumbers.length; i++) {
+    for (let i = 0; i < DIGITS; i++) {
       let compare = computerInputNumbers[i];
 
       if (compare === userInputNumbers[i]) {
@@ -19,21 +19,44 @@ export default class BaseballGame {
 
   renderResult(ball, strike) {
     const result = document.querySelector("#result");
-    const answer = document.createElement("strong");
-    const restart = document.createElement("span");
-    const restartButton = document.createElement("button");
 
     result.innerText = "";
-    if (ball === 0 && strike === 0) {
+    if (strike === 0 && ball === 0) {
       result.innerText = "낫싱";
-    } else if (strike === 0) {
-      result.innerText = `${ball}볼`;
+      return;
+    }
+    this.renderBallCount(ball, result);
+    this.renderStrikeCount(strike, result);
+  }
+
+  renderBallCount(ball, result) {
+    const ballCount = document.createElement("span");
+
+    if (ball === 0) {
+      return;
+    }
+    ballCount.innerText = `${ball}볼`;
+    result.appendChild(ballCount);
+  }
+
+  renderStrikeCount(strike, result) {
+    const strikeCount = document.createElement("span");
+    const answer = document.createElement("strong");
+    const restartMessage = document.createElement("span");
+    const restartButton = document.createElement("button");
+
+    if (strike === 0) {
+      return;
+    }
+    if (strike < 3) {
+      strikeCount.innerText = `${strike}스트라이크`;
+      result.appendChild(strikeCount);
     } else {
       answer.innerText = "💯 정답을 맞추셨습니다! 💯";
       answer.id = "answer";
       result.appendChild(answer);
-      restart.innerText = "게임을 새로 시작하시겠습니까?";
-      result.appendChild(restart);
+      restartMessage.innerText = "게임을 새로 시작하시겠습니까?";
+      result.appendChild(restartMessage);
       restartButton.innerText = "게임 재시작";
       restartButton.addEventListener("click", () => location.reload());
       result.appendChild(restartButton);
