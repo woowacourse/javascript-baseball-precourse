66e3674336f41233abe824bc3e57e4f9d19a4dc2
dande <7bdande@gmail.com>
2020-11-26 21:03:33 +0900

    feat: setComputerInputNumbers 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export default function BaseballGame() {
+  this.MAX_DIGITS = 3;
+
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
@@ -8,6 +10,24 @@ export default function BaseballGame() {
     const MAX_NUMBER = 9;
 
     return Math.floor(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+  };
+
+  this.setComputerInputNumbers = () => {
+    const computerInputNumbers = [];
+
+    while (computerInputNumbers.length < this.MAX_DIGITS) {
+      const randomNumber = this.getRandomNumber();
+      const isExistNumber = computerInputNumbers.includes(randomNumber)
+        ? true
+        : false;
+
+      if (isExistNumber) {
+        continue;
+      }
+      computerInputNumbers.push(randomNumber);
+    }
+
+    return computerInputNumbers;
   };
 }
 
