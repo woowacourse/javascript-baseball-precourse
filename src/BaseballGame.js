c0cd8464c1b42aa9b3e0f1a2e634a743885ed487
JongHyeon Moon <dobest27@gmail.com>
2020-11-27 04:51:33 +0900

    feat: implement BaseballGame play method

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -11,4 +11,38 @@ export default class BaseballGame {
 			this.#answer.push(n);
 		}
 	}
+
+	play(challenge) {
+		const count = {
+			ball: 0,
+			strike: 0,
+		};
+		let result = "";
+
+		for (let i in challenge) {
+			if (this.#answer[i] === challenge[i]) {
+				count.strike++;
+				continue;
+			}
+			if (this.#answer.includes(challenge[i])) {
+				count.ball++;
+			}
+		}
+
+		if (count.strike === 3) {
+			return "정답";
+		}
+		if (count.ball > 0) {
+			result += `${count.ball}볼 `;
+		}
+		if (count.strike > 0) {
+			result += `${count.strike}스트라이크`;
+			// todo : 뭔가 오류가 있다
+		}
+		if (count.ball === 0 && count.strike === 0) {
+			result = "낫싱";
+		}
+
+		return result;
+	}
 }
