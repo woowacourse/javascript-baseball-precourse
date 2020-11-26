a729be21d18fd80191897a849082f18556cff51e
maywngml <maywngml@naver.com>
2020-11-27 00:18:45 +0900

    기능추가: 랜덤으로 3개의 숫자 생성 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
+export default class BaseballGame {
+  constructor() {
+    this.computerInputNumbers = this.getComputerInputNumbers();
+  }
+  //3개의 랜덤 숫자를 생성하는 함수
+  getComputerInputNumbers() {
+    let result = "";
+    while(result.length < 3) {
+      let tempNum = Math.floor(Math.random() * (10 - 1)) + 1;
+      //중복되는 숫자가 아닐시에만 결과값에 추가함
+      if(!result.includes(tempNum)) {
+        result += tempNum;
+      }
+    }
+    return result;
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
-  };
+  }
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-new BaseballGame();
+ new BaseballGame();
