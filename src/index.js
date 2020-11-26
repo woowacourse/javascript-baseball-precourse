d10565da910f36d58d4b01fc070d9c26a48fb8ba
dande <7bdande@gmail.com>
2020-11-26 16:32:29 +0900

    feat: getRandomNumber 추가

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  this.getRandomNumber = () => {
+    const MIN_NUMBER = 1;
+    const MAX_NUMBER = 9;
+
+    return Math.floor(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+  };
+}
 
 new BaseballGame();
