674b43bf01bc1652936df830ceaa4e4fdc8ca307
seojihwan <seojihwan213@naver.com>
2020-11-26 11:40:45 +0900

    feat: 서로 다른 임의의 3자리 수 선택 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    return '결과 값 String';
+  };
+
+  this.makeRandomNumbers = function () {
+    while (true) {
+      const first = Math.floor(Math.random() * 9 + 1);
+      const second = Math.floor(Math.random() * 9 + 1);
+      const third = Math.floor(Math.random() * 9 + 1);
+      const numbers = first * 100 + second * 10 + third;
+      if (this.isComposedOfDifferentNumber(numbers)) {
+        console.log(numbers);
+        return numbers;
+      }
+    }
+  };
+
+  this.isComposedOfDifferentNumber = function (numbers) {
+    const first = String(numbers)[0];
+    const second = String(numbers)[1];
+    const third = String(numbers)[2];
+    if (first !== second && second !== third && third !== first) {
+      return true;
+    }
+    return false;
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
 new BaseballGame();
