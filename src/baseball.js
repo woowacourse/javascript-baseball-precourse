517cf9a9634e68075aec38edad3e4db66e26cd6c
unknown <dudtjr913@naver.com>
2020-11-26 22:26:33 +0900

    Bug : 게임이 종료되었을 때 submit을 할 시 에러창이 뜨질 않음 <description> alert를 사용해서 에러를 띄워주었다.

--- src/baseball.js
+++ src/baseball.js
@@ -8,7 +8,7 @@ const game = new BaseballGame();
 const submitUserInput = () => {
   const { isInputRight, play, showResultOnScreen, runningGame, answer } = game;
 
-  if (!runningGame) return;
+  if (!runningGame) return alert("게임을 재시작해주세요.");
   if (isInputRight()) {
     const resultText = play(answer, userInput.value);
 
