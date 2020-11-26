3b62af066fc55d362deeccbbd93544385f1ec338
yujo <yujo@student.42seoul.kr>
2020-11-26 14:59:39 +0900

    refactor : 기존 작성한 함수들 화살표 함수에서 일반 함수로 변경
    프로그래밍 요구사항에 명시 된 exprot default 를 사용하기 위해 변경.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 // export default function BaseballGame() {
-//   this.this.play = function (computerInputNumbers, userInputNumbers) {
+//   this.play = function (computerInputNumbers, userInputNumbers) {
 //     return '결과 값 String';
 //   };
 // }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+import makeAnswer from './makeAnswer.js';
 
-// new BaseballGame();
-
-import { makeAnswer } from './makeAnswer.js';
-
-const BaseballGame = () => {
+export default function BaseballGame() {
   const answer = makeAnswer();
   console.log(answer);
-};
+}
 
 BaseballGame();
--- src/makeAnswer.js
+++ src/makeAnswer.js
@@ -1,14 +1,14 @@
-const getRandomNumber = () => {
+function getRandomNumber() {
   const min = 1;
   const max = 10;
 
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
-export const makeAnswer = () => {
+export default function makeAnswer() {
   const answer = new Set();
   while ([...answer].length < 3) {
     answer.add(getRandomNumber());
   }
   return [...answer];
-};
+}
