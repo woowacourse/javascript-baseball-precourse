2a708fb6163ffb46560cd94b8fdff4699835a333
2SOOY <1401660@naver.com>
2020-11-26 16:49:55 +0900

    Feat - 사용자가 입력한 값에 대해  엔터 or 버튼 클릭 처리 : App - 최상위 컴포넌트에 클릭 및 키보드 이벤트 등록 - Enter시 입력창 내의 내용 가져오기 - 확인 버튼 클릭 시 입력창의 내용 가져오기

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,35 @@
-import TryButton from './TryButton.js';
 import { generateTargetNumbers } from '../utils/generateTargetNumbers.js';
 
 class BaseballGame {
   constructor($target) {
     this.$target = $target;
-    this.tryButton = new TryButton(document.querySelector('#submit'));
+    this.userInput = document.querySelector('#user-input');
+    this.tryButton = document.querySelector('#submit');
 
     this.answer = generateTargetNumbers();
+
+    this.bindEvents();
   }
 
   play(computerInputNumbers, userInputNumbers) {
     return '결과 값 String';
+  }
+
+  bindEvents() {
+    this.$target.addEventListener('click', this.onClick.bind(this));
+    this.$target.addEventListener('keydown', this.onKeyDown);
+  }
+
+  onClick({ target }) {
+    if (target.id !== 'submit') return;
+
+    console.log(this.userInput.value);
+  }
+
+  onKeyDown({ target, key }) {
+    if (key !== 'Enter') return;
+
+    console.log(target.value);
   }
 }
 
