44227f0827d5e9ccb3fdcc5757cb85a82da16653
Huijiny <jiin20803@gmail.com>
2020-11-27 11:10:44 +0900

    feat: get random number function for computer random inputs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
+    
     return "결과 값 String";
   };
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+function getRandomNumber() {
+  return Math.floor(Math.random() * 9) + 1;
+}
 
-new BaseballGame();
+new BaseballGame()
