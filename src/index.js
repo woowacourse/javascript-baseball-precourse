dd74cb4aafe3f88bcd9b2d1f180d015ab78fde24
ChayanRin <k852012@naver.com>
2020-11-27 16:54:36 +0900

    feat: init index.js
    index.js 파일 초기화

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+export default class BaseballGame {}
 
 new BaseballGame();
