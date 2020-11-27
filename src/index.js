1b46b6c1baea115b30886cb3ad990c6209ecdd79
lulu-kang <dalkang.hy@gmail.com>
2020-11-27 13:25:29 +0900

    feat: 컴퓨터의 랜덤값 생성 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 export default function BaseballGame() {
+  function createComputerInputNumbers() {
+    const length = 3;
+    let computerInputNumbers = [];
+    let i = 0;
+
+    while (i < length) {
+      const randomNumber = Math.floor(Math.random() * 9 + 1);
+      if (notSameNumber(randomNumber)) {
+        computerInputNumbers.push(randomNumber);
+        i++;
+      }
+    }
+
+    function notSameNumber(randomNumber) { 
+      return computerInputNumbers.every((computerInputNumber) => randomNumber !== computerInputNumber);
+    };
+
+    return computerInputNumbers;
+  };
+
+  createComputerInputNumbers();
+
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
 new BaseballGame();
