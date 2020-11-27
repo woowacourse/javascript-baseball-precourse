ab1386c2cf9dd5d1cbdcc1806c93e4bd0977a269
minkyung <mnk918p@gmail.com>
2020-11-27 17:14:34 +0900

    feat: 유저의 입력을 받고 스트라이크가 몇 개인지 세는 countStrike 기능 추가 Readme.md의 구현할 기능 목록 - 5번 기능 구현

--- src/game-count.js
+++ src/game-count.js
@@ -2,6 +2,18 @@ import randomNumberMaker from "./random-number-maker.js";
 
 const randomNumberArray = randomNumberMaker(3)
 
+export function countStrike(validUserInputValueArray) {
+  let strikeCount = 0;
+
+  for (let i = 0; i < randomNumberArray.length; i++) {
+    if (validUserInputValueArray[i] === randomNumberArray[i]) {
+      strikeCount += 1;
+    }
+  }
+
+  return strikeCount;
+}
+
 export function countBall(validUserInputValueArray) {
   let ballCount = 0;
 
