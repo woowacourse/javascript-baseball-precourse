9b2d540533706583577cc20c27f85ea61c9f8429
shinhyogeun <tls1gy2rms3@naver.com>
2020-11-28 01:58:37 +0900

    feat : 만들어진 숫자가 0이 아닌지 확인하기.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 export default function BaseballGame() {
 
-// * 랜덤으로 서로 다르고 0이 아닌 3자리 숫자 만들기
+// * 랜덤으로 0과 9사이의 숫자를 만든다.
     this.makeRandomInt = function () {
         const randomNumber = parseInt(Math.random()*10);
         return randomNumber;
     };
+// * 0이 아닌지 확인하기.
+    this.isNotZero = function (number) {
+        if (number != 0) {
+            return true;
+        };
+        return false;
+    };
 };
+
 let baseballGame = new BaseballGame();
