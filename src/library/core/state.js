99ab3a8bb412f0c4e939b378b35e8fc7e51de542
jho2301 <jho2301@gmail.com>
2020-11-27 14:12:34 +0900

    feat: State 클래스 구현
    컴포넌트의 상태를 나타냄, 상태 변화시 반응성 구현

---
+++ src/library/core/state.js
@@ -0,0 +1,30 @@
+class State {
+  _value;
+  #renders;
+
+  constructor(initialValue) {
+    this._value = initialValue;
+    this.#renders = new Set();
+  }
+
+  subscribe = (render) => {
+    if (typeof render === 'function') {
+      this.#renders.add(render);
+    }
+  };
+
+  renderAll = () => {
+    this.#renders.forEach((render) => render());
+  };
+
+  get value() {
+    return this._value;
+  }
+
+  set value(newValue) {
+    this._value = newValue;
+    this.renderAll();
+  }
+}
+
+export default State;
