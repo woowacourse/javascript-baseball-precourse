aa37c25b88508f9263e5fb737912d35bf4db4d70
jum0 <wnsah052@naver.com>
2020-11-27 16:13:17 +0900

    feat : 확인 버튼을 통해 사용자로부터 입력을 받는 기능 구현

--- src/index.js
+++ src/index.js
@@ -3,20 +3,26 @@ import { NUMBER_LIST, START_INDEX_ZERO, NUMBER_DIGIT } from "./constants.js";
 export default function BaseballGame() {
   
   const init = function() {
-    const computerInputNumbers = getRandomNumbersList();
+    const computerInputNumbers = getRandomNumberList();
+    const submitButton = document.querySelector("#submit");
 
-    // console.log(computerInputNumbers);
+    submitButton.addEventListener("click", getUserInput);
   };
 
-  const getRandomNumbersList = function() {
+  const getRandomNumberList = function() {
     let tmpNumbers = NUMBER_LIST;
     
     tmpNumbers.sort(function() {
       return Math.random() - Math.random();
     });
-
     return tmpNumbers.slice(START_INDEX_ZERO, NUMBER_DIGIT);
   };
+
+  const getUserInput = function() {
+    const userInput = document.querySelector("#user-input").value;
+
+    console.log(userInput);
+  }
 
   // const play = function (computerInputNumbers, userInputNumbers) {
   //   return "결과 값 String";
