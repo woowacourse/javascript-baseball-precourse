141d1f42e279d200c4d0942b3520d70264a2db42
minkyung <mnk918p@gmail.com>
2020-11-27 18:31:44 +0900

    feat: 게임의 결과를 출력하는 gameResult 기능 추가 Readme.md의 구현할 기능 목록 - 6번 기능 구현

--- src/game-count.js
+++ src/game-count.js
@@ -1,3 +1,4 @@
+import userInput, { isValidUserInput } from "./user-input.js";
 import randomNumberMaker from "./random-number-maker.js";
 
 const randomNumberArray = randomNumberMaker(3)
@@ -24,4 +25,29 @@ export function countBall(validUserInputValueArray) {
   }
 
   return ballCount;
+}
+
+const LENGTH_OF_NUMBERS_GIVEN = 3;
+
+export default function gameResult() {
+  const validUserInputValueArray = isValidUserInput(userInput(), LENGTH_OF_NUMBERS_GIVEN);
+  const BALL_COUNT = countBall(validUserInputValueArray);
+  const STRIKE_COUNT = countStrike(validUserInputValueArray);
+  let RETURN_MESSAGE = '';
+
+  if (BALL_COUNT !== 0 && STRIKE_COUNT !== 0) {
+    RETURN_MESSAGE = RETURN_MESSAGE.concat(`${BALL_COUNT}볼 ${STRIKE_COUNT}스트라이크`);
+
+  } else if (BALL_COUNT !== 0) {
+    RETURN_MESSAGE = RETURN_MESSAGE.concat(`${BALL_COUNT}볼`);
+
+  } else if (STRIKE_COUNT !== 0) {
+    RETURN_MESSAGE = RETURN_MESSAGE.concat(`${STRIKE_COUNT}스트라이크`);
+
+  } else {
+    RETURN_MESSAGE = RETURN_MESSAGE.concat("낫싱");
+
+  }
+
+  return RETURN_MESSAGE;
 }
