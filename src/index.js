be4cd3496726734084a8a43c51c72de4f838d794
HyuuunjuKim <gus7wn@gmail.com>
2020-11-27 17:48:56 +0900

    올바른 input 확인하는 함수 구현

--- src/index.js
+++ src/index.js
@@ -50,6 +50,18 @@ function isInclude(num, arr) {
   }
 }
 
+function isCorrectInput(userInput) {
+  const userInputSet = new Set(Array.from(userInput));
+  if (
+    userInput.length === 3 &&
+    userInputSet.size === 3 &&
+    !userInput.includes("0")
+  ) {
+    return true;
+  }
+  return false;
+}
+
 const baseballGame = new BaseballGame();
 
 let computerInputNumbers = makeNewNumber();
@@ -60,23 +72,29 @@ const btnInput = document.getElementById("submit");
 
 btnInput.onclick = function () {
   userInputNumbers = document.getElementById("user-input").value;
-  const result = baseballGame.play(computerInputNumbers, userInputNumbers);
-  // 결과 출력
-  if (result === "3스트라이크") {
-    // 숫자를 전부 맞힌경우
-    document.getElementById(
-      "result"
-    ).innerHTML = `<div>정답을 맞추셨습니다!</div><div>게임을 새로 시작하시겠습니까? <button id="game-restart-button">게임 재시작</button></div>`;
+  if (isCorrectInput(userInputNumbers)) {
+    const result = baseballGame.play(computerInputNumbers, userInputNumbers);
+    // 결과 출력
+    if (result === "3스트라이크") {
+      // 숫자를 전부 맞힌경우
+      document.getElementById(
+        "result"
+      ).innerHTML = `<div>정답을 맞추셨습니다!</div><div>게임을 새로 시작하시겠습니까? <button id="game-restart-button">게임 재시작</button></div>`;
 
-    const btnRestart = document.getElementById("game-restart-button");
-    // 게임 재시작
-    btnRestart.onclick = function () {
-      computerInputNumbers = makeNewNumber(); // computerInputNumber 바꾸기
-      document.getElementById("user-input").value = ""; // input 초기화
-      document.getElementById("result").innerHTML = ""; // result 초기화
-    };
+      const btnRestart = document.getElementById("game-restart-button");
+      // 게임 재시작
+      btnRestart.onclick = function () {
+        computerInputNumbers = makeNewNumber(); // computerInputNumber 바꾸기
+        document.getElementById("user-input").value = ""; // input 초기화
+        document.getElementById("result").innerHTML = ""; // result 초기화
+      };
+    } else {
+      // 숫자를 못 맞힌경우
+      document.getElementById("result").innerHTML = result;
+    }
   } else {
-    // 숫자를 못 맞힌경우
-    document.getElementById("result").innerHTML = result;
+    alert("올바른 숫자를 입력하세요.");
+    document.getElementById("user-input").value = ""; // input 초기화
+    document.getElementById("result").innerHTML = ""; // result 초기화
   }
 };
