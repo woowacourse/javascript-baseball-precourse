ab85afe8b1d87cd237ec42bb7761c77accd8fa36
SEONBIN <tjsqls2003@gmail.com>
2020-11-27 10:56:01 +0900

    create compareNumbers

--- src/index.js
+++ src/index.js
@@ -37,25 +37,45 @@ export default function BaseballGame() {
     } else if (userInput.length !== 3 || this.isDuplication(userInput)) {
       alert("1~9까지의 수를 중복없이 3개를 작성해주세요.");
     } else {
-      const inputNumber = userInput.split('').map(x => Number(x))
+      const inputNumber = userInput.split('').map(x => Number(x));
 
-      return inputNumber
+      return inputNumber;
     }
   };
 
+  this.compareNumbers = function (computerInputNumbers, userInputNumbers) {
+    let ball = 0;
+    let strike = 0;
+
+    for (let i = 0; i < 3; i++) {
+      const pitching = computerInputNumbers.indexOf(userInputNumbers[i]);
+
+      if (pitching == i) {
+        strike += 1;
+      } else if (pitching !== -1) {
+        ball += 1;
+      }
+    }
+
+    return [ball, strike];
+  };
+
   this.play = function (computerInputNumbers, userInputNumbers) {
-    console.log(computerInputNumbers, userInputNumbers)
+    const [ball, strike] = this.compareNumbers(computerInputNumbers, userInputNumbers);
+
+    console.log(computerInputNumbers, userInputNumbers);
+    console.log(ball, strike);
     // return "결과 값 String";
   };
 
-  const computerInputNumbers = this.setTargetNumber()
+  const computerInputNumbers = this.setTargetNumber();
   const submitBtn = document.getElementById("submit");
 
   submitBtn.addEventListener("click", () => {
     const userInputNumbers = this.getInputNumber()
 
     if (userInputNumbers) {
-      this.play(computerInputNumbers, userInputNumbers)
+      this.play(computerInputNumbers, userInputNumbers);
     }
   });
 }
