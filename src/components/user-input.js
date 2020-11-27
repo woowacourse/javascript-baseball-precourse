0f04b5741371eef39c46af515ddd12e921040eb5
jho2301 <jho2301@gmail.com>
2020-11-27 15:16:42 +0900

    feat: 사용자 입력 받기 구현

--- src/components/user-input.js
+++ src/components/user-input.js
@@ -1,5 +1,28 @@
 class UserInput {
-  constructor($target, props) {}
+  #$target;
+  #props;
+  #$userInput;
+
+  constructor($target, props) {
+    this.#$target = $target;
+    this.#props = props;
+    this.#$userInput = $target.querySelector('#user-input');
+    this.initializeEventListener();
+  }
+
+  initializeEventListener() {
+    this.#$target.addEventListener('submit', (event) => this.onSubmit(event));
+  }
+
+  onSubmit(event) {
+    const userNumber = parseInt(this.#$userInput.value, 10);
+    this.#props.userNumber.value = userNumber;
+    event.preventDefault();
+  }
+
+  clearInput = () => {
+    this.#$userInput.value = '';
+  };
 }
 
 export default UserInput;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import getRandomNumber from './library/utils/getRandomNumber.js';
+import UserInput from './components/user-input.js';
+import State from './library/core/state.js';
 import {
   MIN_DIGIT,
   MAX_DIGIT,
   DIGIT_COUNT,
 } from './library/constants/random-number.js';
-import UserInput from './components/user-input.js';
 
 export default class BaseballGame {
   #answer;
+  #userNumber;
 
   constructor() {
-    this.initializeComponents();
     this.#answer = getRandomNumber(MIN_DIGIT, MAX_DIGIT, DIGIT_COUNT);
+    this.#userNumber = new State('');
+    this.initializeComponents();
   }
 
   initializeComponents() {
-    const $userInput = document.getElementById('input-wrap');
-    new UserInput($userInput);
+    const $inputWrap = document.getElementById('input-wrap');
+    new UserInput($inputWrap, { userNumber: this.#userNumber });
   }
 }
 
