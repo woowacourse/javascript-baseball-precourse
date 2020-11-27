db83e429def71cb0d7c3ea02726e0415cab4e0d2
minkyung <mnk918p@gmail.com>
2020-11-27 16:14:35 +0900

    refactor: 유저 입력의 유효성을 검사하는 isValidUserInput 기능을 세분화(숫자, 길이 모두 검증) 유저가 입력한 내용이 숫자인지 검사하던 기존의 기능을 내용의 길이도 검사할 수 있도록 내용을 추가 - Readme.md의 구현할 기능 목록 - 3번 기능 구현

--- src/user-input.js
+++ src/user-input.js
@@ -4,8 +4,20 @@ export default function userInput() {
   return userInputValue;
 }
 
-export function isValidUserInput(userInputValue) {
+export function isValidUserInput(userInputValue, userInputLength) {
+  let validUserInputValueArray = [];
+
   if (!(String(parseInt(userInputValue)) === userInputValue)) {
     alert('숫자를 입력해 주세요!');
+
+    return validUserInputValueArray;
+  } else if (userInputValue.length !== userInputLength) {
+    alert(`길이가 ${userInputLength}인 숫자를 입력해 주세요!`)
+
+    return validUserInputValueArray;
+  } else {
+    validUserInputValueArray = userInputValue.split("").map(Number)
+
+    return validUserInputValueArray;
   }
 }
