c9cae0778818eca08bbf73fa5e6b717173b53c50
jum0 <wnsah052@naver.com>
2020-11-27 14:45:14 +0900

    feat : 컴퓨터 숫자를 생성하는 기능 구현

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const NUMBER_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const START_INDEX_ZERO = 0;
+export const NUMBER_DIGIT = 3;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+import { NUMBER_LIST, START_INDEX_ZERO, NUMBER_DIGIT } from "./constants.js";
+
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+  
+  const init = function() {
+    const computerInputNumbers = getRandomNumbersList();
+
+    // console.log(computerInputNumbers);
   };
+
+  const getRandomNumbersList = function() {
+    let tmpNumbers = NUMBER_LIST;
+    
+    tmpNumbers.sort(function() {
+      return Math.random() - Math.random();
+    });
+
+    return tmpNumbers.slice(START_INDEX_ZERO, NUMBER_DIGIT);
+  };
+
+  // const play = function (computerInputNumbers, userInputNumbers) {
+  //   return "결과 값 String";
+  // };
+
+  init();
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-new BaseballGame();
+BaseballGame();
