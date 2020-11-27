fde4b1db28ed608ef55941bdb342316726cededc
ChayanRin <k852012@naver.com>
2020-11-27 17:16:37 +0900

    feat: implement checkVaild
    주어진 숫자 배열이 조건에 맞는지 확인하는 메서드 구현

--- src/index.js
+++ src/index.js
@@ -13,6 +13,43 @@ export default class BaseballGame {
 
     return computerInputNumbers;
   }
+
+  // 주어진 숫자가 유효한지 확인 메서드
+  checkVaild(userInputNumbers) {
+    // 숫자인지 검사
+    if (isNaN(userInputNumbers.join(""))) {
+      alert("입력한 값이 숫자가 아닙니다");
+
+      return false;
+    }
+
+    // 0이 포함되어있는지 검사
+    if (userInputNumbers.includes("0")) {
+      alert("입력한 숫자에 0이 포함됩니다");
+
+      return false;
+    }
+
+    // 주어진 숫자의 길이가 3인지 검사
+    if (userInputNumbers.length !== 3) {
+      alert("입력한 값이 3자리가 아닙니다");
+
+      return false;
+    }
+
+    // 주어진 숫자내 중복되는 값이 있는지 검사
+    const dupNumbers = userInputNumbers.filter(
+      (num, index) => userInputNumbers.indexOf(num) !== index
+    );
+
+    if (dupNumbers.length > 0) {
+      alert("입력한 값에 중복된 값이 있습니다");
+
+      return false;
+    }
+
+    return true;
+  }
 }
 
 new BaseballGame();
