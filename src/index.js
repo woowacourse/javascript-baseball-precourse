6e9b85094e09fa05ca20b8ddf11b6692fd339bf5
lulu-kang <dalkang.hy@gmail.com>
2020-11-28 01:14:05 +0900

    feat: 유저의 입력값 판별 기능 구현

--- src/index.js
+++ src/index.js
@@ -21,12 +21,30 @@ export default function BaseballGame() {
     return computerInputNumbers;
   };
 
+  // 2. 유저의 입력값 판별 기능
+  const submitButton = document.querySelector("#submit");
+  const userInput = document.querySelector("#user-input")
+  
+  function getUserInputNumbers() {
+    const userInputNumbers = userInput.value.split('');
+    const isDuplicated = new Set(userInputNumbers);
+  
+    if (userInputNumbers.length === arrayLength && userInputNumbers.length === isDuplicated.size) {
+      // 3번 기능 만들면서 수정
+      console.log("pass")
+    } else {
+      alert("1~9까지의 수를 중복없이 3개 작성해 주세요.")
+    }
+  };
+
+  // 3. 컴퓨터의 랜덤값과 유저의 입력값 비교 기능
   // this.play = function (computerInputNumbers, userInputNumbers) {
   //   return "결과 값 String";
   // };
 
   function init() {
     createComputerInputNumbers();
+    submitButton.addEventListener("click", getUserInputNumbers);
   };
 
   init();
