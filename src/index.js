500d5521c010d53c5cd319f414d4e3a41ef76158
Tanney-102 <dhxodms89@gmail.com>
2020-11-28 02:51:13 +0900

    create random number

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
+  this.getRandNum = () => {
+    let ret = '';
+    
+    for(let i=0; i<3; ++i) {
+      const tmpN = Math.floor(Math.random()*9 + 1);
+      ret += tmpN.toString();
+    }
+
+    return ret;
+  };
+  
+  this.play = (computerInputNumbers, userInputNumbers) => {
     return "결과 값 String";
   };
+  
+  
+  this.comNum = this.getRandNum();
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-new BaseballGame();
+let BG = new BaseballGame();
+console.log(BG.comNum);
