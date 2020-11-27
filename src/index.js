08f3499f9590a635736d7ec3be604d671e664da1
da1907 <cd03252@naver.com>
2020-11-28 01:04:50 +0900

    feat: computer number 생성

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+"use strict";
+
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
+  const play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  const getComputerNumber = function () {
+    let computerNumber = "";
+    let candidateNumber = 0;
+
+    while (computerNumber.length < 3) {
+      candidateNumber = Math.floor(Math.random() * 9 + 1);
+      if (computerNumber.indexOf(candidateNumber) !== -1) {
+        continue;
+      } else {
+        computerNumber += candidateNumber;
+      }
+    }
+
+    return computerNumber;
+  };
+
+  console.log(getComputerNumber());
+}
 
 new BaseballGame();
