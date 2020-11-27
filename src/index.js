26bb68e6961dda31eadc2bc9c15fee0fe1d25966
HyuuunjuKim <gus7wn@gmail.com>
2020-11-27 18:23:05 +0900

    indent depth 낮추기 위해, 정답 맞춘 경우 method로 빼고 코드 순서 변경

--- src/index.js
+++ src/index.js
@@ -23,6 +23,20 @@ export default function BaseballGame() {
       return `${numOfBall}볼 ${numOfStrike}스트라이크`;
     }
   };
+
+  this.playNewGame = function () {
+    document.getElementById(
+      "result"
+    ).innerHTML = `<div>정답을 맞추셨습니다!</div><div>게임을 새로 시작하시겠습니까? <button id="game-restart-button">게임 재시작</button></div>`;
+
+    const btnRestart = document.getElementById("game-restart-button");
+    // 게임 재시작
+    btnRestart.onclick = function () {
+      computerInputNumbers = makeNewNumber(); // computerInputNumber 바꾸기
+      document.getElementById("user-input").value = ""; // input 초기화
+      document.getElementById("result").innerHTML = ""; // result 초기화
+    };
+  };
 }
 
 // 새로운 숫자 만들기
@@ -72,29 +86,19 @@ const btnInput = document.getElementById("submit");
 
 btnInput.onclick = function () {
   userInputNumbers = document.getElementById("user-input").value;
-  if (isCorrectInput(userInputNumbers)) {
-    const result = baseballGame.play(computerInputNumbers, userInputNumbers);
-    // 결과 출력
-    if (result === "3스트라이크") {
-      // 숫자를 전부 맞힌경우
-      document.getElementById(
-        "result"
-      ).innerHTML = `<div>정답을 맞추셨습니다!</div><div>게임을 새로 시작하시겠습니까? <button id="game-restart-button">게임 재시작</button></div>`;
-
-      const btnRestart = document.getElementById("game-restart-button");
-      // 게임 재시작
-      btnRestart.onclick = function () {
-        computerInputNumbers = makeNewNumber(); // computerInputNumber 바꾸기
-        document.getElementById("user-input").value = ""; // input 초기화
-        document.getElementById("result").innerHTML = ""; // result 초기화
-      };
-    } else {
-      // 숫자를 못 맞힌경우
-      document.getElementById("result").innerHTML = result;
-    }
-  } else {
+  // 올바르지 않은 input을 입력한 경우
+  if (!isCorrectInput(userInputNumbers)) {
     alert("올바른 숫자를 입력하세요.");
     document.getElementById("user-input").value = ""; // input 초기화
     document.getElementById("result").innerHTML = ""; // result 초기화
+    return;
+  }
+  // 올바른 input을 입력한 경우
+  const result = baseballGame.play(computerInputNumbers, userInputNumbers);
+  // 결과 출력
+  if (result === "3스트라이크") {
+    baseballGame.playNewGame(); // 숫자를 전부 맞힌경우
+  } else {
+    document.getElementById("result").innerHTML = result; // 숫자를 못 맞힌경우
   }
 };
