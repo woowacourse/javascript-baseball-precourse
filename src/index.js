66c24ba36c367b7706b1d0d51342da8ca0f4d801
Tanney-102 <dhxodms89@gmail.com>
2020-11-28 04:02:20 +0900

    separate getRandNum func from the BaseballGame obj

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
+let comNum = getRandNum();
+const BG = new BaseballGame();
+
+
+// functions
 export default function BaseballGame() {
-  this.getRandNum = () => {
-    let ret = '';
-    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    // 중복을 피하기 위해 nums에서 랜덤으로 하나씩 splice하여 가져온다.
-    
-    for(let i=0; i<3; ++i) {
-      const tmpN = nums.splice(Math.floor(Math.random()*(9-i)), 1)[0];
-      ret += tmpN.toString();
-    }
-
-    return ret;
-  };
-
   this.isValidInput = (userInput) => {
     // 1~9가 아닌 문자와 일치할 경우
     if(/[^1-9]/.test(userInput)) return false;
@@ -29,9 +21,17 @@ export default function BaseballGame() {
   this.play = (computerInputNumbers, userInputNumbers) => {
     return "결과 값 String";
   };
-  
-  
-  this.comNum = this.getRandNum();
 }
 
-let BG = new BaseballGame();
+function getRandNum() {
+  let ret = '';
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  // 중복을 피하기 위해 nums에서 랜덤으로 하나씩 splice하여 가져온다.
+  
+  for(let i=0; i<3; ++i) {
+    const tmpN = nums.splice(Math.floor(Math.random()*(9-i)), 1)[0];
+    ret += tmpN.toString();
+  }
+
+  return ret;
+};
