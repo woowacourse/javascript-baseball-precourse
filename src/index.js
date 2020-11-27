11458e928cd4aa2cc5308907aa56d465091b4be1
ChayanRin <k852012@naver.com>
2020-11-27 16:59:48 +0900

    feat: implement generateRandomNumber
    컴퓨터의 3자리숫자 생성위한 랜덤숫자 생성 메서드 작성

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-export default class BaseballGame {}
+export default class BaseballGame {
+  // 랜덤 3자리 수 생성 메서드
+  generateRandomNumber() {
+    let computerInputNumbers = [];
+
+    while (computerInputNumbers.length < 3) {
+      const randomNumber = Math.floor(Math.random() * 8 + 1);
+
+      if (!computerInputNumbers.includes(randomNumber)) {
+        computerInputNumbers.push(randomNumber);
+      }
+    }
+
+    return computerInputNumbers;
+  }
+}
 
 new BaseballGame();
