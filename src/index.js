ad9e7ebf964a9da96dd217617e07fbf802e6a846
bigsaigon333 <likepeppermint@gmail.com>
2020-11-28 01:15:51 +0900

    feat: '유저의 입력값을 검증하는 함수: isValid' 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
+export default class BaseballGame {
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  isValid(userInputNumbers) {
+    const NUMBER_DIGITS = "123456789";
+    let _isValid = null;
+
+    if (userInputNumbers.length !== 3) {
+      _isValid = false;
+    } else if (!this.isContainingGivenCharacters(userInputNumbers, NUMBER_DIGITS)) {
+      _isValid = false;
+    } else if (!this.hasDifferentNumbers(userInputNumbers)) {
+      _isValid = false;
+    } else {
+      _isValid = true;
+    }
+    
+    return _isValid;
+  }
+
+  isContainingGivenCharacters(str, GIVEN_CHARACTERS) {
+    const splittedStr = str.split("");
+    const isContaining = splittedStr.every(char => GIVEN_CHARACTERS.includes(char));
+
+    return isContaining;
+  }
+
+  hasDifferentNumbers(str) {
+    const splittedStr = str.split("");
+    const strSet = Array.from(new Set(splittedStr));
+
+    return strSet.length === str.length;
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
+
+    return "";
+  }
+
+}
 
 new BaseballGame();
