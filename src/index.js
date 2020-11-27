952080a2c93c66c99c799113066d68dbbc2a4d37
intae92 <kangit2013@gmail.com>
2020-11-27 23:26:04 +0900

    feat: 1부터 9까지 난수 생성 'getRandomIntInclusive' docs: 기능 목록 체크

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
+// export default function BaseballGame() {
+//   this.play = function (computerInputNumbers, userInputNumbers) {
 //     return "결과 값 String";
-//   }
+//   };
 // }
+
+export default class BaseballGame {
+  constructor() {}
+
+  getRandomIntInclusive() {
+    return Math.floor(Math.random() * 9) + 1;
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  }
+}
 
 new BaseballGame();
