ef91fa8f5b0f02a26a1721037a61c3db302eb8b4
ginger-kang <kdhoon07@gmail.com>
2020-11-28 19:08:13 +0900

    feat: create random number for computer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+const getComputerNumbers = () => {
+  let resultNumbers = [];
+
+  while (resultNumbers.length < 3) {
+    const randomNumber = Math.floor(Math.random() * 9) + 1;
+    if (!resultNumbers.includes(randomNumber)) {
+      resultNumbers.push(randomNumber);
+    }
+  }
+
+  return resultNumbers.join("");
+};
+
 export default function BaseballGame() {
   const submitButton = document.querySelector("#submit");
   const userInput = document.querySelector("#user-input");
+  const computerNumbers = getComputerNumbers();
 
   const play = (computerInputNumbers, userInputNumbers) => {
     return "결과 값 String";
   };
 
   const gameStart = () => {
-    console.log("game start!");
+    console.log(computerNumbers);
   };
 
   const init = () => {
