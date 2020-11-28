5dda7c503a1786e36375977603b9bcae5b0338ec
jehong <jehong@student.42seoul.com>
2020-11-29 05:26:09 +0900

    feat: add HandleRestartButton()

--- src/handleResult.js
+++ src/handleResult.js
@@ -1,16 +1,17 @@
+import HandleInit from './handleInit.js';
+
 export default function HandleResult() {
   let _blockInput = false;
 
-  this.IsCorrect = function () {
+  this.IsCorrect = function (ResultArea, input) {
     _blockInput = true;
 
     while (_blockInput === true) {
       BlockBeforeRestart();
     }
 
-    DisplaySuccessMessage();
-
-    HandleRestartButton();
+    if (_blockInput === false)
+      return DisplaySuccessMessage(ResultArea, input);
   }
 
   const BlockBeforeRestart = (button, input) => {
@@ -24,14 +25,30 @@ export default function HandleResult() {
     })
   }
 
-  const DisplaySuccessMessage = () => {
+  const DisplaySuccessMessage = (resultArea, input) => {
     let restartMessage = document.createElement('span');
     let restartButton = document.createElement('button');
 
     restartMessage.textContent = '게임을 새로 시작하시겠습니까?';
     restartButton.textContent = '게임 재시작';
-    _resultArea.innerHTML = `<h4>🎉<strong> 정답을 맞추셨습니다! </strong>🎉</h4>`;
-    _resultArea.appendChild(restartMessage);
-    _resultArea.appendChild(restartButton);
+    resultArea.innerHTML = `<h4>🎉<strong> 정답을 맞추셨습니다! </strong>🎉</h4>`;
+
+    resultArea.appendChild(restartMessage);
+    resultArea.appendChild(restartButton);
+
+    let answer = HandleRestartButton(resultArea, input, restartButton);
+
+    return answer;
+  }
+
+  const HandleRestartButton = (resultArea, input, restartButton) => {
+    const InitUtils = new HandleInit();
+    let answer = '';
+
+    restartButton.addEventListener('click', () => {
+      answer = InitUtils.InitGame(resultArea, input);
+    })
+
+    return answer;
   }
 }
