a2fc4a3a8a59a3ec1390f0ada3fc0633eb4d14e7
Shim MunSeong <Puterism.K@gmail.com>
2020-11-28 20:14:51 +0900

    feat: 사용자로부터 겹치지 않는 3자리 숫자의 형태로 입력받는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
+const userInputEl = document.querySelector('#user-input');
+const submitEl = document.querySelector('#submit');
+
 export default class BaseballGame {
   constructor() {
     this.computerNumbers = this.generateComputerNumbers();
+
+    this.setEventListener();
+  }
+
+  setEventListener() {
+    submitEl.addEventListener('click', this.getUserInput);
+  }
+
+  getUserInput() {
+    const userInput = userInputEl.value;
+    const userInputNumbers = userInput.split('');
+
+    if (hasDuplicates(userInputNumbers)) {
+      alert('1~9까지의 숫자를 중복 없이 입력해주세요');
+      userInputEl.focus();
+    }
+
+    function hasDuplicates(array = []) {
+      const visited = [];
+
+      return array.some((item) => {
+        let result = false;
+
+        if (visited.includes(item)) {
+          result = true;
+        } else {
+          visited.push(item);
+        }
+
+        return result;
+      });
+    }
   }
 
   generateComputerNumbers() {
@@ -17,12 +52,12 @@ export default class BaseballGame {
     function isUniqueNumber(numbers, randomNumber) {
       return numbers.every((number) => number !== randomNumber);
     }
-    
+
     return numbers;
   }
 
   play(computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    return '결과 값 String';
   }
 }
 
