eb041a484749b6f2961504ebb1d86e65011461f9
Kwakcena <rhkrgudwh@gmail.com>
2020-11-28 20:46:37 +0900

    컴퓨터가 랜덤한 숫자를 생성하도록 하라
    - 게임 시작 시 컴퓨터는 랜덤한 숫자를 생성한다.
    - 서로다른 3개의 숫자로 3자리 숫자를 만든다.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
+export default class BaseballGame {
+  constructor() {
+    this.computerInputNumbers = '';
+    this.startNewGame();
+  }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  startNewGame = () => {
+    this.computerInputNumbers = this.createComputerInputNumbers();
+    console.log(this.computerInputNumbers);
+  }
+
+  createComputerInputNumbers = () => {
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let computerNumbers = '';
+    while (computerNumbers.length !== 3) {
+      const randomIndex = Math.floor(Math.random() * numbers.length);
+      computerNumbers += numbers[randomIndex];
+      numbers.splice(randomIndex, 1);
+    }
+
+    return computerNumbers;
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
+    return '결과 값 String';
+  }
+}
 
 new BaseballGame();
