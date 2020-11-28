c76e3906a13cbe80b79f757586029f427841c7cc
da1907 <cd03252@naver.com>
2020-11-28 14:40:38 +0900

    feat: 사용자 입력 받고 유효성 검증

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 export default function BaseballGame() {
+  let computerInputNumbers = 0;
+
   const play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
@@ -17,11 +19,63 @@ export default function BaseballGame() {
         computerNumber += candidateNumber;
       }
     }
-
-    return computerNumber;
+    computerInputNumbers = parseInt(computerNumber);
+    return;
   };
 
-  console.log(getComputerNumber());
+  const startGame = function () {
+    getComputerNumber();
+  };
+
+  // 123 ~ 987 사이에 있는 수인지
+  const confirmThreeDigits = function (userInput) {
+    let set = new Set(userInput);
+    console.log(set);
+    if (set.size < 3 || !(userInput >= 123 && userInput <= 987)) {
+      alert("세 자리의 중복되지 않는 숫자를 입력해주세요!");
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // 0 포함되어 있지 않은지
+  const confirmNoZero = function (userInput) {
+    if (userInput.indexOf(0) !== -1) {
+      alert("1~9 사이에 있는 수 세 개를 입력해주세요!");
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const validateInput = function (userInput) {
+    if (!(confirmThreeDigits(userInput) && confirmNoZero(userInput))) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const getUserInput = function () {
+    let userInput = document.getElementById("user-input").value;
+    let userInputNumbers = 0;
+
+    if (validateInput(userInput)) {
+      userInputNumbers = parseInt(userInput);
+      play(computerInputNumbers, userInputNumbers);
+    }
+
+    return;
+  };
+
+  startGame();
+  const submitButton = document.getElementById("submit");
+  if (submitButton) {
+    submitButton.addEventListener("click", getUserInput);
+  }
+
+  console.log(computerInputNumbers);
 }
 
 new BaseballGame();
