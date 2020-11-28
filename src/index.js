873ccbf29bfd1a354b8ec75552cbac09256aad71
박종한 <exuberant94@naver.com>
2020-11-29 00:20:27 +0900

    기능 구현 - DOM tree에서 user-input를 id로 가지는 div 정보를 얻어옴

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    return '결과 값 String';
   };
 }
 
+const input = document.getElementById('user-input');
 // export default class BaseballGame {
 //   play(computerInputNumbers, userInputNumbers) {
 //     return "결과 값 String";
