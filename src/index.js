3ce8f8c2606d01eb4ddd5a8e432d8445c91b27c9
Huijiny <jiin20803@gmail.com>
2020-11-28 11:23:20 +0900

    feat: check duplication of input numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 export default function BaseballGame() {
+  const submit = document.getElementById("submit");
+  const userInput = document.getElementById("user-input");
+  
+  
   this.play = function (computerInputNumbers, userInputNumbers) {
-    
+    console.log("playing...");
     return "결과 값 String";
   };
 
   this.getComputerInputNumbers = function () {
     let computerNumbers = [];
-
     while (computerNumbers.length < 3) {
       let randomNumber = getRandomNumber();
 
@@ -16,7 +19,38 @@ export default function BaseballGame() {
     }
 
     return computerNumbers;
-  }
+  };
+
+  this.showError = function(userInputNumber) {
+    if(userInputNumber.length != 3) { 
+      alert("3개의 숫자를 작성해주세요!");
+      return ;
+    } else if( isDuplication(userInputNumber) ){
+      alert("중복된 숫자를 입력하셨습니다!");
+      return ;
+    } else if( userInputNumber.includes(0) ){
+      alert("1-9까지의 숫자만 입력해주세요!");
+      return ;
+    }
+  };
+
+  this.getUserInput = function() {
+    let userInputNumber = userInput.value;
+    this.showError(userInputNumber);
+    this.play(computerInputNumbers, userInputNumber);
+  };
+
+  submit.addEventListener('click',() => this.getUserInput());
+  const computerInputNumbers = this.getComputerInputNumbers();
+
+}
+
+function isDuplication(numbers) {
+  return (
+    numbers[0] === numbers[1] ||
+    numbers[0] === numbers[2] ||
+    numbers[1] === numbers[2]
+    );
 }
 
 function getRandomNumber() {
