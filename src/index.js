8b5a54b6ecb14dff8690fcecfc8fc0340ef0f32e
박종한 <exuberant94@naver.com>
2020-11-29 00:29:49 +0900

    입력값 정상 입력 후 확인 누를 시 그에 맞게 결과값 보여줌

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return '결과 값 String';
-  };
-}
+// export default function BaseballGame() {
+//   this.play = function (computerInputNumbers, userInputNumbers) {
+//     return '결과 값 String';
+//   };
+// }
 
 const input = document.getElementById('user-input');
 submit.onclick = () => {
@@ -17,12 +17,54 @@ submit.onclick = () => {
     alert('숫자가 유효하지 않습니다. 다시 입력해주세요!'); // 중복 발생시 alert
     input.value = '';
     input.focus();
+  } else {
+    // 게임 시작
+    const inputNumberArrays = inputArrays.map((i) => parseInt(i)); // 입력값을 문자열에서 숫자로
+    result.innerHTML = new BaseballGame().play(
+      // 결과값을 얻기 위해 게임 진행
+      randomNumbers,
+      inputNumberArrays,
+    );
   }
 };
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
-new BaseballGame();
+// 컴퓨터가 정해주는 랜덤 수 구하기
+let randomNumbers = getRandomNum();
+
+function getRandomNum() {
+  const arr = [];
+  while (arr.length < 3) {
+    const randomNumber = parseInt(Math.random() * 9) + 1;
+    const isDuplicated = arr.includes(randomNumber);
+    if (!isDuplicated) arr.push(randomNumber);
+  }
+
+  return arr;
+}
+
+export default class BaseballGame {
+  constructor() {
+    this.strike = 0;
+    this.ball = 0;
+  }
+  play(computerInputNumbers, userInputNumbers) {
+    for (let i = 0; i < 3; i++)
+      this.getStrikesAndBalls(computerInputNumbers, userInputNumbers, i);
+
+    return this.getResult();
+  }
+  getStrikesAndBalls(computerInputNumbers, userInputNumbers, idx) {
+    for (let i = 0, len = computerInputNumbers.length; i < len; i++)
+      if (computerInputNumbers[i] === userInputNumbers[idx])
+        i === idx ? this.strike++ : this.ball++;
+  }
+  getResult() {
+    if (this.strike === 3) return '🎉<strong>정답을 맞추셨습니다!</strong>🎉';
+    else if (this.strike === 0 && this.ball === 0) return '낫씽';
+    else if (this.strike === 0) return `${this.ball}볼`;
+    else if (this.ball === 0) return `${this.strike}스트라이크`;
+    else return `${this.ball}볼 ${this.strike}스트라이크`;
+  }
+}
+
+// new BaseballGame(); 버튼 클릭시 게임이 진행되어야 하므로 자동으로 게임 진행되는 부분은 주석처리
