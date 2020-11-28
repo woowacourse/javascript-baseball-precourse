ca0078b953051da62c25018b5c09583093777145
yang <yaaaaang57@naver.com>
2020-11-28 18:27:40 +0900

    check input value

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 export default function BaseballGame() {
   const inputText = document.querySelector('#user-input');
   const submitButton = document.querySelector('#submit');
-  submitButton.addEventListener('click', () => console.log(inputText.value));
+  submitButton.addEventListener('click', () =>
+    this.checkInput(inputText.value)
+  );
 
+  // 3개의 다른 숫자 세개 생성
   this.getRandomNumber = () => {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let pickedNumbers = [];
@@ -12,6 +15,30 @@ export default function BaseballGame() {
       pickedNumbers.push(picked);
     }
     return pickedNumbers;
+  };
+
+  // 중복 값이 있는지 확인
+  this.checkDuplicate = value => {
+    const words = value.split('');
+    const wordsSet = new Set(words);
+    if (words.length == wordsSet.size) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // 규칙에 맞는 값인지 확인
+  this.checkInput = value => {
+    const isNotThree = value.length !== 3;
+    const isNotDigit = parseInt(value) != value;
+    const hasZero = value.includes('0');
+    const isDuplicate = this.checkDuplicate(value);
+    if (isNotThree || isNotDigit || hasZero || isDuplicate) {
+      alert('입력 값이 규칙에 맞지 않습니다. 다시 입력해주세요.');
+      inputText.value = '';
+      inputText.focus();
+    }
   };
 
   this.play = function (computerInputNumbers, userInputNumbers) {
