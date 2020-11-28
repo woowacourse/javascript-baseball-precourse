4e2fe9ee614c6a531dca0e2c90da107a053af1e1
joyfuI <jong970105@gmail.com>
2020-11-28 19:29:38 +0900

    잘못된 입력 메소드 구현

--- src/index.js
+++ src/index.js
@@ -39,6 +39,24 @@ export default class BaseballGame {
     }
     return count;
   }
+
+  checkInput(userInputNumbers) {
+    let ret = true;
+    if (ret && userInputNumbers.match(/^[1-9]{3}$/) === null) {
+      ret = false;
+    }
+    if (ret && (
+      userInputNumbers[0] === userInputNumbers[1]
+      || userInputNumbers[0] === userInputNumbers[2]
+      || userInputNumbers[1] === userInputNumbers[2]
+    )) {
+      ret = false;
+    }
+    if (!ret) {
+      alert("1~9까지의 수를 중복없이 3개를 작성해주세요.");
+    }
+    return ret;
+  }
 }
 
 new BaseballGame();
