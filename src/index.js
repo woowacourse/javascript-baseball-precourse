3a6da305c6eff2cf6c825489f431be72af6214da
박종한 <exuberant94@naver.com>
2020-11-29 00:23:33 +0900

    기능 구현 - 입력한 세 숫자 중 중복발생 시 alert띄우고 입력값 초기화 후 focus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ export default function BaseballGame() {
 }
 
 const input = document.getElementById('user-input');
+submit.onclick = () => {
+  const inputArrays = input.value.split('');
+  // 중복된 값 체크
+  if (
+    inputArrays[0] === inputArrays[1] ||
+    inputArrays[1] === inputArrays[2] ||
+    inputArrays[0] === inputArrays[2] ||
+    inputArrays.length !== 3
+  ) {
+    alert('숫자가 유효하지 않습니다. 다시 입력해주세요!'); // 중복 발생시 alert
+    input.value = '';
+    input.focus();
+  }
+};
 // export default class BaseballGame {
 //   play(computerInputNumbers, userInputNumbers) {
 //     return "결과 값 String";
