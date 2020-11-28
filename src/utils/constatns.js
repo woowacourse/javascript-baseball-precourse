5dd6245f0929c72d9d2f1b9b57120457393ed395
2SOOY <1401660@naver.com>
2020-11-28 22:19:05 +0900

    Add - 태그 ID, 메시지, 각종 상수들 상수 처리를 위한 constant 파일 생성 - ID : DOM 선택자 - GAME : 숫자 범위, 3 - MESSAGE: 사용자 입력 및 힌트에 대한 메시지 - KEY : 키보드 키

---
+++ src/utils/constatns.js
@@ -0,0 +1,26 @@
+export const ID = {
+  APP: 'app',
+  USER_INPUT: 'user-input',
+  SUBMIT_BUTTON: 'submit',
+  RESULT_VIEW: 'result',
+  RESTART_BUTTON: 'game-restart-button'
+};
+
+export const GAME = {
+  THREE: 3,
+  START: 1,
+  LAST: 9,
+};
+
+export const MESSAGE = {
+  SUCCESS: '🎉 정답을 맞추셨습니다 🎉',
+  RESTART: '게임을 새로 시작하시겠습니까 ?',
+  INPUT_ERROR: '숫자를 다시 입력해주세요 ✍',
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱'
+};
+
+export const KEY = {
+  ENTER = 'Enter'
+}
