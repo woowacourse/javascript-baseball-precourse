ddf67a521396282085910307c9e65753a0093c8f
ginger-kang <kdhoon07@gmail.com>
2020-11-28 18:58:01 +0900

    feat: game start when enter Enter key or click ok button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
+  const submitButton = document.querySelector("#submit");
+  const userInput = document.querySelector("#user-input");
+
+  const play = (computerInputNumbers, userInputNumbers) => {
     return "결과 값 String";
   };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  const gameStart = () => {
+    console.log("game start!");
+  };
+
+  const init = () => {
+    submitButton.addEventListener("click", () => gameStart());
+    userInput.addEventListener("keyup", (event) => {
+      if (event.keyCode === 13) {
+        gameStart();
+      }
+    });
+  };
+
+  init();
+}
 
 new BaseballGame();
