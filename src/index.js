a00b591f676435ebc1e7840139ba30fae5f2f3f5
yang <yaaaaang57@naver.com>
2020-11-28 17:33:49 +0900

    modify random number method

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 export default function BaseballGame() {
   this.getRandomNumber = () => {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let pickedNumbers = [];
     for (let i = 0; i < 3; i++) {
       let picked;
-      if (i == 0) {
-        picked = numbers.splice(Math.floor(Math.random() * 9), 1)[0];
-        pickedNumbers.push(picked);
-      } else {
-        picked = numbers.splice(Math.floor(Math.random() * (10 - i)), 1)[0];
-        pickedNumbers.push(picked);
-      }
+      picked = numbers.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+      pickedNumbers.push(picked);
     }
     return pickedNumbers;
   };
 
-  this.play = (computerInputNumbers, userInputNumbers) => {
+  this.play = function (computerInputNumbers, userInputNumbers) {
     return '결과 값 String';
   };
 }
