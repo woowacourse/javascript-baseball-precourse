7fb9e99e6ef07c9e45d33528470bd2c0abbb80dc
yang <yaaaaang57@naver.com>
2020-11-28 17:01:52 +0900

    generate random number

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+  this.getRandomNumber = () => {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let pickedNumbers = [];
+    for (let i = 0; i < 3; i++) {
+      let picked;
+      if (i == 0) {
+        picked = numbers.splice(Math.floor(Math.random() * 9), 1)[0];
+        pickedNumbers.push(picked);
+      } else {
+        picked = numbers.splice(Math.floor(Math.random() * (10 - i)), 1)[0];
+        pickedNumbers.push(picked);
+      }
+    }
+    return pickedNumbers;
+  };
+
+  this.play = (computerInputNumbers, userInputNumbers) => {
+    return '결과 값 String';
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
 new BaseballGame();
