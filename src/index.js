805d84b27231a4ac6a378a8a64561fd66cb0612d
swimme <2sy100@likelion.org>
2020-11-28 13:15:14 +0900

    refactor: change return type of generating random input method

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 export default class BaseballGame {
   constructor() {
-    this.computerInputNumbers = [];
-
     this.init();
   }
 
   init = () => {
-    this.computerInputNumbers = this.setRandomNumbers();
+    this.computerInputNumbers = this.generateRandomNumbers();
   };
 
-  setRandomNumbers() {
-    const randomNumbers = [];
+  generateRandomNumbers() {
+    let randomNumbers = "";
     while (randomNumbers.length != 3) {
-      const number = Math.floor(Math.random() * 9) + 1;
-      if (!randomNumbers.includes(number)) randomNumbers.push(number);
+      const number = String(Math.floor(Math.random() * 9) + 1);
+      if (!randomNumbers.includes(number)) randomNumbers += number;
     }
     return randomNumbers;
   }
 }
+const game = new BaseballGame();
+console.log(game.computerInputNumbers);
