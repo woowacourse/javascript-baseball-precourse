43854882d66f1838bd4650a8da714992393a3988
jehong <jehong@student.42seoul.com>
2020-11-29 05:17:45 +0900

    feat: add DisplaySuccessMessage()

--- src/handleResult.js
+++ src/handleResult.js
@@ -23,4 +23,15 @@ export default function HandleResult() {
         alert('게임을 재시작해주세요')
     })
   }
+
+  const DisplaySuccessMessage = () => {
+    let restartMessage = document.createElement('span');
+    let restartButton = document.createElement('button');
+
+    restartMessage.textContent = '게임을 새로 시작하시겠습니까?';
+    restartButton.textContent = '게임 재시작';
+    _resultArea.innerHTML = `<h4>🎉<strong> 정답을 맞추셨습니다! </strong>🎉</h4>`;
+    _resultArea.appendChild(restartMessage);
+    _resultArea.appendChild(restartButton);
+  }
 }
