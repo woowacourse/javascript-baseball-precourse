6ed2e226dc2b6d88163e3228c12d708a59779777
ket87 <ket8780@gmail.com>
2020-11-28 23:31:14 +0900

    _makeRandomValue 메서드 구현

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@
 // }
 
 export default class BaseballGame {
+  _makeRandomValue(max, min) {
+    // max ~ min 두 값 사이의 값을 출력(max, min값 포함)
+    return Math.floor(Math.random() * max) + min
+  }
+
   play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String"
   }
