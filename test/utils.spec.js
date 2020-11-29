707008b2b79589c5c594cb1c4a7a52c1388c0575
jho2301 <jho2301@gmail.com>
2020-11-29 19:50:49 +0900

    test: utils.spec 테스트코드 작성

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,4 +1,19 @@
 /*
-check;
-getRandomNumber;
+hasDuplicateCharacter;
 */
+
+import hasDuplicateCharacter from '../src/library/utils/check.js';
+
+describe('hasDuplicateCharacter()', () => {
+  test('문자열에 중복되는 캐릭터가 있을 때 true', () => {
+    expect(hasDuplicateCharacter('112')).toBe(true);
+    expect(hasDuplicateCharacter('하하하하')).toBe(true);
+    expect(hasDuplicateCharacter('aba')).toBe(true);
+  });
+
+  test('문자열에 중복되는 캐릭터가 없으면 false', () => {
+    expect(hasDuplicateCharacter('123')).toBe(false);
+    expect(hasDuplicateCharacter('하햐허혀')).toBe(false);
+    expect(hasDuplicateCharacter('abc')).toBe(false);
+  });
+});
