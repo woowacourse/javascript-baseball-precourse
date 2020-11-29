f94e734d26ce3d22b8784280152e75485613e980
Lisboa <suna9267@gmail.com>
2020-11-29 14:55:18 +0900

    게임 결과를 연산하는 game.play 함수 작성

--- src/index.js
+++ src/index.js
@@ -16,6 +16,23 @@ class BaseballGame {
 
     return arr;
   }
+
+  play(userInput) {
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < 3; i += 1) {
+      if (this.answer[i] === Number(userInput[i])) {
+        strike += 1;
+      } else if (this.answer.includes(Number(userInput[i])) === true) {
+        ball += 1;
+      }
+    }
+
+    return {
+      ball,
+      strike
+    };
+  }
 }
 
 let game = new BaseballGame();
