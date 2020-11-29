0b9be7b53f0b55a6fbbd716402ae2d82048edc58
권세진 <0307kwon@gmail.com>
2020-11-29 16:23:44 +0900

    코드 컨벤션에 따라 코드 수정

--- src/baseballGame.js
+++ src/baseballGame.js
@@ -1,18 +1,24 @@
 export default class BaseballGame {
 
+    //스트라이크 갯수를 체크합니다.
     checkStrike(computerNumArr,userInputNumArr){
+    
         let strike = 0;
+
         computerNumArr.forEach((element,index)=>{
             if(element === userInputNumArr[index]){
                 strike++;
             }
         });
+
         return strike;
     }
 
+    // 볼 갯수를 체크합니다. 
     checkBall(computerNumArr,userInputNumArr){
-        let ball = 0;
         const userInputSet = new Set(userInputNumArr);
+        let ball = 0;
+
         computerNumArr.forEach((element,index)=>{
             if(element === userInputNumArr[index]){
                 return;
@@ -20,20 +26,24 @@ export default class BaseballGame {
             if(userInputSet.has(element)){
                 ball++;
             }
-        })
+        });
+
         return ball;
     }
 
+    // 사용자가 예측한 숫자에 대한 평가를 수행합니다. 
     play(computerInputNumbers, userInputNumbers) {
         const computerNumArr = [...String(computerInputNumbers)];
         const userInputNumArr = [...String(userInputNumbers)];
         let strike = 0;
         let ball = 0;
         let return_str = "";
+
         //스트라이크
         strike += this.checkStrike(computerNumArr,userInputNumArr);
         //볼
         ball += this.checkBall(computerNumArr,userInputNumArr);
+
         //리턴 메세지 구성
         if(strike === 3){
             return_str = `
@@ -49,13 +59,15 @@ export default class BaseballGame {
         }else{
             return_str = "낫싱";
         }
-        console.log(return_str);
         return return_str;
     }
-
+    
+    // 컴퓨터가 랜덤으로 숫자를 선택
     initComputerNum(){
-        let num = "";
+
         const arr = [1,2,3,4,5,6,7,8,9];
+        let num = "";
+
         for(let i=0; i<3; i++){
             let ran = Math.floor(Math.random()*10)%arr.length;
             num+= arr[ran];
@@ -64,9 +76,13 @@ export default class BaseballGame {
         this.computerNum = num*1;
         console.log(num);
     }
+
+    //입력이 0이 아닌 1~9의 숫자들로만 구성되어있는지 확인
     isNum(input){
+    
         const arr = [...input];
         let ret = true;
+
         //숫자인지 확인
         if(isNaN(input*1)){
             return false;
@@ -81,18 +97,23 @@ export default class BaseballGame {
         return ret;
     }
   
+    //올바른 입력인지 모든 조건을 판단
     isAlright(input){
-      const inputSet = new Set([...input]);
-      let retValue = false;
-      //숫자인지, 중복이 존재하는지, 숫자길이가 3인지
-      if(this.isNum(input) && inputSet.size === 3 && String(input).length === 3){
-        retValue = true
-      }
-      return retValue;
+        
+        const inputSet = new Set([...input]);
+        let retValue = false;
+
+        //숫자인지, 중복이 존재하는지, 숫자길이가 3인지
+        if(this.isNum(input) && inputSet.size === 3 && String(input).length === 3){
+            retValue = true
+        }
+
+        return retValue;
     }
 
+    //임시로 컴퓨터의 숫자를 정함
     constructor(){
-        //임시로 컴퓨터의 숫자를 정함
+        
         this.computerNum;
         this.initComputerNum();
     }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ function submitInput(){
   const userInput = document.getElementById("user-input");
   const resultTxt = document.getElementById("result");
   const inputValue = userInput.value;
+
   //이상한 입력은 아닌지 체크
   if(!game.isAlright(inputValue)){
     alert("중복없는 숫자 3개를 써주세요.");
     userInput.value = "";
     return;
   }
+  
   resultTxt.innerHTML = `
   <div> 
   ${game.play(game.computerNum,inputValue)} 
