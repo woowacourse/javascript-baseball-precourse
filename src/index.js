77e5851e3bde47da14e0036ec0a1e7c6bb142b5e
sunhpark42 <sunhpark42@gmail.com>
2020-11-30 00:35:25 +0900

    refactor: put external function into the BaseballGame as method

--- src/index.js
+++ src/index.js
@@ -1,94 +1,90 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * Math.floor(max - min + 1) + min);
-}
-
-function setAnswer() {
-  let answer = [];
-
-  while (answer.length < 3) {
-    let num = getRandomInt(1, 9);
-    
-    if (answer.indexOf(num) > -1) {
-      continue;
-    } else {
-      answer.push(num);
-    }
+  this.getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * Math.floor(max - min + 1) + min);
   }
-  return answer;
-}
 
-function isValidNumber(userInput) {
-  return /[1-9]{3}/.test(userInput)
-}
-
-function isDuplicated(userInput) {
-  const num = userInput.split("");
-  const isDup = num.some(function(x) {
-    return num.indexOf(x) !== num.lastIndexOf(x);
-  });
-
-  return isDup;
-}
-
-function checkUserInput(userInput) {
-  return (isValidNumber(userInput) && !isDuplicated(userInput));
-}
-
-function countStrikeAndBall(computerInputNumbers, userInputNumbers) {
-  const computerNumberArray = String(computerInputNumbers).split("");
-  const userNumberArray = String(userInputNumbers).split("");
-  let strike = 0;
-  let ball = 0;
-
-  userNumberArray.forEach(x => {
-    if (computerNumberArray.indexOf(x) > -1) {
-      if (computerNumberArray.indexOf(x) === userNumberArray.indexOf(x)) {
-        strike += 1;
+  this.setAnswer = function() {
+    let answer = [];
+  
+    while (answer.length < 3) {
+      let num = this.getRandomInt(1, 9);
+      
+      if (answer.indexOf(num) > -1) {
+        continue;
       } else {
-        ball += 1;
+        answer.push(num);
       }
     }
-  })
-
-  return {strike, ball}
-}
-
-function getResult(strike, ball) {
-  let retString = "";
-
-  if (strike === 3) {
-    retString = "승리했습니다.";
-  } else if (strike || ball) {
-    retString = `${ball ? ball+"볼" : ""}${ball && strike ? " " : ""}${strike ? strike+"스트라이크" : ""}`
-  } else {
-    retString = "낫싱";
+    
+    return answer;
+  }
+  
+  this.isValidNumber = function(userInputNumbers) {
+    return /[1-9]{3}/.test(userInputNumbers);
   }
 
-  return retString;
-}
+  this.isDuplicated = function(userInputNumbers) {
+    const num = userInputNumbers.split("");
+    const isDup = num.some(function(x) {
+      return num.indexOf(x) !== num.lastIndexOf(x);
+    });
+  
+    return isDup;
+  }
 
-function getUserInput() {
-  return document.getElementById("user-input").value;
-}
+  this.checkUserInput = function(userInputNumbers) {
+    return (this.isValidNumber(userInputNumbers) && !this.isDuplicated(userInputNumbers));
+  }
 
-function showResult(string) {
-  document.getElementById("result").innerText = string;
-}
+  this.countStrikeAndBall = function(computerInputNumbers, userInputNumbers) {
+    const computerNumberArray = String(computerInputNumbers).split("");
+    const userNumberArray = String(userInputNumbers).split("");
+    let strike = 0;
+    let ball = 0;
+  
+    userNumberArray.forEach(x => {
+      if (computerNumberArray.indexOf(x) > -1) {
+        if (computerNumberArray.indexOf(x) === userNumberArray.indexOf(x)) {
+          strike += 1;
+        } else {
+          ball += 1;
+        }
+      }
+    })
+  
+    return {strike, ball}
+  }
 
-function alertInvalidInputMessage() {
-  alert("잘못된 입력입니다.\n1-9사이의 세 자리 숫자를 중복없이 입력해 주세요.");
+  this.getResult = function(strike, ball) {
+    let retString = "";
+  
+    if (strike === 3) {
+      retString = "승리했습니다.";
+    } else if (strike || ball) {
+      retString = `${ball ? ball+"볼" : ""}${ball && strike ? " " : ""}${strike ? strike+"스트라이크" : ""}`
+    } else {
+      retString = "낫싱";
+    }
+  
+    return retString;
+  }
+
+  this.getUserInput = function() {
+    return document.getElementById("user-input").value;
+  }
+
+  this.showResult = function(string) {
+    document.getElementById("result").innerText = string;
+  }
+
+  this.alertInvalidInputMessage = function() {
+    alert("잘못된 입력입니다.\n1-9사이의 세 자리 숫자를 중복없이 입력해 주세요.");
+  }
+
+  this.play = function(computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  };
 }
 
 new BaseballGame();
