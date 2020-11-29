2721703b690a25fd2068550bc641d5a211a5315e
yujo <yujo@student.42seoul.kr>
2020-11-29 20:48:24 +0900

    refactor: 기능 요구사항에 맞추기 위해 Input입력 후 확인 버튼을 눌러도 기존 입력했던 값이 사라지지 않도록  기능 변경

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -3,7 +3,6 @@ import checkValidInput from './checkValidInput.js';
 export default function getUserInput() {
   const $userInput = document.querySelector('#user-input');
   const userInputNumbers = $userInput.value.split('').map((x) => Number(x));
-  $userInput.value = '';
 
   if (checkValidInput(userInputNumbers)) {
     return userInputNumbers;
