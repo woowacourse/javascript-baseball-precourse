4f8745e4996a2eb6eae19b35eeb376b7b65612e4
jho2301 <jho2301@gmail.com>
2020-11-29 18:42:57 +0900

    fix: html의 모든 컨텐츠가 로딩됐을 때 컴포넌트 초기화

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ export default class BaseballGame {
   constructor() {
     this.setAnswer();
     this.initializeStates();
-    this.initializeComponents();
+    window.onload = () => this.initializeComponents();
   }
 
   setAnswer = () => {
