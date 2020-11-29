a6e68b4303a9387eeab1935144d7a29d0a2a11d2
min1378 <qwes123@naver.com>
2020-11-29 15:38:37 +0900

    feat: 랜덤 값 생성하는 함수 구현(중복 값이 없는 3자리 숫자)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
+export default class BaseballGame {
+  constructor() {
+    this.computerInputNumbers = []
+  }
+  init() {
+    this.computerInputNumbers = this.createRandomNumbers()
+  }
+  createRandomNumbers() {
+    const randomNumbers = []
+    while (randomNumbers.length < 3) {
+      const randomNumber = Math.floor(Math.random() * (10 - 1)) + 1
+      if (randomNumbers.indexOf(randomNumber) !== -1) continue
+      randomNumbers.push(randomNumber)
+    }
+    return randomNumbers
+  }
+  play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String"
   }
 }
@@ -10,4 +25,5 @@ export default function BaseballGame() {
 //   }
 // }
 
-new BaseballGame()
+const game = new BaseballGame()
+game.init()
