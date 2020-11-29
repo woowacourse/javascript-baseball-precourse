e0900e30b52ab91acf8f695f45ae45eea3492a99
yujo <yujo@student.42seoul.kr>
2020-11-29 14:23:05 +0900

    feat: USER가 선택한 값과 컴퓨터의 값을 비교해 스트라이크와 볼이 몇개인지 체크하는 기능 구현

---
+++ src/game/gameProgress.js
@@ -0,0 +1,29 @@
+function getStrikeCount(computerInputNumbers, userInputNumbers) {
+  return computerInputNumbers.reduce((acc, currentValue, index) => {
+    if (currentValue === userInputNumbers[index]) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+}
+
+function getBallCount(computerInputNumbers, userInputNumbers) {
+  return userInputNumbers.reduce((acc, currentValue, index) => {
+    if (
+      currentValue !== computerInputNumbers[index] &&
+      computerInputNumbers.includes(currentValue)
+    ) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+}
+
+export default function gameProgress(computerInputNumbers, userInputNumbers) {
+  const strikeCount = getStrikeCount(computerInputNumbers, userInputNumbers);
+  const ballCount = getBallCount(computerInputNumbers, userInputNumbers);
+
+  console.log(computerInputNumbers, userInputNumbers);
+  console.log('strikeCount : ', strikeCount);
+  console.log('balleCount : ', ballCount);
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
+import gameProgress from './game/gameProgress.js';
 import getUserInput from './input/getUserInput.js';
 import getRandomNumbers from './utils/getRandomNumbers.js';
-
-function prepareGameSet() {
-  //
-}
-
-function startGame() {
-  //
-}
 
 export default function BaseballGame() {
   // 이벤트 리스너 등록
@@ -16,8 +9,7 @@ export default function BaseballGame() {
   const computerInputNumbers = getRandomNumbers();
 
   this.play = function (computerInputNumbers, userInputNumbers) {
-    console.log('computer : ', computerInputNumbers);
-    console.log('user : ', userInputNumbers);
+    return gameProgress(computerInputNumbers, userInputNumbers);
   };
 
   $submit.addEventListener('click', () => {
