2129701d261121b1d85f6295e9368fb3bf011e97
ket87 <ket8780@gmail.com>
2020-11-29 21:51:11 +0900

    validateInput 메서드 구현

--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
 export default class BaseballGame {
-  constructor() {
-    this.computerNumbers = this._makeDiffNumber()
+  _showErrorAlert(str) {
+    alert(`숫자 야구 게임 에러 내용\n ${str}`);
   }
 
-  _showErrorAlert(str) {
-    alert(`숫자 야구 게임 에러 내용\n ${str}`)
+  _validateInput(input) {
+    if (typeof input !== "string") {
+      return false;
+    }
+    const inputArr = input.split("");
+    const inputSet = new Set(inputArr);
+
+    return inputSet.size === input.length ? true : false;
   }
 
   play(computerInputNumbers, userInputNumbers) {
-    const computerNumbers = computerInputNumbers || this.computerNumbers
-    if (computerInputNumbers) return "결과 값 String"
+    const computerNumbers = computerInputNumbers || this.computerNumbers;
+    if (computerInputNumbers) return "결과 값 String";
   }
 }
 
 class RandomBaseball {
   constructor(length) {
-    this.computerNumbers = this._makeDiffNumber(length)
+    this.computerNumbers = this._makeDiffNumber(length);
   }
 
   resetRandomBaseball(length) {
-    this.computerNumbers = this._makeDiffNumber(length)
+    this.computerNumbers = this._makeDiffNumber(length);
   }
 
   getBaseball() {
-    return this.computerNumbers
+    return this.computerNumbers;
   }
 
   _makeRandomValue(max, min) {
     // max ~ min 두 값 사이의 값을 출력(max, min값 포함)
-    return Math.floor(Math.random() * max) + min
+    return Math.floor(Math.random() * max) + min;
   }
 
   _makeDiffNumber(length = 3) {
-    const numberSet = new Set()
+    const numberSet = new Set();
     while (numberSet.size < length) {
-      const randomNumber = this._makeRandomValue(9, 1)
-      numberSet.add(randomNumber)
+      const randomNumber = this._makeRandomValue(9, 1);
+      numberSet.add(randomNumber);
     }
     // set에서 string으로 변환
-    let stringNumbers = [...numberSet].join("")
-    return parseInt(stringNumbers)
+    let stringNumbers = [...numberSet].join("");
+    return parseInt(stringNumbers);
   }
 }
-
-const randomBaseball = new RandomBaseball()
-console.log(randomBaseball)
