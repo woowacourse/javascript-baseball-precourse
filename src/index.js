90dbcc99d6716df58944ceedc0822575de8251c6
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-11-30 09:45:47 +0900

    feat : 사용자가 입력한 숫자의 유효성을 검사하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -2,11 +2,41 @@ export default function BaseballGame() {
   const $userInput = document.querySelector("#user-input");
   const $submitBtn = document.querySelector("#submit");
 
-  let userInputNumbers = new Array(3).fill(0);
+  const hasSameNumber = (numberArray) => {
+    const sameIndex = numberArray.filter(
+      (value, index) => value === numberArray[index + 1]
+    );
+    return sameIndex.length !== 0;
+  };
+
+  const isValidNumber = (numberArray) => {
+    if (hasSameNumber(numberArray)) {
+      alert("서로 다른 숫자를 입력해주세요");
+      return true;
+    }
+
+    if (numberArray.indexOf("0") !== -1) {
+      alert("1부터 9까지의 숫자 중에서 입력해주세요");
+      return true;
+    }
+
+    if (numberArray.length !== 3) {
+      alert("숫자 3개를 입력해주세요");
+      return true;
+    }
+
+    return false;
+  };
 
   const handleUserInputSubmit = (e) => {
     e.preventDefault();
-    userInputNumbers = $userInput.value.split("");
+    let userInputNumbers = null;
+
+    const currentInputNumber = $userInput.value.split("");
+    if (!isValidNumber(currentInputNumber)) {
+      userInputNumbers = currentInputNumber;
+    }
+    console.log(userInputNumbers);
     $userInput.value = "";
   };
 
