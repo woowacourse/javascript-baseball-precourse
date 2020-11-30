86371d83ce615790949bb0429be9496fbd44525e
swon3210 <swon3210@gmail.com>
2020-11-30 15:50:04 +0900

    feat: implement calculatePoints function
    this function is not public

--- src/classes/baseballGame.js
+++ src/classes/baseballGame.js
@@ -1,4 +1,24 @@
+import { NUMBERS_LENGTH } from '../constants/configuration.js'
+
 export default class BaseballGame {
+  _calculatePoints(numbers1, numbers2) {
+    const points = {
+      strikeCount: 0,
+      ballCount: 0,
+    };
+    const nonStrikeNumbers = [];
+    for (let i = 0; i < NUMBERS_LENGTH; i++) {
+      if (numbers1[i] === numbers2[i]) {
+        points.strikeCount += 1;
+      } else {
+        nonStrikeNumbers.push(numbers1[i]);
+        nonStrikeNumbers.push(numbers2[i]);
+      }
+    }
+    points.ballCount = nonStrikeNumbers.length - new Set(nonStrikeNumbers).size;
+
+    return points;
+  }
   play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   }
