3a0957dcf28b9f63f39d2cfd0be01e9683ee62f5
jang-yunmi <newapache@naver.com>
2020-11-30 23:55:01 +0900

    style: javascript 컨벤션 반영

--- src/index.js
+++ src/index.js
@@ -12,24 +12,39 @@ export default function BaseballGame() {
 const findLocation = (target, nums) => {
   const numArray = nums.split("");
   const idx = numArray.findIndex((num) => num === target);
+
   return idx;
 };
 
 const compareNums = (computerNum, inputNum) => {
   let ball = 0;
   let strike = 0;
+  let resultString = "";
+
   for (let i = 0; i <= inputNum.length; i++) {
     let idx = findLocation(inputNum[i], computerNum);
-    if (idx === -1) continue;
-    else if (idx === i) strike++;
-    else ball++;
+    if (idx === -1) {
+      continue;
+    } else if (idx === i) {
+      strike++;
+    } else {
+      ball++;
+    }
   }
 
-  if (strike === 3 && ball === 0) gameData.isSuccess = true;
-  else if (strike === 0 && ball === 0) return "낫싱";
-  else if (strike !== 0 && ball === 0) return `${strike}스트라이크`;
-  else if (strike === 0 && ball !== 0) return `${ball}볼`;
-  else return `${ball}볼 ${strike}스트라이크`;
+  if (strike === 3 && ball === 0) {
+    gameData.isSuccess = true;
+  } else if (strike === 0 && ball === 0) {
+    resultString = "낫싱";
+  } else if (strike !== 0 && ball === 0) {
+    resultString = `${strike}스트라이크`;
+  } else if (strike === 0 && ball !== 0) {
+    resultString = `${ball}볼`;
+  } else {
+    resultString = `${ball}볼 ${strike}스트라이크`;
+  }
+
+  return resultString;
 };
 
 const resetResult = () => {
@@ -51,37 +66,48 @@ const updateResult = (result) => {
     resetResult();
     init();
   } else {
-    console.log(gameData.computerNumbers);
     document.getElementById("result").innerHTML = result;
   }
 };
 
 const validateInput = (input) => {
   const inputArray = input.split("");
-  if (input.length !== 3 || input.includes("0") || isNaN(parseInt(input)))
-    return false;
-  if (new Set(inputArray).size !== inputArray.length) return false;
-  return true;
+  let isValidate = true;
+
+  if (input.length !== 3 || input.includes("0") || isNaN(parseInt(input))) {
+    isValidate = false;
+  }
+  if (new Set(inputArray).size !== inputArray.length) {
+    isValidate = false;
+  }
+
+  return isValidate;
 };
 
 const getUserInput = () => {
   let userInput = document.getElementById("user-input").value;
+
   if (!validateInput(userInput)) {
     alert("세 자리의 중복 없는 숫자를 다시 입력해주세요.");
     document.getElementById("user-input").value = "";
   } else {
     let result = new BaseballGame().play(gameData.computerNumbers, userInput);
+    console.log(result, gameData.computerNumbers);
     updateResult(result);
   }
 };
 
 const makeComputerInput = () => {
-  const nums = [];
+  let nums = [];
   let digit = Math.floor(Math.random() * 10);
+
   for (let i = 0; i < 3; i++) {
-    while (nums.includes(digit)) digit = Math.floor(Math.random() * 10);
+    while (nums.includes(digit)) {
+      digit = Math.floor(Math.random() * 10);
+    }
     nums.push(digit);
   }
+
   return nums.join("");
 };
 
