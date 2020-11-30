d256215c60886bca73f90d056680adb308660559
swon3210 <swon3210@gmail.com>
2020-11-30 15:58:07 +0900

    refactor: make more accurate alert message in playGame function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ALL_MATCH_TEXT, NUMBERS_LENGTH } from './constants/configuration.js';
+import { ALL_MATCH_TEXT, NUMBERS_LENGTH, NUMBER_CANDIDATES } from './constants/configuration.js';
 
 import BaseballGame from './classes/baseballGame.js';
 import Opponent from './classes/opponent.js';
@@ -16,7 +16,7 @@ let randomOpponentNumbers = opponent.getRandomNumbers();
 const playGame = () => {
   const playResult = baseballGame.play(randomOpponentNumbers, userInputElement.value);
   if (playResult === '') {
-    alert(`잘못된 입력입니다. '서로 다른' ${NUMBERS_LENGTH}자리 숫자를 입력해주세요!`);
+    alert(`잘못된 입력입니다. '서로 다른' ${NUMBERS_LENGTH}자리 숫자를 ${NUMBER_CANDIDATES} 숫자들만을 이용해서 입력해주세요!`);
     return
   }
   resultElement.innerText = playResult;
