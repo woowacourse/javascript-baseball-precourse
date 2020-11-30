d91d2275150669d86474bf24ee0ee6b70022d13b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-11-30 18:34:21 +0900

    fix : 랜덤 숫자 생성시 오류 수정
    - 게임을 새로 시작할 때마다, 삭제된 배열의 수가 다시 복구 되도록 코드 수정

--- src/numberGenerator.js
+++ src/numberGenerator.js
@@ -1,8 +1,9 @@
 const computerInputNumbers = new Array(3).fill(0);
-const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const NUMBER_LENGTH = 3;
 
 export default function numberGenerator() {
+  const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   for (let i = 0; i < NUMBER_LENGTH; i++) {
     const pickNumber = Math.floor(Math.random() * (numberArray.length - 1) + 1);
     const selectedIndex = numberArray.indexOf(pickNumber);
