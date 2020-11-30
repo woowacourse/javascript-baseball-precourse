b8c8787b1db2abfb7b7781f7965c320f9abc8a51
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-11-30 09:40:55 +0900

    feat : 사용자가 입력한 숫자를 저장하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 export default function BaseballGame() {
+  const $userInput = document.querySelector("#user-input");
+  const $submitBtn = document.querySelector("#submit");
+
+  let userInputNumbers = new Array(3).fill(0);
+
+  const handleUserInputSubmit = (e) => {
+    e.preventDefault();
+    userInputNumbers = $userInput.value.split("");
+    $userInput.value = "";
+  };
+
+  $submitBtn.addEventListener("click", handleUserInputSubmit);
+
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
 }
-
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
 
 new BaseballGame();
