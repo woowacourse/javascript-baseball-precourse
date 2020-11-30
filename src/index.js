61c2c3366db2343cf2ff98eca341d13097d9bd1f
jang-yunmi <newapache@naver.com>
2020-11-30 12:27:28 +0900

    feat: 게임 초기화 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+const gameData = {
+  computerNumbers: -1,
+  isSuccess: false,
+};
+
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+const getUserInput = () => {};
 
-new BaseballGame();
+const makeComputerInput = () => {};
+
+const init = () => {
+  gameData.isSuccess = false;
+  gameData.computerNumbers = makeComputerInput();
+  document.getElementById("submit").addEventListener("click", getUserInput);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  init();
+});
