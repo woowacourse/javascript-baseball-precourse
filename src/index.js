f8ae222f8090882a38b7e16bc8382224f4ae0581
song <johyesong8686@gmail.com>
2020-11-30 18:27:49 +0900

    Add ValidateInput Function and Modify README.md

--- src/index.js
+++ src/index.js
@@ -14,12 +14,39 @@ export function createRandomNums() {
   return randomNumbers;
 }
 
+export function validateInput(userInputNumbers) {
+  const nonDuplicateNumbers = Array.from(new Set(userInputNumbers));
+  // 숫자가 아닐때, 글자수가 3이 아닐때,중복된 숫자로 전체 길이가 3이 안될때 걸러냄
+  if (
+    !Number(userInputNumbers) ||
+    userInputNumbers.length !== 3 ||
+    nonDuplicateNumbers.length !== 3
+  ) {
+    alert("숫자를 올바르게 입력하세요.[세 자리의 다른수 ex.127]");
+
+    return false;
+  }
+
+  return true;
+}
+
 export default function BaseballGame() {
+  const submitButton = document.querySelector("#submit");
   this.computerNumbers = createRandomNums();
   // console.log(this.computerNumbers);
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
+
+  submitButton.addEventListener("click", () => {
+    const userInput = document.querySelector("#user-input");
+
+    if (validateInput(userInput.value)) {
+    } else {
+      userInput.value = "";
+      userInput.focus();
+    }
+  });
 }
 
 new BaseballGame();
