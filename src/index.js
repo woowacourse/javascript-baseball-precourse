ab1ce32d6ca18a3556c10cccfcfcfe66414ed1b4
minkyung <mnk918p@gmail.com>
2020-11-30 15:58:59 +0900

    refactor: game_play.js 내용을 index.js로 이동, 필요 없어진 game_play.js 파일 삭제 주어진 BaseballGame() 함수를 사용해 게임이 진행될 수 있도록 index.js에 game-play 관련 기능을 추가

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
-import playGame from "./game-play.js";
 import randomNumberMaker from "./random-number-maker.js";
+import userInput, { isValidUserInput } from "./user-input.js";
+import countStrike, { isGameEnded, countBall } from "./game-count.js";
+import printResult, { clearResult, makeRestartBtn } from "./print-result.js";
+
+const state = {
+  givenNumberLength: 3,
+  randomNumber: "",
+  userInputValue: "",
+  ballCount: "",
+  strikeCount: "",
+  returnMessage: ""
+};
 
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-
-    return "결과 값 String";
-  };
-}
-
-const state = { 'randomNumber' : '', 'givenNumberLength' : 3 };
-
-function init() {
   const givenNumberLength = state.givenNumberLength;
-  const randomNumberArray = randomNumberMaker(givenNumberLength);
-  state.randomNumber = randomNumberArray;
+  const submitButton = document.getElementById("submit");
+
+  state.randomNumber = randomNumberMaker(givenNumberLength);
+
+  this.play = function(computerInputNumbers, userInputNumbers) {
+    let validUserInputValueArray = [];
+
+    if (isValidUserInput(userInputNumbers, givenNumberLength)) {
+      validUserInputValueArray = userInputNumbers.split("").map(Number);
+
+      state.ballCount = countBall(computerInputNumbers, validUserInputValueArray);
+      state.strikeCount = countStrike(computerInputNumbers, validUserInputValueArray);
+      state.returnMessage = printResult(state.ballCount, state.strikeCount);
+    } else {
+      clearResult();
+    }
+
+    return state.returnMessage;
+  };
+
+  this.restartPlay = function() {
+    state.randomNumber = randomNumberMaker(givenNumberLength);
+    clearResult(state);
+  };
+
+  submitButton.addEventListener("click", () => {
+    state.userInputValue = userInput();
+    this.play(state.randomNumber, state.userInputValue);
+
+    if (isGameEnded(state.strikeCount)) {
+      makeRestartBtn();
+      const gameRestartButton = document.getElementById("game-restart-button");
+
+      gameRestartButton.addEventListener("click", () => {
+        this.restartPlay();
+      });
+    }
+  });
 }
-
-init();
-
-document.getElementById('submit').addEventListener('click', () => {  
-  playGame(state);
-})
 
 new BaseballGame();
-// BaseballGame.play(computerInputNumbers, computerInputNumbers);
-
-// const baseballGame = new BaseballGame();
-// baseballGame.play();
