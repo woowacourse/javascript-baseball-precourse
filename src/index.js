c947111c23ac972a12e772db1e376c115c56cd76
jooeun <joonie811@naver.com>
2020-12-01 18:27:21 +0900

    feat: 랜덤값 생성 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+  const submitBtn = document.querySelector("#submit");
+  const userInput = document.querySelector("#user-input");
+  const result = document.querySelector("#result");
+  let computerInputNumbers = [];
+
+  this.randomNumGenerator = function () {
+    const MAX_NUM = 9;
+    const MIN_NUM = 1;
+    let selectedNum = new Array();
+
+    while (!(selectedNum.length === 3)){
+      let randomNum = Math.floor(Math.random() * (MAX_NUM + 1 - MIN_NUM) + MIN_NUM);
+      if (!selectedNum.includes(randomNum.toString())){
+        selectedNum.push(randomNum.toString());
+      }
+    }
+    return selectedNum;
+  }
+
+  this.init = function() {
+    result.innerHTML = '';
+    userInput.value = '';
+    computerInputNumbers = this.randomNumGenerator();
+  }
+
+  this.init();
+}
 
 new BaseballGame();
