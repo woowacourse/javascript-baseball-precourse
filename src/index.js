e807b153d970a62c08324516a48a4de5f0cd87f5
nxxc <nomad.nxxc@gmail.com>
2020-12-01 15:54:56 +0900

    [feat] 중복 없는 세자리 숫자 만들기 & 초기설정
    - querySelector를 이용한 DOM 요소들 가져오기
    - setRandomNumber를 통해 중복 없는 세자리 숫자 만들기
    - gameInit 을 통해 게임 초기화

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
-}
+export default class BaseballGame {
+    constructor() {
+        this.randomNumbers;
+        this.userInputNumbers;
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+        this.userInput = document.querySelector('#user-input');
+        this.confirmBtn = document.querySelector('#submit');
+        this.resultSection = document.querySelector('#result');
+
+        this.gameInit();
+    }
+    gameInit() {
+        this.setRandomNumber();
+    }
+
+    setRandomNumber() {
+        const candidates = [];
+        while (candidates.length < 3) {
+            const candidate = Math.floor(Math.random() * 9) + 1;
+            if (!candidates.includes(candidate)) {
+                candidates.push(candidate);
+            }
+        }
+        const generatedNumber = candidates.join('');
+        this.randomNumbers = generatedNumber;
+    }
+    // numberValidator() {}
+    // handleUserInput() {}
+    // onConfirmBtnClick() {}
+    // showAlert() {}
+    // compareNumbers() {}
+    // appendResult() {}
+    // onRestartBtnClick() {}
+    // gameInit() {}
+
+    play(computerInputNumbers, userInputNumbers) {
+        return '결과 값 String';
+    }
+}
 
 new BaseballGame();
