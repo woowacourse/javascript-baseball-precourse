0d07ad82c019d71e26bae5678d0dd141ae8234a2
min1378 <qwes123@naver.com>
2020-12-01 17:33:38 +0900

    refactor: 컨벤션에 맞게 if문에 중괄호 추가

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ export default class BaseballGame {
 
   inputValidation(string) {
     const stringToNumber = Number(string)
-    if (isNaN(stringToNumber)) return true
-    if (stringToNumber < 100 || stringToNumber > 999) return true
-    if (string.indexOf("0") > -1) return true
+    if (isNaN(stringToNumber)) {
+      return true
+    }
+    if (stringToNumber < 100 || stringToNumber > 999) {
+      return true
+    }
+    if (string.indexOf("0") > -1) {
+      return true
+    }
     const NumberList = string.split("")
     const isDuplicateNumber = NumberList.some((number) => NumberList.indexOf(number) !== NumberList.lastIndexOf(number))
-    if (isDuplicateNumber) return true
+    if (isDuplicateNumber) {
+      return true
+    }
     return false
   }
   getUserInputNumbers() {
@@ -27,7 +35,6 @@ export default class BaseballGame {
     this.userInputNumbers = userInputString.split("")
     const result = document.getElementById("result")
     result.innerHTML = this.play(this.computerInputNumbers, this.userInputNumbers)
-    console.log(this.computerInputNumbers, this.userInputNumbers)
     const restartBtn = document.getElementById("restart")
     if (restartBtn) {
       restartBtn.addEventListener("click", () => this.init())
@@ -40,8 +47,9 @@ export default class BaseballGame {
   }
   init() {
     this.computerInputNumbers = this.createRandomNumbers()
-    if (!this.isRestart) this.setBtnEventListener()
-
+    if (!this.isRestart) {
+      this.setBtnEventListener()
+    }
     const userInput = document.getElementById("user-input")
     userInput.value = ""
     const result = document.getElementById("result")
@@ -51,7 +59,9 @@ export default class BaseballGame {
     const randomNumbers = []
     while (randomNumbers.length < 3) {
       const randomNumber = (Math.floor(Math.random() * (10 - 1)) + 1).toString()
-      if (randomNumbers.indexOf(randomNumber) !== -1) continue
+      if (randomNumbers.indexOf(randomNumber) !== -1) {
+        continue
+      }
       randomNumbers.push(randomNumber)
     }
     return randomNumbers
@@ -59,8 +69,12 @@ export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
     const strikeCount = computerInputNumbers.filter((computerNumber, index) => computerNumber === userInputNumbers[index]).length
     const ballCount = computerInputNumbers.filter((computerNumber, index) => userInputNumbers.indexOf(computerNumber) !== index && userInputNumbers.indexOf(computerNumber) > -1).length
-    if (strikeCount === 3) return `🎉<strong>정답을 맞추셨습니다!</strong>🎉</b></br>게임을 새로 시작하시겠습니까? <button id="restart">재시작</button>`
-    if (strikeCount === 0 && ballCount === 0) return "낫싱"
+    if (strikeCount === 3) {
+      return `🎉<strong>정답을 맞추셨습니다!</strong>🎉</b></br>게임을 새로 시작하시겠습니까? <button id="restart">재시작</button>`
+    }
+    if (strikeCount === 0 && ballCount === 0) {
+      return "낫싱"
+    }
     const ballString = ballCount > 0 ? `${ballCount}볼` : ""
     const strikeString = strikeCount > 0 ? ` ${strikeCount}스트라이크` : ""
     const resultString = ballString + strikeString
