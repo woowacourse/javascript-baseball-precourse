c4dc2a67a2de5909f340c114b33d106dcb5d3cbc
Jeongmin Woo <greenblues1190@gmail.com>
2021-11-25 01:08:36 +0900

    feat(BaseballGame): 사용자와 상대바의 수 비교 기능 구현
    BaseballGame의 play 메소드에서 사용자와 상대방의 수를 비교해 ballCount와 strikeCount를 반환합니다. submit 버튼을 클릭하면 미리 생성해둔 수를 사용하기 위해 play 메소드를 감싼 playWithGeneratedNumber 메소드가 호출됩니다.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
-import { NUMBER_RANGE_MIN, NUMBER_RANGE_MAX } from "./config.js";
+/* eslint-disable func-names */
 import { validateInput } from "./validation.js";
+import { NUMBER_RANGE_MIN, NUMBER_RANGE_MAX } from "./config.js";
 
-const generateRandomNumbers = () => {
-  const numberSet = new Set();
+function BaseballGame() {
+  this.generateRandomNumbers = function () {
+    const numberSet = new Set();
 
-  while (numberSet.size < 3) {
-    const newNumber = MissionUtils.Random.pickNumberInRange(
-      NUMBER_RANGE_MIN,
-      NUMBER_RANGE_MAX
-    );
+    while (numberSet.size < 3) {
+      // eslint-disable-next-line no-undef
+      const newNumber = MissionUtils.Random.pickNumberInRange(
+        NUMBER_RANGE_MIN,
+        NUMBER_RANGE_MAX
+      );
 
-    if (!numberSet.has(newNumber)) {
-      numberSet.add(newNumber);
+      if (!numberSet.has(newNumber)) {
+        numberSet.add(newNumber);
+      }
     }
-  }
 
-  return [...numberSet].join('');
-};
+    return [...numberSet].join("");
+  };
+
+  this.computerNumbers = this.generateRandomNumbers();
+
+  this.play = function (computerInputNumbers, userInputNumbers) {
+    const result = this.compareNumbers(computerInputNumbers, userInputNumbers);
+
+    return result;
+  };
+
+  this.playWithGeneratedNumber = function (userInputNumbers) {
+    return this.play(this.computerNumbers, userInputNumbers);
+  };
+
+  this.compareNumbers = function (computerInputNumbers, userInputNumbers) {
+    const answerSet = new Set(computerInputNumbers);
+    let ballCount = 0;
+    let strikeCount = 0;
+    let idx = 0;
+
+    for (; idx < 3; idx += 1) {
+      if (userInputNumbers[idx] === computerInputNumbers[idx]) {
+        strikeCount += 1;
+      } else if (answerSet.has(userInputNumbers[idx])) {
+        ballCount += 1;
+      }
+    }
+
+    return { ballCount, strikeCount };
+  };
+}
+
+const baseballGame = new BaseballGame();
 
 const handleSubmitForm = (e) => {
   e.preventDefault();
@@ -34,7 +69,7 @@ const handleClickSubmitButton = () => {
     return;
   }
 
-  console.log(generateRandomNumbers());
+  console.log(baseballGame.playWithGeneratedNumber(userInput));
 };
 
 const initEventListener = () => {
