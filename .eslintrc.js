f6479b60ee4b949ca963bfd774abdb0bdbee657e
Jeongmin Woo <greenblues1190@gmail.com>
2021-11-24 19:40:15 +0900

    chore: eslint, prettier, gitignore 설정

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-base",
+        "prettier",
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
