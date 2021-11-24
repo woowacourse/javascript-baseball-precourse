c655841c331844c8d93aacdb7021ef2f6b9bfc33
leezer94 <2kunhee94@gmail.com>
2021-11-24 23:42:22 +0900

    feat : strikeCount, ballCount 기반 결과값 innerHTML 표시하는 기능 구현

---
+++ src/gameResult.js
@@ -0,0 +1,21 @@
+import { gameCount } from "./utils/gameCount.js";
+
+export function gameResult(computerInput, userInput) {
+  const result = document.querySelector("#result");
+  const [strikeCount, ballCount] = gameCount(computerInput, userInput);
+
+  if (!(strikeCount === 0 && ballCount === 0)) {
+    if (strikeCount === 3) {
+      result.innerHTML =
+        "🎉정답을 맞추셨습니다!🎉 <br><br> 게임을 재시작 하시겠습니까?  ";
+    } else if (strikeCount === 0) {
+      result.innerHTML = `${ballCount}: 볼`;
+    } else if (ballCount === 0) {
+      result.innerHTML = `${strikeCount}: 스트라이크`;
+    } else {
+      result.innerHTML = `${ballCount}: 볼 , ${strikeCount}: 스트라이크`;
+    }
+  } else {
+    result.innerHTML = "낫싱";
+  }
+}
