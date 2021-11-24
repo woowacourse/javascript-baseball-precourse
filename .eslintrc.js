ce29522d79a9be3d1acd227ccb72ff2774853939
InSeongSo <goflvhxj2547@gmail.com>
2021-11-24 16:51:23 +0900

    🔬Chore: 의존성 설치
    @woowacourse/mission-utils, ESLinter, Prettier, Airbnb config를 설치하고 설정한다.

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true, // 'module' is not defined. 에러가 발생하면 기입
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:cypress/recommended',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
