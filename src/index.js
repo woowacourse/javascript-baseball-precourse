294fb3c08d8dc2462238064837043ceebd65dc60
wonsss <bigdatajang@gmail.com>
2021-11-24 22:43:31 +0900

    feat: play 메서드 기능 추가
    사용자로부터 입력받은 숫자 3개를 컴퓨터가 저장해둔 숫자 3개와 비교하여 목표하는 반환값 정상 출력 성공

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
-    return '결과 값 String';
+    let valueOfStrike = 0;
+    let valueOfBall = 0;
+    for (let i = 0; i < 3; i++) {
+      if (String(computerInputNumbers)[i] === String(userInputNumbers)[i]) {
+        valueOfStrike++;
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (String(computerInputNumbers)[i] === String(userInputNumbers)[j]) {
+          valueOfBall++;
+          continue;
+        }
+      }
+    }
+
+    valueOfBall -= valueOfStrike;
+
+    return valueOfStrike === 0 && valueOfBall === 0
+      ? '낫싱'
+      : valueOfStrike === 0 && valueOfBall !== 0
+      ? `${valueOfBall}볼`
+      : valueOfStrike !== 0 && valueOfBall === 0
+      ? `${valueOfStrike}스트라이크`
+      : `${valueOfBall}볼 ${valueOfStrike}스트라이크`;
   }
 }
 
 const baseballGame = new BaseballGame();
-baseballGame.play(123, 456); // '낫싱'
-baseballGame.play(123, 345); // '1볼'
-baseballGame.play(123, 432); // '2볼'
-baseballGame.play(123, 312); // '3볼'
-baseballGame.play(123, 145); // '1스트라이크'
-baseballGame.play(123, 134); // '1볼 1스트라이크'
-baseballGame.play(123, 132); // '2볼 1스트라이크'
-baseballGame.play(123, 124); // '2스트라이크'
+console.log(baseballGame.play(123, 456)); // '낫싱'
+console.log(baseballGame.play(123, 345)); // '1볼'
+console.log(baseballGame.play(123, 432)); // '2볼'
+console.log(baseballGame.play(123, 312)); // '3볼'
+console.log(baseballGame.play(123, 145)); // '1스트라이크'
+console.log(baseballGame.play(123, 134)); // '1볼 1스트라이크'
+console.log(baseballGame.play(123, 132)); // '2볼 1스트라이크'
+console.log(baseballGame.play(123, 124)); // '2스트라이크'
