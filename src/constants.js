3c366463dcdc7d0857f5f8151802227d38a047c2
byhhh2 <rlaalsdl111@gmail.com>
2021-11-24 21:31:50 +0900

    style: create constants file

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const BALL = '볼';
+export const STRIKE = '스트라이크';
+export const NOTHING = '낫싱';
+export const WIN = '🎉 정답을 맞추셨습니다 ! 🎉';
+export const RESTART_BUTTON_TEXT = '게임 재시작';
+export const RESTART_TEXT = '게임을 새로 시작하시겠습니까? ';
