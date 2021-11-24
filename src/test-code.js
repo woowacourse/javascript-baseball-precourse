557c3a1d5f62f63e5268d1a777caca21dd6cfd06
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-11-24 16:08:04 +0900

    Feat(test): 사용자 유효성 체크 추가

--- src/test-code.js
+++ src/test-code.js
@@ -2,5 +2,19 @@
     테스트 코드 작성
 */
 export default class BaseballGame {
+    constructor() {
+        console.log(this.isUserInputVerify("122"));
+    }
+
+    isUserInputVerify(userInputNumber) {
+        const isThreeLength = userInputNumber.length === 3;
+        const isUniqueWord = [...new Set(userInputNumber.split(""))].length === 3;
+        const isOnlyNumber = isNaN(userInputNumber) === false;
+
+        return isThreeLength && isUniqueWord && isOnlyNumber;
+    }
+
     play(computerInputNumbers, userInputNumbers) {}
 }
+
+new BaseballGame();
