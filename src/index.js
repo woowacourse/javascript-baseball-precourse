e0839fa5a72b5636411f702d9bdd8a24dfda2592
Ahn <jin7969@gmail.com>
2021-11-24 23:00:46 +0900

    feat: 사용자 입력값을 받아오는 기능 추가

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ export default function BaseballGame() {
     let computerInputNumbers = pickRandomNumbers();
     console.log(computerInputNumbers);
 
+    const $userInput = document.getElementById("user-input");
+    const $submitButton = document.getElementById("submit");
+    const $resultDiv = document.getElementById("result");
+    const $resetButton = document.createElement("button");
+    $resetButton.innerHTML = "게임 재시작";
+    $resetButton.id = "game-restart-button";
+
     this.play = function (computerInputNumbers, userInputNumbers) {
         return "결과 값 String";
     };
+
+    function handleInputNumbers(event) {
+        event.preventDefault();
+        const userInputNumbers = $userInput.value;
+        console.log(userInputNumbers);
+    }
+
+    $submitButton.addEventListener("click", handleInputNumbers);
 }
 
 new BaseballGame();
