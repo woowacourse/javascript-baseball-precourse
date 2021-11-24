d4cc37312c183a15cb6680cb864172e7bd384a47
Jeongmin Woo <greenblues1190@gmail.com>
2021-11-24 21:24:22 +0900

    feat(*): 입력값 유효성 검증 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-/* eslint-disable no-console */
+import { validateInput } from "./validation.js";
+
 const handleSubmitForm = (e) => {
   e.preventDefault();
 };
 
 const handleClickSubmitButton = () => {
   const userInput = document.querySelector("#user-input").value.trim();
-  console.log(userInput);
+
+  const validationResult = validateInput(userInput);
+
+  if (!validationResult.result) {
+    alert(validationResult.message);
+    document.querySelector("#user-input").value = "";
+
+    return;
+  }
+
+  console.log("play");
 };
 
-document.querySelector("form").addEventListener("submit", handleSubmitForm);
+const initEventListener = () => {
+  document.querySelector("form").addEventListener("submit", handleSubmitForm);
 
-document
-  .querySelector("#submit")
-  .addEventListener("click", handleClickSubmitButton);
+  document
+    .querySelector("#submit")
+    .addEventListener("click", handleClickSubmitButton);
+};
+
+export default initEventListener;
+
+initEventListener();
---
+++ src/validation.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/prefer-default-export */
+import {
+  ERROR_WRONG_DIGIT,
+  ERROR_OUT_OF_RANGE,
+  ERROR_NOT_UNIQUE,
+} from "./config.js";
+
+const isThreeDigits = (input) => input.length === 3;
+
+const isNumber = (input) => {
+  const inputArray = input.split("");
+  return !inputArray.some((number) => number < "1" || number > "9");
+};
+
+const isUnique = (input) => input.length === new Set(input).size;
+
+const validateInput = (input) => {
+  let result = true;
+  const message = [];
+
+  if (!isThreeDigits(input)) {
+    result = false;
+    message.push(ERROR_WRONG_DIGIT);
+  }
+
+  if (!isNumber(input)) {
+    result = false;
+    message.push(ERROR_OUT_OF_RANGE);
+  }
+
+  if (!isUnique(input)) {
+    result = false;
+    message.push(ERROR_NOT_UNIQUE);
+  }
+
+  return {
+    result,
+    message: message.join(" "),
+  };
+};
+
+export { validateInput };
