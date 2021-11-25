a5e71626b0bb3e46f2faa7f4e5573420b24287ef
InSeongSo <goflvhxj2547@gmail.com>
2021-11-25 11:58:56 +0900

    🗃Refactor: 상수 수정
    야구 게임에 직접 영향을 주는 상수를 객체로 관리한다.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,12 @@
-export const BASEBALL_DIGIT = 3;
-export const ZERO_NUMBER = 0;
+export const BASEBALL = Object.freeze({
+  DIGIT: 3, // 자리수
+  ZERO: 0,
+  START: 1,
+  END: 9,
+});
+
 export const ERROR_MESSAGES = Object.freeze({
-  'not-enough-value': `입력 값을 확인해주세요.\n필요 자리수 : ${BASEBALL_DIGIT}`,
+  'not-enough-value': `입력 값을 확인해주세요.\n필요 자리수 : ${BASEBALL.DIGIT}`,
   'invalid-value': '옳지 못한 값입니다.',
   'included-zero': '0을 제외하고 입력해주세요.',
   'is-duplicated-value': '중복된 값은 입력될 수 없습니다.',
