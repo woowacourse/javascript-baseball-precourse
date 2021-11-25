4ded3a7176f7cbf620e897424b9f60894b666bb1
InSeongSo <goflvhxj2547@gmail.com>
2021-11-25 10:57:17 +0900

    ✒️Feat: '0' 체크 기능 추가
    1. 공통 함수 리팩토링(조건문 분리)
    2. 상수 추가

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,12 @@
-import { BASEBALL_DIGIT, ERROR_MESSAGES } from '../constants/index.js';
+import { BASEBALL_DIGIT, ZERO_NUMBER, ERROR_MESSAGES } from '../constants/index.js';
 
 export const $ = selector => document.querySelector(selector);
+
+export const isCorrectLength = (value, digit) => value.trim().length !== digit;
+
+export const isNumber = value => !Number.isNaN(+value);
+
+export const isIncludes = (value, target) => value.includes(target);
 
 export const isDuplicateLetter = value => {
   const target = value.split('');
@@ -13,9 +19,9 @@ export const setErrorMessage = type =>
   alert(ERROR_MESSAGES[type]);
 
 export const isInValid = value => {
-  if (value.trim().length !== BASEBALL_DIGIT)
-    return !setErrorMessage('not-enough-value');
-  if (Number.isNaN(+value)) return !setErrorMessage('invalid-value');
+  if (isCorrectLength(value, BASEBALL_DIGIT)) return !setErrorMessage('not-enough-value');
+  if (!isNumber(value)) return !setErrorMessage('invalid-value');
+  if (isIncludes(value, ZERO_NUMBER)) return !setErrorMessage('included-zero');
   if (isDuplicateLetter(value)) return !setErrorMessage('is-duplicated-value');
   return false;
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,8 @@
 export const BASEBALL_DIGIT = 3;
+export const ZERO_NUMBER = 0;
 export const ERROR_MESSAGES = Object.freeze({
   'not-enough-value': `입력 값을 확인해주세요.\n필요 자리수 : ${BASEBALL_DIGIT}`,
   'invalid-value': '옳지 못한 값입니다.',
+  'included-zero': '0을 제외하고 입력해주세요.',
   'is-duplicated-value': '중복된 값은 입력될 수 없습니다.',
 });
