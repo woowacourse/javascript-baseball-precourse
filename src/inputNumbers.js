f930f87677fa147b4ef5c83cdb4a7cb01258f111
jswith <9geul@naver.com>
2021-11-25 18:02:07 +0900

    feat: 컴퓨터의 3자리 숫자 생성 함수

---
+++ src/inputNumbers.js
@@ -0,0 +1,10 @@
+export const generateComputerInputNumbers = () => {
+  const arr = [];
+  while (arr.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!arr.includes(randomNumber)) {
+      arr.push(randomNumber);
+    }
+  }
+  return arr.join('');
+};
