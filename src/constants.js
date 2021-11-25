354aca00d548a43b90ccfa362ac230847daf4b8c
LAH1203 <lah1203@naver.com>
2021-11-25 09:57:17 +0900

    feat: 게임에 필요한 상수를 작성한 파일 생성

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const BALL = '볼';
+export const STRIKE = '스트라이크';
+export const NOTHING = '낫싱';
+export const SUCCESS = '🎉 정답을 맞추셨습니다! 🎉';
+export const RESTART_TEXT = '게임을 새로 시작하시겠습니까?';
+export const RESTART_BUTTON_TEXT = '게임 재시작';
+export const ALERT_TEXT = '쓰여 있는 조건에 맞게 입력해주세요';
+
+export const GAME_RULE = {
+    answerLength: 3,
+    answerRange: {
+        min: 1,
+        max: 9
+    }
+};
