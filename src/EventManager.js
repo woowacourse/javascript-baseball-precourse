188f7345a657b7fd05dc0ca5a608f565d81d1e68
Jeongmin Woo <greenblues1190@gmail.com>
2021-11-25 17:47:09 +0900

    feat(EventManager): 결과 출력 기능 구현
    - BaseballGame.playWithGeneratedNumber 메소드가 반환한 resultString이 '3스트라이크'면 정답 메시지와 게임 재시작 버튼 출력. 아니면 resultString 출력

--- src/EventManager.js
+++ src/EventManager.js
@@ -1,31 +1,53 @@
 /* eslint-disable func-names */
 import { validateInput } from "./validation.js";
 import { $, $on } from "./helpers.js";
+import {
+  RESULT_CORRECT,
+  MESSAGE_CORRECT_ANSWER,
+  MESSAGE_RESTART_GAME,
+} from "./config.js";
 
 export default function EventManager() {
   this.form = $("form");
+  this.$userInput = $("#user-input");
   this.$submitButton = $("#submit");
+  this.$resultDiv = $("#result");
+  this.$gameRestartButton = document.createElement("button");
+  this.$gameRestartButton.setAttribute("id", "game-restart-button");
+  this.$gameRestartButton.innerText = "재시작";
 
   const handleSubmitForm = function (e) {
     e.preventDefault();
   };
 
   const handleClickSubmitButton = function (baseballGame) {
-    const userInput = document.querySelector("#user-input").value.trim();
-    const validationResult = validateInput(userInput);
+    const userInputNumbers = this.$userInput.value.trim();
+    const validationResult = validateInput(userInputNumbers);
 
     if (!validationResult.result) {
       alert(validationResult.message);
-      document.querySelector("#user-input").value = "";
+      this.$userInput.value = "";
 
       return;
     }
 
-    console.log(baseballGame.playWithGeneratedNumber(userInput));
+    const resultString = baseballGame.playWithGeneratedNumber(userInputNumbers);
+
+    if (resultString === RESULT_CORRECT) {
+      const correctMessageTemplate = `
+        <h4>${MESSAGE_CORRECT_ANSWER}</h4>
+        ${MESSAGE_RESTART_GAME}
+      `;
+
+      this.$resultDiv.innerHTML = correctMessageTemplate;
+      this.$resultDiv.appendChild(this.$gameRestartButton);
+    } else {
+      this.$resultDiv.innerHTML = resultString;
+    }
   };
 
   this.initEventListeners = (arg) => {
     $on(this.form, "submit", handleSubmitForm);
-    $on(this.$submitButton, "click", handleClickSubmitButton.bind(null, arg));
+    $on(this.$submitButton, "click", handleClickSubmitButton.bind(this, arg));
   };
 }
--- src/config.js
+++ src/config.js
@@ -3,7 +3,10 @@ const NUMBER_RANGE_MIN = 1;
 const NUMBER_RANGE_MAX = 9;
 
 // Result Messages
+const RESULT_CORRECT = "3스트라이크";
 const RESULT_NOTHING = "낫싱";
+const MESSAGE_CORRECT_ANSWER = "🎉정답을 맞추셨습니다!🎉";
+const MESSAGE_RESTART_GAME = "게임을 새로 시작하시겠습니까?";
 
 // Error codes
 const ERROR_WRONG_DIGIT = "3자리가 아닙니다.";
@@ -13,7 +16,10 @@ const ERROR_NOT_UNIQUE = "중복된 수가 있습니다.";
 export {
   NUMBER_RANGE_MIN,
   NUMBER_RANGE_MAX,
+  RESULT_CORRECT,
   RESULT_NOTHING,
+  MESSAGE_CORRECT_ANSWER,
+  MESSAGE_RESTART_GAME,
   ERROR_WRONG_DIGIT,
   ERROR_OUT_OF_RANGE,
   ERROR_NOT_UNIQUE,
