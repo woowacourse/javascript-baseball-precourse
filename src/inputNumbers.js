7b8635ebdcb3d0e64a770cc6526345c27e858587
jswith <9geul@naver.com>
2021-11-25 18:35:37 +0900

    feat: 사용자의 숫자를 얻는 함수

--- src/inputNumbers.js
+++ src/inputNumbers.js
@@ -15,3 +15,11 @@ const isValidLength = () => $userInput.value.length === 3;
 const isNumbers = () => !isNaN(Number($userInput.value));
 const isOverlap = () => new Set($userInput.value).size !== 3;
 const isContainZero = () => $userInput.value.includes('0');
+
+export const getUserInputNumbers = () => {
+  if (!isValidLength() || !isNumbers() || isOverlap() || isContainZero()) {
+    alert('🚨 1부터 9까지 서로 다른 3개의 수를 입력해주세요!');
+    return;
+  }
+  return $userInput.value;
+};
