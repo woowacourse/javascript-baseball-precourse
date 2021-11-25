c786ce365f289a7ee626d08b1dfec73a8b048318
hanyoonseok <hys6078@naver.com>
2021-11-25 20:04:16 +0900

    refactor: 필요없는 export 제거 functions/result.js 내 export되지 않는 함수들의 export 제거

--- src/functions/result.js
+++ src/functions/result.js
@@ -2,6 +2,47 @@ import { state } from '../state/index.js';
 import { RESTART_MESSAGE, NOTHING_MESSAGE, CLEAR_MESSAGE } from '../constants/result-message.js';
 import { RESULT_DIV, SUBMIT_BUTTON, USER_INPUT } from '../constants/html-doms.js';
 
+function restartGameSetting() {
+  state.initBallStrike();
+  state.initUserInput();
+  state.makeRandomNumber();
+  RESULT_DIV.innerHTML = '';
+  USER_INPUT.value = '';
+}
+
+function makeRestartButton() {
+  const restartButton = document.createElement('button');
+
+  restartButton.innerText = '게임 재시작';
+  restartButton.id = 'game-restart-button';
+
+  restartButton.addEventListener('click', event => {
+    event.preventDefault();
+    SUBMIT_BUTTON.disabled = false;
+    restartGameSetting();
+  });
+
+  return restartButton;
+}
+
+function makeRestartComponent() {
+  const divTag = document.createElement('div');
+  const restartButton = makeRestartButton();
+
+  divTag.innerHTML = RESTART_MESSAGE;
+
+  divTag.appendChild(restartButton);
+  RESULT_DIV.appendChild(divTag);
+}
+
+export function makeResultDom(resultMessage) {
+  RESULT_DIV.innerHTML = resultMessage;
+  if (state.strikeCount === 3) {
+    SUBMIT_BUTTON.disabled = true;
+    makeRestartComponent();
+  }
+}
+
 export function makeResultMessage() {
   let resultMessage = '';
   if (state.ballCount > 0) {
@@ -18,45 +59,4 @@ export function makeResultMessage() {
   }
 
   return resultMessage;
-}
-
-export function restartGameSetting() {
-  state.initBallStrike();
-  state.initUserInput();
-  state.makeRandomNumber();
-  RESULT_DIV.innerHTML = '';
-  USER_INPUT.value = '';
-}
-
-export function makeRestartButton() {
-  const restartButton = document.createElement('button');
-
-  restartButton.innerText = '게임 재시작';
-  restartButton.id = 'game-restart-button';
-
-  restartButton.addEventListener('click', event => {
-    event.preventDefault();
-    SUBMIT_BUTTON.disabled = false;
-    restartGameSetting();
-  });
-
-  return restartButton;
-}
-
-export function makeRestartComponent() {
-  const divTag = document.createElement('div');
-  const restartButton = makeRestartButton();
-
-  divTag.innerHTML = RESTART_MESSAGE;
-
-  divTag.appendChild(restartButton);
-  RESULT_DIV.appendChild(divTag);
-}
-
-export function makeResultDom(resultMessage) {
-  RESULT_DIV.innerHTML = resultMessage;
-  if (state.strikeCount === 3) {
-    SUBMIT_BUTTON.disabled = true;
-    makeRestartComponent();
-  }
 }
