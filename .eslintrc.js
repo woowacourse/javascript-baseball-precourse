e96f6903573870b68039600449b92a2e0d4ea73f
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-26 07:40:38 +0900

    style: eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['google', 'airbnb'],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+  },
+};
