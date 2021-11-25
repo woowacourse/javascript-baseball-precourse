21ff07ea70b9239d68de22a1613922dddaea53ec
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-25 21:29:00 +0900

    feat: 사용자가 값 입력 후 엔터 및 버튼 클릭 처리
    변경사항:
    1. dom.js 파일 생성 후 query 선택 함수 생성
    2. onSubmitPlayerInput 함수 생성 후 제출 된 값 확인

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import { pickUniqueThreeNumbers } from './utils/index.js';
+import { $ } from './utils/dom.js';
 
 function BaseballGame () {
   this.answer = pickUniqueThreeNumbers();
 
   this.init = () => {
-    console.log(this.answer);
-  }
+    initEventListeners();
+  };
+
+  const initEventListeners = () => {
+    $('form').addEventListener('submit', onSubmitPlayerInput);
+  };
+
+  const onSubmitPlayerInput = (event) => {
+    event.preventDefault();
+    const input = $('#user-input').value;
+    console.log(`제출된 값: ${input}`);
+  };
 }
 
 const baseballgame = new BaseballGame();
