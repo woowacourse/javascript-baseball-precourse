890204db8d4376454538fd36f86181fc1893239a
InKyoJeong <viapolar@naver.com>
2021-11-26 00:07:13 +0900

    refactor: modify input valid function
    - 사용자 인풋을 검증하는 함수를 수정

--- src/valid.js
+++ src/valid.js
@@ -1,20 +1,27 @@
 import { ERROR, NUM } from "./constants.js";
 
+const isInvalidNumber = (input) => {
+  return input.includes(NaN) || input.includes(0);
+};
+
+const isExceedLength = (input) => {
+  return input.length !== NUM.MAX_LENGTH;
+};
+
+const isDuplicatedNumber = (input) => {
+  return new Set([...input]).size !== NUM.MAX_LENGTH;
+};
+
 export const isValidUserInput = (input) => {
-  let isValid = true;
-
-  if (
-    input.length !== NUM.MAX_LENGTH ||
-    input.includes(0) ||
-    input.includes(NaN)
-  ) {
+  if (isInvalidNumber(input) || isExceedLength(input)) {
     alert(ERROR.NUMBER_IS_NOT_CORRECT);
-    isValid = false;
-  }
-  if (new Set([...input]).size !== NUM.MAX_LENGTH) {
-    alert(ERROR.NUMBER_IS_DUPLICATED);
-    isValid = false;
+    return false;
   }
 
-  return isValid;
+  if (isDuplicatedNumber(input)) {
+    alert(ERROR.NUMBER_IS_DUPLICATED);
+    return false;
+  }
+
+  return true;
 };
