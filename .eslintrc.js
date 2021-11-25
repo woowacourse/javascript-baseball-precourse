881300a74fc524c5416eaa89af4b711faf31703a
InSeongSo <goflvhxj2547@gmail.com>
2021-11-25 10:58:41 +0900

    🔬Chore: 의존성 삭제, eslint 설정 변경
    1. @woowacourse/mission-utils cdn으로 변경 : 타입스크립트 lint와 충돌남으로 eslint-disable로 진행
    2. 출력길이 80 > 100으로 조정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,12 @@ module.exports = {
     es2021: true,
     node: true, // 'module' is not defined. 에러가 발생하면 기입
   },
-  extends: [
-    'eslint:recommended',
-    'airbnb',
-    'plugin:cypress/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'airbnb', 'plugin:cypress/recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
     'import/prefer-default-export': 'off',
     'import/extensions': ['off'],
+    'class-methods-use-this': 'off',
     'prettier/prettier': [
       'error',
       {
@@ -22,7 +18,7 @@ module.exports = {
         useTabs: false,
         tabWidth: 2,
         trailingComma: 'all',
-        printWidth: 80,
+        printWidth: 100,
         bracketSpacing: true,
         arrowParens: 'avoid',
         endOfLine: 'auto',
