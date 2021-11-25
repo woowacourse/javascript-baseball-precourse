51b24fd5562af0d361ddf355c72c3a64cd6e7649
mangming2 <leejiho35@gmail.com>
2021-11-25 23:19:46 +0900

    [Update]: index.js
    기본적인 기능 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,26 +1,130 @@
-import MissionUtils from "@woowacourse/mission-utils";
 export default function BaseballGame() {
   const btnSubmit=document.querySelector('#submit');
   const userInput = document.querySelector("#user-input");
   const result = document.querySelector("#result");
   let computerInputNumbers=[];
 
+
+
+  //컴퓨터에서 난수생성
   const generateComNumber=()=>{
-    computerInputNumbers= MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    return computerInputNumbers
+    let newnumbers=[]
+    
+    while(newnumbers.length!=3){
+      let newnum=MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!newnumbers.includes(newnum)){
+        newnumbers.push(newnum);
+       }
+
+      }
+  return newnumbers;
   }
 
+  //유저가 입력한 수 가져오기
+  const getUserNumber=()=>{
+    let usernumber=userInput.value.split('').map(Number);
+    return usernumber;
+  }
+//유저의 입력 유효성 판단
+  const validateUser=()=>{
+    let validatenum=getUserNumber();
+    let valid=''
+    let setvalid=new Set(validatenum);
+    if(validatenum.length!=setvalid.size){
+      valid='중복없이입력ㄱ'
+    }
+    validatenum.forEach(num=>{
+      if(isNaN(num)){
+        valid='숫자를 입력하세요'
+      }
 
-  //this.play = function (computerInputNumbers, userInputNumbers) {
-    //return "결과 값 String";
-    //};
+    }
+    )
+    if (validatenum.length>3){
+      valid='범위에 알맞게 입력하세요'
+    }
+    
+    return valid
+  }
+
+//strike ball 낫싱 판단
+  const getHint=(computerInputNumbers, userInputNumbers)=>{
+    let strikeCount=0;
+    let ballCount=0;
+    userInputNumbers.forEach((num,index)=>{
+      
+      if(num==computerInputNumbers[index]){
+        strikeCount++;
+      }
+      else if(computerInputNumbers.includes(num)){
+        ballCount++;
+      }
+    }
+    )
+    console.log(strikeCount)
+    return [strikeCount,ballCount];
+  }
+//결과 화면에 출력
+const getResult=(answer)=>{
+  if(answer=="정답"){
+    result.innerHTML='<strong>정답입니다</strong>'
+
+  }
+  else{
+    result.innerHTML=answer
+  }
+
+}
+
+  const play = function (computerInputNumbers, userInputNumbers) {
+    
+    let [strike,ball]=getHint(computerInputNumbers,userInputNumbers);
+    let answer="";
+    if (ball>0){
+      answer+=`${ball}볼`
+    }
+    if (strike>0){
+      answer+=`${strike}스트라이크`
+    }  
+    if (ball === 0 && strike === 0) {
+      answer= "낫싱";
+    }
+    if (strike==3){
+      answer="정답"
+    }
+    
+    console.log(strike)
+    return answer;
+    };
   
+  btnSubmit.addEventListener("click",()=>{
+      let usernumber=getUserNumber();
+      let valid=validateUser();
+      if (valid==''){
+        console.log(usernumber);
+        console.log(computerInputNumbers);
+        console.log(validateUser());
+        console.log(play(computerInputNumbers,usernumber))
+        getResult(play(computerInputNumbers,usernumber))
+      }else{
+        alert(valid)
+      }
+      
+    })
 
 
-
-
-  computerInputNumbers=generateComNumber();
-  console.log(computerInputNumbers)
+  const init=()=>{
+    resetValue();
+    computerInputNumbers=generateComNumber();
+    console.log(computerInputNumbers);
+    
+  }  
+  const resetValue=()=>{
+    result.innerHTML='';
+    userInput.value='';
+  }
+  
+  init();
 }
 
 BaseballGame();
