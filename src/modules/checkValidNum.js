b291ad414776bba4bf01c424c679a4fb1ccd0c84
liswkjts <liswktjs@gmail.com>
2021-11-25 23:15:17 +0900

    fix : (checkValidNum) 🐛 사용자가 0을 입력했을 때 경우에 대한 대처 놓쳤던 사용자가 0을 입력했을 때에 대해서 return false를 하도록 대처 하였다 checkValidNum.js

--- src/modules/checkValidNum.js
+++ src/modules/checkValidNum.js
@@ -9,6 +9,12 @@ export default function checkValidNum(userInputNum){
     if ([...setUserInput].length !== userInputNum.length){
         return false 
     }
+    //1~9사이의 숫자를 입력하지 않았을 때 (=0을 입력했을 때)
+    for(let i =0; i < userInputNum.length; i ++){
+        if(Number(userInputNum[i]) === 0 ){
+            return false;
+        }
+    }
     //앞의 조건들을 통과했을 때 return true
     return true
 }  
