9339e3c5001f20ac277a6f11c4cfb9abd9722364
liswkjts <liswktjs@gmail.com>
2021-11-25 19:14:13 +0900

    feat : (getUserNumbers) ⚡user가 입력한 값 받아오기 index에서 submit버튼을 클릭시 playGame 메서드 실행하도록 조치, playGame에서는 외부 모듈에서 받아온 유저 값과 컴퓨터의 값을 play함수에 넘겨줌 index.js/dom.js/getUserNumber.js/README.md

---
+++ src/dom/dom.js
@@ -0,0 +1,2 @@
+//편하게 dom에 접근하기 위한 함수
+export const $ = (selector) => document.querySelector(selector);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
+import {$} from './dom/dom.js';
 import getComputerNumbers from "./modules/getComputerNumbers.js";
-
+import getUserNumbers from './modules/getUserNumbers.js';
 
 export default function BaseballGame() {
-
-  const computerInputNumbers = getComputerNumbers();
-  this.play = function (computerInputNumbers, userInputNumbers) {
-
+  //user가 입력한 값과, 컴퓨터가 생성한 값들을 담을 객체
+  this.gameInfoObject = {
+    computerInputNumbers : [],
+    userInputNumbers: [],
+  }
+  
+  //init 초기화 작업 
+  this.init = () => {
+    this.gameInfoObject.computerInputNumbers = getComputerNumbers();
+    initEventListener();
+  }
+  
+  this.play = function (computerInputNumbers, userInputNumbers) {    
+      console.log(computerInputNumbers, userInputNumbers);
       return "결과 값 String";
   };
+
+  const playGame = () => {
+      //유저가 입력한 값 받아오기
+      this.gameInfoObject.userInputNumbers = getUserNumbers($('#user-input').value);
+      this.play(this.gameInfoObject.computerInputNumbers, this.gameInfoObject.userInputNumbers);
+  }
+  
+  const initEventListener = () => {
+    $('#app form').addEventListener("submit",(e) =>{
+      e.prevetDefault();
+    })
+    //user가 submit버튼 클릭시 playGame 함수 실행 
+    $('#submit').addEventListener("click", playGame);
+  }
 }
 
-new BaseballGame();
+const baseballGamePlay = new BaseballGame();
+baseballGamePlay.init();
