1085caca892bc6b6d0e7194adafbbb1636d79e10
InSeongSo <goflvhxj2547@gmail.com>
2021-11-25 12:52:52 +0900

    ✒️Feat: play 기능 추가
    1. play 기능을 추가하고 조건문에 return문을 넣는 것으로 수정한다.
    2. 생성자 함수에 exclude 프로퍼티를 추가한다.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 /* eslint-disable no-undef */
 import { $, isInValid } from './common/index.js';
 import BaseballGame from './components/index.js';
-import { BASEBALL } from './constants/index.js';
+import { BASEBALL, EMPTY } from './constants/index.js';
 
-const baseball = new BaseballGame({
+const $input = $('#user-input');
+const $button = $('#submit');
+
+const game = new BaseballGame({
   digit: BASEBALL.DIGIT,
+  exclude: BASEBALL.ZERO,
   start: BASEBALL.START,
   end: BASEBALL.END,
 });
 
-const $input = $('#user-input');
 $input.setAttribute('maxLength', BASEBALL.DIGIT * 2);
 
-const $button = $('#submit');
 /* eslint-disable consistent-return */
 $button.addEventListener('click', event => {
   event.preventDefault();
   const { value } = $input;
   $input.focus();
-  if (isInValid(value)) $input.value = '';
+  if (isInValid(value)) {
+    $input.value = EMPTY;
+    return;
+  }
+  game.play(value);
 });
