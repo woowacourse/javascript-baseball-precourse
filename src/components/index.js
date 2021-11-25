5734e0cd18a04deee1769e453e1b27116b3b048c
InSeongSo <goflvhxj2547@gmail.com>
2021-11-25 12:49:57 +0900

    ✒️Feat: 게임을 실행하는 메서드
    1. play 메서드를 추가하고 컴퓨터의 타겟 넘버와 비교하여 결과를 확인한다.
    2. 생성자 파라미터에 exclude 프로퍼티를 추가하여 예외로 둘 숫자를 지정한다.

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,52 @@
 import { isNumberObject } from '../common/index.js';
+import { BASEBALL, EMPTY } from '../constants/index.js';
 
 /* eslint-disable no-undef */
 const { pickNumberInRange } = MissionUtils.Random;
 
 export default class BaseballGame {
   constructor(state) {
-    const { digit, start, end } = isNumberObject(state);
+    const { digit, exclude, start, end } = isNumberObject(state);
+
     this.digit = digit;
-    this.computerInputNumbers = this.getRandomNumbers(start, end);
-    console.log(this.computerInputNumbers);
+    this.exclude = exclude;
+    this.start = start;
+    this.end = end;
+
+    this.computerInputNumbers = this.getRandomNumbers(this.start, this.end);
   }
 
-  getRandomNumbers(startNumber, endNumber) {
+  getRandomNumbers() {
     const result = new Set();
+    /* eslint-disable no-constant-condition */
     while (true) {
-      result.add(pickNumberInRange(startNumber, endNumber));
-      if (result.size === this.digit) return [...result].join('');
+      result.add(pickNumberInRange(this.start, this.end));
+      if (result.size === this.digit) return [...result].join(EMPTY);
     }
+  }
+
+  checkCorrectNumber(userInputNumbers) {
+    const targetNumbers = this.computerInputNumbers.split(EMPTY);
+    const restNumbers = targetNumbers.reduce((numbers, value, index) => {
+      if (userInputNumbers[index] !== value) numbers.push(userInputNumbers[index]);
+      return numbers;
+    }, []);
+
+    const { length: MISMATCH } = restNumbers;
+    const SIMILAR = new Set([...restNumbers, ...targetNumbers]).size;
+
+    return [this.digit - MISMATCH, this.digit - SIMILAR + MISMATCH];
+  }
+
+  play(userInputNumbers) {
+    let result = EMPTY;
+
+    const [STRIKE, BALL] = this.checkCorrectNumber(userInputNumbers);
+
+    if (STRIKE === this.digit) result = BASEBALL.WIN;
+    else if (STRIKE + BALL === this.exclude) result = BASEBALL.NOTING;
+    else result = `${BALL ? `${BALL}볼` : EMPTY} ${STRIKE ? `${STRIKE}스트라이크` : EMPTY}`.trim();
+
+    console.log(result);
   }
 }
