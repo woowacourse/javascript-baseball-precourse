9915bd749590cf393a8480ccf183a16b01288b88
jswith <9geul@naver.com>
2021-11-25 20:37:41 +0900

    refactor: 모듈 분리

---
+++ src/computerInputNumbers.js
@@ -0,0 +1,12 @@
+const generateComputerInputNumbers = () => {
+  const arr = [];
+  while (arr.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!arr.includes(randomNumber)) {
+      arr.push(randomNumber);
+    }
+  }
+  return arr.join('');
+};
+
+export default generateComputerInputNumbers;
--- src/userInputNumbers.js
+++ src/userInputNumbers.js
@@ -1,25 +1,16 @@
 import { $userInput } from './constants.js';
 
-export const generateComputerInputNumbers = () => {
-  const arr = [];
-  while (arr.length < 3) {
-    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!arr.includes(randomNumber)) {
-      arr.push(randomNumber);
-    }
-  }
-  return arr.join('');
-};
-
 const isValidLength = () => $userInput.value.length === 3;
 const isNumbers = () => !isNaN(Number($userInput.value));
 const isOverlap = () => new Set($userInput.value).size !== 3;
 const isContainZero = () => $userInput.value.includes('0');
 
-export const getUserInputNumbers = () => {
+const getUserInputNumbers = () => {
   if (!isValidLength() || !isNumbers() || isOverlap() || isContainZero()) {
     alert('🚨 1부터 9까지 서로 다른 3개의 수를 입력해주세요!');
     return;
   }
   return $userInput.value;
 };
+
+export default getUserInputNumbers;
