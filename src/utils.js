602d2b52721c1c6c295dc0722cb8ee03203ce686
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-11-26 23:41:57 +0900

    Style: 코드 패턴 통일

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,15 @@
-const $ = (selector) => document.querySelector(selector);
+export const $ = (selector) => document.querySelector(selector);
 
-const createElement = (tagName, tagText) => {
+export function createElement(tagName, tagText) {
     const $create = document.createElement(tagName);
     $create.innerText = tagText;
 
     return $create;
-};
+}
 
-const combineElement = (elements) => {
+export function combineElement(elements) {
     const $fragment = document.createDocumentFragment();
     elements.forEach((element) => $fragment.append(element));
 
     return $fragment;
-};
-
-export { $, createElement, combineElement };
+}
