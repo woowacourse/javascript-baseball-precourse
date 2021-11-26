105bb585de485d510cff26dab3e231b769064a45
YUNHO KANG <ansghkdbsgh@gmail.com>
2021-11-26 16:16:51 +0900

    feat: get number from user if button clicked

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
+export default function BaseballGame() {
 
-//console.log(pickThreeDigitRandomNumber());
-pickThreeDigitRandomNumber();
-
-function isSetLengthUnderThree(setA){
-    if (setA.size < 3){
-        return true;
+    /* pickThreeDigitRandomNumber(); */
+    function isSetLengthUnderThree(setA){
+        if (setA.size < 3){
+            return true;
+        }
+        return false;
     }
-    return false;
+
+    function convertSetToString(setA){
+        let strArray = [];
+        for(let i of setA){
+            strArray.push(i);
+        }
+        return(strArray.join(""));
+    }
+
+    function getThreeDigitRandomNumber(){
+        const setA = new Set;
+        while(isSetLengthUnderThree(setA)){
+            setA.add(MissionUtils.Random.pickNumberInRange(1,9));
+        }
+        return convertSetToString(setA);
+    }
+
+    const computerInputNumbers = getThreeDigitRandomNumber();
+    console.log(computerInputNumbers);
+
+    /*get user number*/
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', submitButtonOn);
+
+    //TODO Check User Num Is Valid
+    function submitButtonOn(){
+        let usernumber = getUserNum();
+    }
+
+    function getUserNum(){
+        let userInput = document.getElementById('user-input').value;
+        return userInput;
+    }
+
 }
 
-function pickThreeDigitRandomNumber(){
-    const setA = new Set;
-    while(isSetLengthUnderThree(setA)){
-        setA.add(MissionUtils.Random.pickNumberInRange(1,9));
-    }
-    console.log(setA);
-}
+new BaseballGame();
