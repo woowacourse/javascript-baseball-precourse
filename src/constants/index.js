00c02a9b81a69b2af29d9fd55f1fad85be383aab
최광희 <polyckh@gmail.com>
2021-11-26 18:08:06 +0900

    feat: 사용자의 입력 값을 랜덤 넘버와 비교

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
 export const VALID_NUMBER_LENGTH = 3;
 export const MIN_RANDOM_NUMBER = 1;
 export const MAX_RANDOM_NUMBER = 9;
+export const BALL = '볼';
+export const STRIKE = '스트라이크';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { $ } from './util/dom.js';
-import { MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER } from './constants/index.js';
+import {
+  MAX_RANDOM_NUMBER,
+  MIN_RANDOM_NUMBER,
+  BALL,
+  STRIKE,
+} from './constants/index.js';
 import { getRandomNumber, isValidInput } from './number.js';
 
 const $userInput = $('#user-input');
@@ -8,22 +13,44 @@ const $form = $('form');
 
 function BaseballGame() {
   this.randomNumber;
-  this.userInput;
+  this.score = {
+    [BALL]: 0,
+    [STRIKE]: 0,
+  };
 
   this.init = () => {
     initEventListener();
     setRandomNumber();
   };
 
+  this.play = (computerInputNumbers, userInputNumbers) => {
+    clearScore();
+    setScore(computerInputNumbers, userInputNumbers);
+  };
+
+  const setScore = (computerInputNumbers, userInputNumbers) => {
+    [...userInputNumbers].forEach((number, idx) => {
+      if (computerInputNumbers[idx] === number) {
+        this.score[STRIKE] += 1;
+      } else if (computerInputNumbers.includes(number)) {
+        this.score[BALL] += 1;
+      }
+    });
+    console.log(this.score);
+  };
+  const clearScore = () => {
+    this.score[BALL] = 0;
+    this.score[STRIKE] = 0;
+  };
   const setUserInput = () => {
-    const input = getUserInput();
-    if (!isValidInput(input)) {
+    const userInput = getUserInput();
+    if (!isValidInput(userInput)) {
       $userInput.value = '';
       $userInput.focus();
       alert('서로 다른 3자리 수를 입력해 주세요.');
       return;
     }
-    this.userInput = input;
+    this.play(this.randomNumber, userInput);
   };
   const getUserInput = () => $userInput.value;
 
