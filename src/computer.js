b6a9b9afcfdb037230c10bbf83d77fe7594fbefd
byhhh2 <rlaalsdl111@gmail.com>
2021-11-26 20:20:55 +0900

    fix: fix test error when getting random number

--- src/computer.js
+++ src/computer.js
@@ -1,3 +1,29 @@
 export const ramdomAnswer = () => {
-  return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('');
+  let randomNum = getRandomNum();
+
+  while (
+    isDuplicated(randomNum.firstNum, randomNum.secondNum, randomNum.thirdNum)
+  ) {
+    randomNum = getRandomNum();
+  }
+
+  return '' + randomNum.firstNum + randomNum.secondNum + randomNum.thirdNum;
+};
+
+const getRandomNum = () => {
+  const randomNum = {
+    firstNum: MissionUtils.Random.pickNumberInRange(1, 9),
+    secondNum: MissionUtils.Random.pickNumberInRange(1, 9),
+    thirdNum: MissionUtils.Random.pickNumberInRange(1, 9),
+  };
+
+  return randomNum;
+};
+
+const isDuplicated = (firstNum, secondNum, thirdNum) => {
+  if (firstNum == secondNum || secondNum == thirdNum || firstNum == thirdNum) {
+    return true;
+  }
+
+  return false;
 };
