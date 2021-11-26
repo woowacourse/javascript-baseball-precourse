a9cdfa174283f801a1d708a235c729ad803e2ea7
byhhh2 <rlaalsdl111@gmail.com>
2021-11-26 21:16:56 +0900

    refactor: extract function that alert to user

--- src/exception.js
+++ src/exception.js
@@ -8,25 +8,20 @@ import {
 } from './constants.js';
 
 export const userInputException = (userInputNumbers) => {
-  let isException = true;
-
-  if (!userInputNumbers || isNaN(Number(userInputNumbers))) {
-    alert(NAN_ALERT);
-  } else if (userInputNumbers.includes(' ')) {
-    alert(BLANK_SPACE_ALERT);
-  } else if (userInputNumbers.includes('-')) {
-    alert(NEGATIVE_NUMBER_ALERT);
-  } else if (userInputNumbers.length !== 3) {
-    alert(WRONG_LENGTH_ALERT);
-  } else if (!checkDigitsRangeCorrect(userInputNumbers)) {
-    alert(OUT_RANGE_ALERT);
-  } else if (isDuplicated(userInputNumbers)) {
-    alert(DUPLICATED_NUMBER_ALERT);
-  } else {
-    isException = false;
-  }
+  let isException = alertToUser(userInputNumbers);
 
   return isException;
+};
+
+const alertToUser = (userInputNumbers) => {
+  if (!userInputNumbers || isNaN(Number(userInputNumbers))) alert(NAN_ALERT);
+  else if (userInputNumbers.includes(' ')) alert(BLANK_SPACE_ALERT);
+  else if (userInputNumbers.includes('-')) alert(NEGATIVE_NUMBER_ALERT);
+  else if (userInputNumbers.length !== 3) alert(WRONG_LENGTH_ALERT);
+  else if (!checkDigitsRangeCorrect(userInputNumbers)) alert(OUT_RANGE_ALERT);
+  else if (isDuplicated(userInputNumbers)) alert(DUPLICATED_NUMBER_ALERT);
+  else return false;
+  return true;
 };
 
 const checkDigitsRangeCorrect = (userInputNumbers) => {
