db8de44639da9c6975f78c6dc285aa963d00ad04
leegwae <leegwae@gmail.com>
2021-11-27 08:46:26 +0900

    fix: 요구된 mission util 메서드 사용하여 테스트 통과

--- src/library/generateComputerInput.js
+++ src/library/generateComputerInput.js
@@ -3,7 +3,15 @@ import { NUMBER_MIN, NUMBER_MAX, LENGTH_REQUIRED } from '../consts.js';
 const { MissionUtils } = window;
 
 function generateComputerInput() {
-  return MissionUtils.Random.pickUniqueNumbersInRange(NUMBER_MIN, NUMBER_MAX, LENGTH_REQUIRED).join('');
+  const numbers = [];
+
+  while (numbers.length < LENGTH_REQUIRED) {
+    const number = MissionUtils.Random.pickNumberInRange(NUMBER_MIN, NUMBER_MAX);
+
+    if (!numbers.includes(number)) numbers.push(number);
+  }
+
+  return numbers.join('');
 }
 
 export default generateComputerInput;
