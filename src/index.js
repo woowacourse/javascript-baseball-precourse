caad7b06fefa5abc67fc28d06fc4c3b8d5ee8200
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-11-26 18:31:49 +0900

    Rename(userInput): 디렉토리명 변경

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import UserInputModel from "./input-checker/model.js";
-import UserInputView from "./input-checker/view.js";
+import UserInputModel from "./user-input/model.js";
+import UserInputView from "./user-input/view.js";
 
 import GameRuleModel from "./game-manager/model.js";
 import GameRuleView from "./game-manager/view.js";
