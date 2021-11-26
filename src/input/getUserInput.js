d9cabe7ab45ce508a1a613037d6f67d3b2ece5a0
DomMorello <zuzudnf@gmail.com>
2021-11-26 18:18:41 +0900

    test: 사용자 입력 유효성 검사 테스트

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -2,11 +2,12 @@ import isValidInput from "./isValidInput.js";
 
 export default function getUserInput() {
   const $userSubmitButton = document.querySelector("#submit");
+
   $userSubmitButton.addEventListener("click", event => {
     event.preventDefault(); // 개발을 위해서 잠시 페이지 새로고침을 일단 막아둠
     const userInputValue = document.querySelector("#user-input").value;
-    if (isValidInput(userInputValue.split("").map(element => +element))) {
-      console.log("valid");
+    if (isValidInput(userInputValue.split('').map((elem) => +elem))) {
+      console.log("valid!");
     }
   });
 }
