940a17871a5f5d6bc2a7bbe533887ff1e1647938
sohyeonyun <wja06018@naver.com>
2021-11-26 09:18:58 +0900

    fix: get user input number

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ export default class BaseballGame {
     this.userInputText = document.querySelector('#user-input');
     this.submitButton = document.querySelector('#submit');
     this.gameResult = document.querySelector('#result');
+
+    this.submitButton.addEventListener('click', () => this.startGame());
   }
 
   setComputerNumber() {
@@ -17,6 +19,39 @@ export default class BaseballGame {
     }
     console.log(`computerInput: ${computerInputNumbers.join('')}`);
     return computerInputNumbers.join('');
+  }
+
+  isValidNumber(inputValue) {
+    if (inputValue.length !== 3) {
+      return false;
+    }
+    if (isNaN(inputValue)) {
+      return false;
+    }
+    return true;
+  }
+
+  getUserInput() {
+    const inputValue = this.userInputText.value;
+    if (this.isValidNumber(inputValue)) {
+      return inputValue;
+    }
+    alert(`error input`);
+    return null;
+  }
+
+  setUserInputClean() {
+    this.userInputText.value = '';
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
+    console.log(`${computerInputNumbers} vs. ${userInputNumbers}`);
+    return '결과 값 String';
+  }
+
+  startGame() {
+    this.play(this.computerInputNumbers, this.getUserInput());
+    this.setUserInputClean();
   }
 }
 
