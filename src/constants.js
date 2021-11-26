3ac9804d40b2011f9de7529544f074f482988bbc
ziho <ziho@kakao.com>
2021-11-26 09:37:43 +0900

    feat: 결과 메세지, 숫자 규칙 상수 정의

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const RESULT_MESSAGE = Object.freeze({
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+  CORRECT: '🎉정답을 맞추셨습니다!🎉',
+  RESTART: '게임을 새로 시작하시겠습니까?',
+  ZERO_INCLUDED: '1부터 9까지의 숫자만 입력해주세요.',
+  NOT_A_NUMBER: '숫자만 입력해주세요.',
+  NUMBER_DUPLICATED: '서로 다른 숫자를 입력해주세요.',
+  THREE_DIGIT: '3자리 수를 입력해주세요.',
+});
+
+export const NUMBER_RULES = Object.freeze({
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 9,
+  LENGTH: 3,
+})
