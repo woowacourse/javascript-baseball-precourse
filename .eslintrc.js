22c0a5642b70d66bf291e97519120e0aa999b8f3
leegwae <leegwae@gmail.com>
2021-11-27 03:33:33 +0900

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-tabs': 'off',
+  },
+};
