7e2c519dce01b735370da294248dddf044c3bd54
liswkjts <liswktjs@gmail.com>
2021-11-26 10:44:06 +0900

    feat: (play) ⚡strikeCount와 ballCount에 따른 결과값 String 생성 getPlayResultCount 메서드를 통해서 받아온 스트라이크 개수와 볼 개수에 따라서 낫싱, 스트라이크 , 볼 결과가 담긴 String을 play메서드에서 반환한다 index.js / README.md

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {$} from './dom/dom.js';
 import getComputerNumbers from "./modules/getComputerNumbers.js";
 import getUserNumbers from './modules/getUserNumbers.js';
+import getPlayResultCount from './modules/getPlayResultCount.js';
 
 export default function BaseballGame() {
   //user가 입력한 값과, 컴퓨터가 생성한 값들을 담을 객체
@@ -16,18 +17,33 @@ export default function BaseballGame() {
   }
   
   this.play = function (computerInputNumbers, userInputNumbers) {    
-      console.log(computerInputNumbers, userInputNumbers);
-      return "결과 값 String";
+      const result = getPlayResultCount(computerInputNumbers, userInputNumbers);
+      const ballCount = result[0];
+      const strikeCount = result[1];
+      if(ballCount === 0 && strikeCount === 0){
+        return "낫싱"
+      }
+      if(ballCount !== 0 && strikeCount !== 0){
+        return `${ballCount}볼 ${strikeCount}스트라이크`;
+      }
+      if(ballCount !== 0 && strikeCount === 0){
+        return `${ballCount}볼`;
+      }
+      return `${strikeCount}스트라이크`;
   };
 
   const playGame = () => {
       //유저가 입력한 값 받아오기
       this.gameInfoObject.userInputNumbers = getUserNumbers($('#user-input').value);
-      this.play(this.gameInfoObject.computerInputNumbers, this.gameInfoObject.userInputNumbers);
+      if(this.gameInfoObject.userInputNumbers !== ""){
+        let compareResult = this.play(this.gameInfoObject.computerInputNumbers, this.gameInfoObject.userInputNumbers);
+        console.log(compareResult,this.gameInfoObject.computerInputNumbers, this.gameInfoObject.userInputNumbers);
+      }
+      return;
   }
   
   const initEventListener = () => {
-    $('#app form').addEventListener("submit",(e) =>{
+    $('#user-input-form').addEventListener("submit",(e) =>{
       e.prevetDefault();
     })
     //user가 submit버튼 클릭시 playGame 함수 실행 
