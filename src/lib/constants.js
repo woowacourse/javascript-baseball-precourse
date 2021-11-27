241512870daaaa8cac2c354f81e464e27b9dc589
junzzi <jjh01027717502@gmail.com>
2021-11-28 02:05:20 +0900

    feat: add Constants

---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+export const NOTHING = "낫싱";
+export const STRIKE = "스트라이크";
+export const BALL = "볼";
+export const FINISH =
+  "🎉 <b>정답을 맞추셨습니다.</b> 🎉 <br/> <br/> 게임을 다시 시작하시겠습니까?";
+export const PLAIN_TEXT = "";
