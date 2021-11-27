5c5c8c74afc3a1eeda0defe233ce31e27d398c65
liswkjts <liswktjs@gmail.com>
2021-11-27 12:21:39 +0900

    fix : (printGamePlayResult) 🐛 필요없는 부분 제거 유저가 답을 맞춰 게임이 끝나는 경우의 수는 index.js에서 처리를 해주었기 때문에 printGamePlayResult함수에서는 해당 처리 부분을 삭제한다 printGamePlayResult.js

--- src/modules/printGamePlayResult.js
+++ src/modules/printGamePlayResult.js
@@ -1,10 +1,5 @@
 import { $ } from "../dom/dom.js";
 
 export default function printGamePlayResult(gameResult){
-    if(gameResult === 'gameEnd'){
-        printGameEnd();
-    }else if(gameResult !== 'gameEnd'){
-        $('#result').innerText = `${gameResult}`;
-    }
-    
+    $('#result').innerText = `${gameResult}`;
 }
