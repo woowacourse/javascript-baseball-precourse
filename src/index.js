f57f9eb9cc2f7ffadfc5229c0c58bfff64d0d7ed
koowoy <uk960214@gmail.com>
2021-11-27 22:53:18 +0900

    feat(game class): add game class at index.js
    - create game class, initiated with random answer generated
    - create play method at game class
    - input value valditaion moved inside play method
    - connect input event listener to separate function
    - create event handler function that calls play method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import generateAnswer from "./generateAnswer.js";
-import handleInputSubmit from "./handleInputSubmit.js";
+import validateInput from "./validateInput.js";
 
-let answer = generateAnswer();
+// define game class
+export default class BaseballGame {
+  constructor() {
+    // generate answer on class initiation
+    this.answer = generateAnswer();
+  }
+
+  // define play method
+  play(computerInputNumbers, userInputNumbers) {
+    if (!validateInput(userInputNumbers)) return;
+  }
+}
+
+// initiate game
+let game = new BaseballGame();
+
+// handle user input
+function handleUserInput() {
+  // get user input value
+  let inputValue = document.querySelector("#user-input").value;
+  game.play(game.answer, inputValue);
+}
 
 // get user submit button
 let inputButton = document.querySelector("#submit");
 
-inputButton.addEventListener("click", handleInputSubmit);
+// user submit button event listener
+inputButton.addEventListener("click", handleUserInput);
