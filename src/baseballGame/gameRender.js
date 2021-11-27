b036513533aa9c7a79649c9b6747cc3a1cfee097
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-11-27 15:49:34 +0900

    Refactor: 오류 메시지 출력 부분 구조 개선

--- src/baseballGame/gameRender.js
+++ src/baseballGame/gameRender.js
@@ -2,13 +2,19 @@ import { $, createElement, combineElement } from "../utils.js";
 import { RESULT_CODE, RESULT_TEXT, ALERT_MESSAGE } from "../data/constants.js";
 import { $result, $userInput, $submit } from "../data/elements.js";
 
-export function errorMessage(erroCode) {
-    let alertText = "";
-    if (erroCode === RESULT_CODE.ERROR_USERINPUT_LENGTH) alertText = ALERT_MESSAGE.ERROR_USERINPUT_LENGTH;
-    else if (erroCode === RESULT_CODE.ERROR_USERINPUT_UNIQUE_WORD) alertText = ALERT_MESSAGE.ERROR_USERINPUT_UNIQUE_WORD;
-    else if (erroCode === RESULT_CODE.ERROR_USERINPUT_NUMBER_RANGE) alertText = ALERT_MESSAGE.ERROR_USERINPUT_NUMBER_RANGE;
+export function errorMessage(ERROR_TYPE) {
+    switch (ERROR_TYPE) {
+        case RESULT_CODE.ERROR_USERINPUT_LENGTH:
+            alert(ALERT_MESSAGE.USERINPUT_LENGTH);
+            break;
+        case RESULT_CODE.ERROR_USERINPUT_UNIQUE_WORD:
+            alert(ALERT_MESSAGE.USERINPUT_UNIQUE_WORD);
+            break;
+        case RESULT_CODE.ERROR_USERINPUT_NUMBER_RANGE:
+            alert(ALERT_MESSAGE.USERINPUT_NUMBER_RANGE);
+            break;
+    }
 
-    alert(alertText);
     $userInput.focus();
 }
 
--- src/data/constants.js
+++ src/data/constants.js
@@ -21,7 +21,7 @@ export const RESULT_TEXT = {
 };
 
 export const ALERT_MESSAGE = {
-    ERROR_USERINPUT_LENGTH: "3글자로 입력하여주세요.\n(1~9 숫자, 중복 없는 수 3자리)",
-    ERROR_USERINPUT_NUMBER_RANGE: "1에서 9 사이의 숫자만 입력할 수 있습니다.\n(1~9 숫자, 중복 없는 수 3자리)",
-    ERROR_USERINPUT_UNIQUE_WORD: "중복된 숫자가 있습니다.\n(1~9 숫자, 중복 없는 수 3자리)",
+    USERINPUT_LENGTH: "3글자로 입력하여주세요.\n(1~9 숫자, 중복 없는 수 3자리)",
+    USERINPUT_NUMBER_RANGE: "1에서 9 사이의 숫자만 입력할 수 있습니다.\n(1~9 숫자, 중복 없는 수 3자리)",
+    USERINPUT_UNIQUE_WORD: "중복된 숫자가 있습니다.\n(1~9 숫자, 중복 없는 수 3자리)",
 };
