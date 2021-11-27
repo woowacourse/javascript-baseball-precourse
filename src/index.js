8c9e262095570d6cf2a9cbc5745e8158aadb32c1
(yunjin) <sion11290805@gmail.com>
2021-11-27 19:04:33 +0900

    feat: pickUniqueNumbersInRange -> pickNumberInRange 함수로 변경

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { checkResult, showRestartGameButton } from './checkResult.js';
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
     const resultDiv = document.getElementById("result")
-    const userValidNumber = checkNumber(userInputNumbers);console.log(computerInputNumbers, userValidNumber)
+    const userValidNumber = checkNumber(userInputNumbers)
     const resultText = checkResult(computerInputNumbers, userValidNumber)
     showRestartGameButton(resultText)
     resultDiv.textContent = resultText
--- src/setNumber.js
+++ src/setNumber.js
@@ -3,10 +3,19 @@ import BaseballGame from './index.js'
 export function setNumber() { 
   const baseball = new BaseballGame()
   const userFrom = document.getElementById("user-form")
-  const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('')
+  const randomNumber = setRandonNumber()
   userFrom.addEventListener('submit', (e) => {
     const userNumber = e.target[0].value;
     e.preventDefault()
     baseball.play(randomNumber, userNumber)
   })
+}
+
+function setRandonNumber() {
+  const randomNumberSet = new Set();
+  while (randomNumberSet.size !== 3) {
+    randomNumberSet.add(MissionUtils.Random.pickNumberInRange(1, 9))
+  }
+
+  return [...randomNumberSet].join('');
 }
