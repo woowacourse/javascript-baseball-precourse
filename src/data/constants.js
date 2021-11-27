208d26890f101c399131bf11961e05b8b7ff5f48
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-11-27 22:57:37 +0900

    Style: 출력 텍스트 교정

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,13 +1,13 @@
 export const RESULT_CODE = {
-    ERROR_USERINPUT_LENGTH: Symbol("유저 입력 값이 세글자가 아님"),
-    ERROR_USERINPUT_UNIQUE_WORD: Symbol("유저 입력 값에 중복 단어가 있음"),
-    ERROR_USERINPUT_NUMBER_RANGE: Symbol("유저 입력 값은 1~9사이의 숫자만 입력할 수 있음"),
+    ERROR_USERINPUT_LENGTH: Symbol("유저 입력값이 세글자가 아님"),
+    ERROR_USERINPUT_UNIQUE_WORD: Symbol("유저 입력값에 중복 단어가 있음"),
+    ERROR_USERINPUT_NUMBER_RANGE: Symbol("유저 입력값은 1~9사이의 숫자만 입력할 수 있음"),
 
     DONE_USERINPUT_VALID: Symbol("유저 입력값 검증 성공"),
 };
 
 export const ERROR_MESSAGE = {
-    DEV_ONLY_NUMBER_STRING: "유저 입력 값에 1~9 사이가 아닌 값이 전달되었음",
+    DEV_ONLY_NUMBER_STRING: "유저 입력값에 1~9 사이가 아닌 값이 전달되었음",
 };
 
 export const RESULT_TEXT = {
