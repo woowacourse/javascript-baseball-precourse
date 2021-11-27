ff26bdeb672684c156d932c2fb7c6cbf930f528f
ziho <ziho@kakao.com>
2021-11-27 10:59:58 +0900

    feat: 입력값 검증 메서드 정의

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import { NUMBER_RULES, RESULT_MESSAGE } from './constants.js';
+import { NUMBER_RULES, RESULT_MESSAGE, VERIFIED_CODE } from './constants.js';
 
 export default class BaseballGame {
+
+  constructor() {
+    this.$userInput = document.getElementById('user-input');
+  }
 
   play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
@@ -12,6 +16,20 @@ export default class BaseballGame {
     while (computerNumberList.size < NUMBER_RULES.LENGTH)
       computerNumberList.add(MissionUtils.Random.pickNumberInRange(NUMBER_RULES.MIN_NUMBER, NUMBER_RULES.MAX_NUMBER));
 
-    return Array.from(computerNumberList);
+    return Array.from(computerNumberList).join('');
   }
+
+  verifyInputNumber(userInputValue) {
+    if (isNaN(userInputValue)) {
+      return VERIFIED_CODE.NOT_A_NUMBER;
+    } else if (userInputValue.includes(0)) {
+      return VERIFIED_CODE.ZERO_INCLUDED;
+    } else if (userInputValue.length < NUMBER_RULES.LENGTH) {
+      return VERIFIED_CODE.THREE_DIGIT;
+    } else if (new Set(userInputValue).size !== NUMBER_RULES.LENGTH) {
+      return VERIFIED_CODE.NUMBER_DUPLICATED;
+    }
+    return VERIFIED_CODE.VERIFIED;
+  }
+
 }
