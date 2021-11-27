b4d28910f93f4fcc61151153c5400501d0cf2dbf
hozzijeong <ghwls0905@naver.com>
2021-11-27 14:50:31 +0900

    Feat: 중복된 숫자 혹은 4자리 이상의 숫자 입력시 alert 띄우기 적용

--- src/index.js
+++ src/index.js
@@ -11,14 +11,19 @@ export default function BaseballGame() {
     this.play = function (computerInputNumbers, userInputNumbers) {
         const comNums = computerInputNumbers.split("");
         const userNums = userInputNumbers.split("");
-        const strCnt = checkStrike(comNums, userNums);
-        const ballCnt = checkBall(comNums, userNums);
+        if (checkUserNums(userNums)) {
+            alert("올바른 숫자를 입력해주세요");
+            document.getElementById("user-input").value = "";
+        } else {
+            const strCnt = checkStrike(comNums, userNums);
+            const ballCnt = checkBall(comNums, userNums);
 
-        if (strCnt === 0 && ballCnt === 0) return "낫싱";
-        else if (strCnt !== 0 && ballCnt !== 0)
-            return `${ballCnt}볼 ${strCnt}스트라이크`;
-        else if (strCnt === 0 && ballCnt !== 0) return `${ballCnt}볼`;
-        else if (strCnt === 3) return "정답";
+            if (strCnt === 0 && ballCnt === 0) return "낫싱";
+            else if (strCnt !== 0 && ballCnt !== 0)
+                return `${ballCnt}볼 ${strCnt}스트라이크`;
+            else if (strCnt === 0 && ballCnt !== 0) return `${ballCnt}볼`;
+            else if (strCnt === 3) return "정답";
+        }
     };
 
     const getVaule = function () {
@@ -42,10 +47,31 @@ export default function BaseballGame() {
         // 볼을 확인하는 함수. 볼의 조건은 comNums안에 있는 값들 중 같은 인덱스가 아니면서, 해당 값을 포함하고 있는 함수를 의미.
         let cnt = 0;
         comNums.forEach((element, index) => {
+            checkNums(element, index);
+        });
+
+        function checkNums(element, index) {
             userNums.forEach((ele, idx) => {
                 if (idx !== index && element === ele) cnt++;
             });
-        });
+        }
         return cnt;
+    };
+
+    const checkUserNums = function (userNums) {
+        let isPass = true;
+
+        if (userNums.length > 3) isPass = false;
+        userNums.forEach((element, index) => {
+            checkNums(element, index);
+        });
+
+        function checkNums(element, index) {
+            userNums.forEach((ele, idx) => {
+                if (idx !== index && element === ele) isPass = false;
+            });
+        }
+
+        return isPass;
     };
 }
