74e8255d3e5d89c7fa244f585e8067272a9314d7
minbr0ther <minbr0ther@naver.com>
2021-11-27 15:42:19 +0900

    refactor: 중복되는 기능 함수로 추출

--- src/utils/isValidNumbers.js
+++ src/utils/isValidNumbers.js
@@ -2,15 +2,18 @@ import utils from './utils.js';
 import DOMUtils from './DOMUtils.js';
 import { ERROR } from '../constants.js';
 
+const showErrorMessage = (errorMessage, element) => {
+  alert(errorMessage);
+  DOMUtils.initValue('#user-input');
+  element.focus();
+  return false;
+};
+
 const isValidNumbers = (userInput) => {
   if (!utils.isThreeDigitUniqueNumbers(userInput)) {
-    alert(ERROR.REQUIRE_THREE_UNIQUE_NUMBERS);
-    DOMUtils.initValue(userInput);
-    return false;
+    showErrorMessage(ERROR.REQUIRE_THREE_UNIQUE_NUMBERS, userInput);
   } else if (!utils.isDuplicateNumbers(userInput)) {
-    alert(ERROR.REQUIRE_NOT_DUPLICATE_NUMBERS);
-    DOMUtils.initValue(userInput);
-    return false;
+    showErrorMessage(ERROR.REQUIRE_NOT_DUPLICATE_NUMBERS, userInput);
   } else {
     return true;
   }
