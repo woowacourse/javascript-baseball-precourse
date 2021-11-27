4119673bc7c34656411f2f718d89306656e995d4
Seungjae Lee <sjlee4108@gmail.com>
2021-11-27 11:12:59 -0500

    feat: implement play and display game result

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,16 @@ const GAME_NUM_DIGIT = 3;
 const NAN_ERROR_MSG = 'The input contains non-numeric character.';
 const LENGTH_ERROR_MSG = 'The input does not match the required length.';
 const RANGE_ERROR_MSG = 'The input contains a digit that is out of the range.';
-const DUPLICATE_ERROR_MSG = 'The input has a duplicated value.'
+const DUPLICATE_ERROR_MSG = 'The input has a duplicated value.';
+
+// 결과 메세지
+const NOTHING_MSG = '낫싱';
+const STRIKE_MSG = '스트라이크';
+const BALL_MSG = '볼';
+
+// HTML 텍스트
+const WIN_MESSAGE = '<b>🎉 정답을 맞추셨습니다! 🎉</b> <br /><br /> 게임을 재시작 하시겠습니까?  ';
+const RESTART_GAME = '게임 재시작';
 
 export default {
   GAME_NUM_RANGE,
@@ -14,5 +23,10 @@ export default {
   NAN_ERROR_MSG,
   LENGTH_ERROR_MSG,
   RANGE_ERROR_MSG,
-  DUPLICATE_ERROR_MSG
-}
+  DUPLICATE_ERROR_MSG,
+  NOTHING_MSG,
+  STRIKE_MSG,
+  BALL_MSG,
+  WIN_MESSAGE,
+  RESTART_GAME,
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import ComputerAnswer from "./components/computerAnswer.js"
-import { checkValidUserInput } from "./functions/index.js"
+import ComputerAnswer from "./components/computerAnswer.js";
+import { checkValidUserInput } from "./functions/index.js";
+import constants from './constants/index.js';
+
 export default class BaseballGame {
 
-  /**
-   * @param {Array} numRange 길이가 2인 배열. [startInclusive, endInclusive]. 배열 안 숫자가 [0-9]라고 가정한다.
-   * @param {Number} numDigit 총 자리수
-   */
-  constructor(numRange, numDigit){
-    this.numRange = numRange;
-    this.numDigit = numDigit;
-    this.computerNumber = new ComputerAnswer(numRange, numDigit);
+  constructor() {
+    this.numRange = constants.GAME_NUM_RANGE;
+    this.numDigit = constants.GAME_NUM_DIGIT;
+    this.computerNumber = new ComputerAnswer(this.numRange, this.numDigit);
     this.#initElements();
     this.#addOnClickListeners();
   }
@@ -20,6 +18,17 @@ export default class BaseballGame {
   #initElements(){
     this.$inputText = document.getElementById('user-input');
     this.$submitButton = document.getElementById('submit');
+    this.$resultDiv = document.getElementById('result');
+    this.#initRestartButton();
+  }
+
+  /**
+   * @summary 재시작 버튼 element 생성하고 속성에 저장하기.
+   */
+  #initRestartButton(){
+    this.$restartButton = document.createElement('button');
+    this.$restartButton.setAttribute('id', 'game-restart-button');
+    this.$restartButton.textContent = constants.RESTART_GAME;
   }
 
   /**
@@ -36,17 +45,97 @@ export default class BaseballGame {
     const userInputNumbers = this.$inputText.value
     const [isValidInput, errorMsg] = checkValidUserInput(userInputNumbers, this.numRange, this.numDigit);
     if (isValidInput){
-      const resultStr = this.play(this.computerNumber.getNumber(), userInputNumbers);
+      const resultStr = this.play(this.computerNumber.getNumber(), parseInt(userInputNumbers));
+      this.#displayResult(resultStr);
     }else{
       alert(errorMsg);
     }
   }
 
+  /**
+   * @param {Number} computerInputNumbers 3자리 중복 없는 컴퓨터 숫자
+   * @param {Number} userInputNumbers 3자리 중복 없는 유저값 숫자
+   * @returns {String} 게임 결과 스트링 값 리턴.
+   */
   play(computerInputNumbers, userInputNumbers) {
-    return "결과 값 String"
+    console.log(computerInputNumbers, userInputNumbers);
+    const strikeCnt = this.#getStrikeCount(computerInputNumbers, userInputNumbers);
+    const ballCnt = this.#getBallCount(computerInputNumbers, userInputNumbers);
+    return this.#getResultString(strikeCnt,ballCnt);
   }
+
+  /** 
+   * @param {Number} computerInputNumbers 3자리 중복 없는 컴퓨터 숫자
+   * @param {Number} userInputNumbers 3자리 중복 없는 유저값 숫자
+   * @returns {Number} 스트라이크 횟수 리턴
+   */
+  #getStrikeCount(computerInputNumbers, userInputNumbers) {
+    const userInputStr = userInputNumbers.toString();
+    return [...computerInputNumbers.toString()].filter((num, i) => num === userInputStr[i]).length;
+  }
+
+  /** 
+   * @param {Number} computerInputNumbers 3자리 중복 없는 컴퓨터 숫자
+   * @param {Number} userInputNumbers 3자리 중복 없는 유저값 숫자
+   * @returns {Number} 볼 횟수 리턴
+   */
+  #getBallCount(computerInputNumbers, userInputNumbers){
+    const computerNumStr = computerInputNumbers.toString();
+    const computerNumSet = new Set([...computerNumStr]);
+    return [...userInputNumbers.toString()].filter((num, i) => computerNumSet.has(num) && num !== computerNumStr[i]).length;
+  }
+
+  /** 
+   * @param {Number} strikeCount 스트라이크 횟수
+   * @param {Number} ballCount 볼 횟수
+   * @returns {Number} 스트라이크, 볼 결과 스트링으로 변환
+   */
+  #getResultString(strikeCount, ballCount){
+    if(strikeCount === 0 && ballCount == 0) return constants.NOTHING_MSG;
+
+    const msgList = []
+    if (ballCount > 0) msgList.push(ballCount.toString() + constants.BALL_MSG);
+    if (strikeCount > 0) msgList.push(strikeCount.toString() + constants.STRIKE_MSG);
+
+    return msgList.join(' ');
+  }
+
+  /**
+   * @summary play 결과를 html element로 추가
+   * @param {String} resultMsg play 함수 결과값
+   */
+  #displayResult(resultMsg){
+    if (this.#isCorrectResult(resultMsg)){
+      this.#addReplayHTML()
+    }else{
+      this.#addTextHTML(resultMsg);
+    }
+  }
+
+  /**
+   * @summary 게임 정답 시 축하 메세지와 재시작 버튼 추가
+   */
+  #addReplayHTML(){
+    this.$resultDiv.innerHTML = constants.WIN_MESSAGE;
+    this.$resultDiv.appendChild(this.$restartButton);
+  }
+
+  /**
+   * @summary play 메세지값을 통해 유저가 승리했는지 확인
+   */
+  #isCorrectResult(resultMsg){
+    return resultMsg === this.numDigit+constants.STRIKE_MSG;
+  }
+
+  /**
+   * @summary 주워진 String을 결과 div에 추가
+   */
+  #addTextHTML(message){
+    this.$resultDiv.innerHTML = message;
+  }
+
 
 
 }
 
-new BaseballGame([1,9], 3)
+new BaseballGame();
