494ed78fd583fe5aa18a1e17b5426003ccdf4cda
On <cks3066@naver.com>
2021-11-28 02:43:15 +0900

    feat: 사용자 입력 숫자가 유효하지 않을 경우 alert 에러 메시지 및 다시 입력

---
+++ src/constant.js
@@ -0,0 +1,1 @@
+export const ERROR_MESSAGE = "1~9까지의 수를 중복없이 3개 입력해주세요.";
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import { generateRandomNumbers } from "./init.js";
 import { checkUserInputValue } from "./check.js";
+import { ERROR_MESSAGE } from "./constant.js";
 
 const userInput = document.getElementById("user-input");
 const buttonSubmit = document.getElementById("submit");
 
-const userInputValue = null;
-
 export default function BaseballGame() {
-  const setRandomNumbers = generateRandomNumbers();
+  const computerInputNumbers = generateRandomNumbers();
 
   this.play = function (computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   };
-
-  console.log(setRandomNumbers);
 }
 
 buttonSubmit.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(checkUserInputValue(userInput.value));
+  if (!checkUserInputValue(userInput.value)) {
+    alert(ERROR_MESSAGE);
+    userInput.focus();
+  }
 });
 
 new BaseballGame();
