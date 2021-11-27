4d5a663c19eaf3f4bbaf8042a14c19257b29a750
NE5054 <w_lan123@naver.com>
2021-11-27 12:11:33 +0900

    feat: 필요한 상수 파일 정의

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+//Error Message
+export const $lengthError = '세자릿수를 입력해주세요';
+export const $inputRangeError = '1 ~ 9 범위의 수 세자리를 입력해주세요';
+export const $overlapError = '중복되지 않는 자릿수를 입력해주세요';
+
+//DOM Element Object
+export const $checkButton = document.getElementById('submit');
+export const $inputElement = document.getElementById('user-input');
+export const $resultElement = document.getElementById('result');
+
+//DOM Element String
+export const answerDocument = `
+    <div>🎉<strong>정답을 맞추셨습니다</strong>🎉</div>
+    <p>게임을 새로 시작하시겠습니까? <button id="game-restart-button">게임 재시작</button></p>
+`;
