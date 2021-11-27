d87068aad59ffa47d94b2ee1670978f2568de5e2
Seungjae Lee <sjlee4108@gmail.com>
2021-11-27 10:01:01 -0500

    fix: change export config

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,18 @@
 // 게임 조건
-export const GAME_NUM_RANGE = [1,9];
-export const GAME_NUM_DIGIT = 3;
+const GAME_NUM_RANGE = [1,9];
+const GAME_NUM_DIGIT = 3;
 
 // 에러 메세지
-export const NAN_ERROR_MSG = 'The input contains non-numeric character.';
-export const LENGTH_ERROR_MSG = 'The input does not match the required length.';
-export const RANGE_ERROR_MSG = 'The input contains a digit that is out of the range.';
-export const DUPLICATE_ERROR_MSG = 'The input has a duplicated value.'
+const NAN_ERROR_MSG = 'The input contains non-numeric character.';
+const LENGTH_ERROR_MSG = 'The input does not match the required length.';
+const RANGE_ERROR_MSG = 'The input contains a digit that is out of the range.';
+const DUPLICATE_ERROR_MSG = 'The input has a duplicated value.'
+
+export default {
+  GAME_NUM_RANGE,
+  GAME_NUM_DIGIT,
+  NAN_ERROR_MSG,
+  LENGTH_ERROR_MSG,
+  RANGE_ERROR_MSG,
+  DUPLICATE_ERROR_MSG
+}
