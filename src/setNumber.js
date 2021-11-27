d4fc8b71c48545042ba6b5b4c3bab74a1a3d4430
(yunjin) <sion11290805@gmail.com>
2021-11-27 10:27:35 +0900

    feat: 유저가 3개의 숫자를 입력하는 기능

--- src/setNumber.js
+++ src/setNumber.js
@@ -1,5 +1,12 @@
 import BaseballGame from './index.js'
 
 export function setNumber() { 
+  const baseball = new BaseballGame()
+  const userFrom = document.getElementById("user-form")
   const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('')
+  userFrom.addEventListener('submit', (e) => {
+    const userNumber = e.target[0].value;
+    e.preventDefault()
+    baseball.play(randomNumber, userNumber)
+  })
 }
