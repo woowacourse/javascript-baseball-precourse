f4b379e4ca01b7c7c922252c4d0a33007f288de1
Chamy619 <yd95069506@naver.com>
2021-11-27 12:13:13 +0900

    feat: 게임 진행하는 기능 구현
    기능 1을 사용해 랜덤 수를 가지고 있도록 함
    확인 버튼 클릭시 유효하지 않은 입력의 경우 alert 띄움
    유효한 입력의 경우 결과를 출력

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-export default function BaseballGame() {}
+import { VALIDATE_MESSAGES } from './constants.js';
+import createComputerInputNumbers from './createComputerInputNumbers.js';
+import countStrikeAndBall from './countStrikeAndBall.js';
+import getResultMessage from './getResultMessage.js';
+import validateInput from './validateInput.js';
+
+export default function BaseballGame() {
+  const $result = document.getElementById('result');
+  const $submit = document.getElementById('submit');
+  const $userInput = document.getElementById('user-input');
+
+  this.computerInputNumbers = null;
+  this.userInputNumbers = null;
+
+  this.init = () => {
+    this.computerInputNumbers = createComputerInputNumbers();
+    $result.innerHTML = '';
+    attachSubmitEvent();
+  };
+
+  this.play = (computerInputNumbers, userInputNumbers) => {
+    console.log(computerInputNumbers);
+    console.log(userInputNumbers);
+    return getResultMessage(countStrikeAndBall(computerInputNumbers, userInputNumbers));
+  };
+
+  const getUserInputNumbers = () => {
+    const userInputString = $userInput.value;
+    const userInputValidateMessage = validateInput(userInputString);
+    if (userInputValidateMessage !== VALIDATE_MESSAGES.valid) {
+      alert(userInputValidateMessage);
+      return false;
+    }
+
+    return userInputString.split('').map((numberString) => Number(numberString));
+  };
+
+  const attachSubmitEvent = () => {
+    $submit.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      this.userInputNumbers = getUserInputNumbers();
+      if (!this.userInputNumbers) {
+        return;
+      }
+
+      const hint = this.play(this.computerInputNumbers, this.userInputNumbers);
+      $result.innerText = hint;
+    });
+  };
+}
+
+const baseball = new BaseballGame();
+baseball.init();
