19f01dd0bc83bbb72b753f852540fac15cfbb7a2
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-11-27 14:46:12 +0900

    feat: 게임 진행 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+export const BASEBALL = {
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+  ALERT_ONLY_NUM: '숫자만 입력해주세요.',
+  ALERT_THREE_NUM: '3개의 숫자를 입력해주세요.',
+  ALERT_ONE_TO_NINE: '1~9까지의 숫자를 입력해주세요.',
+  ALERT_DUPLICATION: '중복 없이 3개의 숫자를 입력해주세요.',
+  ANSWER: '🎉 정답을 맞추셨습니다! 🎉',
+  RESTART: '게임을 새로 시작하시겠습니까?',
+};
