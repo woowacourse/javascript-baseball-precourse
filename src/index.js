d2d2e1d5f0ead61666e09bc2f46f55671f260f94
On <cks3066@naver.com>
2021-11-28 01:27:06 +0900

    feat : 1에서 9까지 서로 다른 임의의 수 3개 생성 함수 추가

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
+import { generateRandomNumbers } from "./init.js";
+
 export default function BaseballGame() {
-    this.play = function (computerInputNumbers, userInputNumbers) {
-      return "결과 값 String";
-    };
-  }
-  
-  export default class BaseballGame {
-    play(computerInputNumbers, userInputNumbers) {
-      return "결과 값 String";
-    }
-  }
-  
-  // 예시
-  play(123, 456); // '낫싱'
-  play(123, 345); // '1볼'
-  play(123, 432); // '2볼'
-  play(123, 312); // '3볼'
-  play(123, 145); // '1스트라이크'
-  play(123, 134); // '1볼 1스트라이크'
-  play(123, 132); // '2볼 1스트라이크'
-  play(123, 124); // '2스트라이크'
-  
+  const setRandomNumbers = generateRandomNumbers();
+
+  this.play = function (computerInputNumbers, userInputNumbers) {
+    return "결과 값 String";
+  };
+
+  console.log(setRandomNumbers);
+}
+
+const buttonSubmit = document.getElementById("submit");
+
+buttonSubmit.addEventListener("click", (e) => {
+  console.log("hi");
+});
+
+new BaseballGame();
---
+++ src/init.js
@@ -0,0 +1,3 @@
+export function generateRandomNumbers() {
+  return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+}
