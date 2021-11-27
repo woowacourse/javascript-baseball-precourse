28936addedf7aef537170e01bc541b766a306b97
prefer2 <sunho6200@gmail.com>
2021-11-27 17:36:44 +0900

    chore: 직관적인 함수명으로 변경(index.js) compareAnswers->gameResult

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { $userInput, $result } from "./constants/constants.js";
-import compareAnswers from "./game/gameResult.js";
+import gameResult from "./game/gameResult.js";
 import playGame from "./game/playGame.js";
 import makeAnswer from "./game/makeAnswer.js";
 
@@ -12,7 +12,7 @@ export default function BaseballGame() {
   };
 
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return compareAnswers(computerInputNumbers, userInputNumbers);
+    return gameResult(computerInputNumbers, userInputNumbers);
   };
 
   reset();
