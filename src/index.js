599818eb020823b3381f0a48fbaeae2b503b46cc
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-11-27 16:41:36 +0900

    feat: 사용자 숫자 조건 만족 확인 기능

--- src/index.js
+++ src/index.js
@@ -17,6 +17,68 @@ export default function BaseballGame() {
     return computerInputNumbers
   };
 
+  //check 1- 숫자인지 확인
+  const checktype = num => {
+    if (isNaN(Number(num))) {
+      alert(BASEBALL.ALERT_ONLY_NUM);
+      return true;
+    }
+    return false;
+  }
+
+  //check 2- 3자리인지 확인
+  const checkLen = num => {
+    if (num.length != 3) {
+      alert(BASEBALL.ALERT_THREE_NUM)
+      return true;
+    }
+    return false;
+  }
+
+  //check3 - 숫자 범위 확인 (0 포함 확인)
+  const checkOnetoNine = num => {
+    if (num.includes(0)) {
+      alert(BASEBALL.ALERT_ONE_TO_NINE);
+      return true;
+    }
+    return false;
+  }
+
+  //check4 - 숫자 중복 여부 확인
+  const checkDuplication = num => {
+    if (num[0] == num[1] || num[1] == num[2] || num[0] == num[2]){
+      alert(BASEBALL.ALERT_DUPLICATION);
+      return true;
+    }
+    return false;
+  }
+
+  // check All
+  const checkNum = num => {
+    if (checktype(num) || checkLen(num) || checkOnetoNine(num) || checkDuplication(num)){
+      console.log("조건 불만족");
+      return false;
+    }
+    console.log("조건 만족");
+    return true;
+  }
+
+
+  // 사용자 수 입력받기
+  const getUserNum = () => {
+    const userInputNumbers = document.getElementById("user-input").value;
+
+    console.log(userInputNumbers);
+    if (checkNum(userInputNumbers)){
+      console.log("반환");
+      return userInputNumbers;
+    }
+    document.getElementById("user-input").autofocus;
+  }
+
+  const submitBtn = document.getElementById("submit");
+  submitBtn.addEventListener("click", getUserNum);
+
   getComputerNum();
 }
 
