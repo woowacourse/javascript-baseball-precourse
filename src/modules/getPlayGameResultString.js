16e2efff18536c132afadae8159b9fd3687c3698
liswkjts <liswktjs@gmail.com>
2021-11-27 13:03:30 +0900

    fix : (getPlayGameResultString)🐛 조건문의 순서를 바꿔줌 stirkecount가 3일때를 먼저 처리 해야  strikeCount가 0이아니고 ballcount가 0일때와 섞이지 않는다 getPlayGameResultString.js

--- src/modules/getPlayGameResultString.js
+++ src/modules/getPlayGameResultString.js
@@ -5,11 +5,11 @@ export default function getPlayGameResultString(ballCount, strikeCount){
     if(ballCount !== 0 && strikeCount === 0){
         return `${ballCount}볼`;
     }
-    if(ballCount === 0 && strikeCount !== 0){
-        return `${strikeCount}스트라이크`;
-    }
     if(strikeCount === 3){
         return 'gameEnd';
+    }
+    if(ballCount === 0 && strikeCount !== 0){
+        return `${strikeCount}스트라이크`;
     }
     return `${ballCount}볼 ${strikeCount}스트라이크`;
 }
