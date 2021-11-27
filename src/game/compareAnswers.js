efd2d7ea087ce3d6a1655901b3a88bab9c5520d8
prefer2 <sunho6200@gmail.com>
2021-11-27 16:31:38 +0900

    feat: 기능 5 게임 재시작 구현

--- src/game/compareAnswers.js
+++ src/game/compareAnswers.js
@@ -22,7 +22,7 @@ export default function compareAnswers(computerAnswer, userAnswer) {
   if (ball + strike === 0) return `낫싱`;
   if (strike === 3)
     return `<h3>🎉 정답을 맞추셨습니다 🎉</h3>
-    게임을 새로 시작하시겠습니까 <button id="game-restart-button">게임 재시작</button>`;
+    게임을 새로 시작하시겠습니까? <button id="game-restart-button">게임 재시작</button>`;
   if (ball === 0) return `${strike}스트라이크`;
   if (strike === 0) return `${ball}볼`;
   else return `${ball}볼 ${strike}스트라이크`;
--- src/game/playGame.js
+++ src/game/playGame.js
@@ -1,4 +1,5 @@
 import { $result, $submit } from "../constants/constants.js";
+import BaseballGame from "../index.js";
 import validUserInput from "../input/validUserInput.js";
 
 export default function playGame(computerAnswer, play) {
@@ -8,6 +9,16 @@ export default function playGame(computerAnswer, play) {
     if (userAnswer) {
       const gameResult = play(computerAnswer, userAnswer);
       $result.innerHTML = gameResult;
+      gameRestart();
     }
   });
 }
+
+const gameRestart = () => {
+  const $restartButton = document.getElementById("game-restart-button");
+  if ($restartButton) {
+    $restartButton.addEventListener("click", () => {
+      new BaseballGame();
+    });
+  }
+};
