060516985f54f736601582faf92c89c14008eb08
KIM YEJI <rladpwl0512@gmail.com>
2021-11-28 01:49:18 +0900

    docs: 요구사항 추가 정리

--- src/createAnswer.js
+++ src/createAnswer.js
@@ -1,7 +1,7 @@
 export const createAnswer = () => {
-  const computerAnwer = new Set();
-  while(computerAnwer.size < 3) {
-    computerAnwer.add(MissionUtils.Random.pickNumberInRange(1, 9));
+  const computerNumber = new Set();
+  while(computerNumber.size < 3) {
+    computerNumber.add(MissionUtils.Random.pickNumberInRange(1, 9));
   }
-  return [...computerAnwer];
+  return [...computerNumber];
 };
