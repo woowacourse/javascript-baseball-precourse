caa0fcde4d169ab4738f0c2748c6adcf050e2bdd
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-11-28 22:43:55 +0900

    refactor: component 함수화
    - div 와 button 생성과정을 함수화

--- src/components/app.js
+++ src/components/app.js
@@ -1,33 +1,45 @@
 import { FONT,DOM_ID,COUNT } from "../constant.js";
 
-export const resultElement = count => {
+const createResultElement = () => {
     const $result = document.createElement('div');
+    $result.id = DOM_ID.RESULT;
+
+    return $result;
+}
+
+const createButtonByID = (ID,text) => {
+    const $button = document.createElement('button');
+    const textNodeButton = document.createTextNode(text);
+
+    $button.id = ID;
+    $button.appendChild(textNodeButton);
+    return $button
+}
+
+export const resultElement = count => {
+    const $result = createResultElement();
     const textNode = document.createTextNode(count);
 
-    $result.id = DOM_ID.RESULT;
     $result.appendChild(textNode);
     return $result;
 }
 
 export const successElement = () => {
-    const $success = document.createElement('div');
+    const $success = createResultElement();
     const textNode = document.createTextNode(COUNT.SUCCESS_TEXT);
-    const $resetButton = document.createElement('button');
-    const textNodeButton = document.createTextNode(COUNT.RESTART);
-
     $success.appendChild(textNode);
-    $resetButton.appendChild(textNodeButton);
+
+    const $resetButton = createButtonByID(DOM_ID.RESET, COUNT.RESTART);
     $success.appendChild($resetButton);
-    $success.id = DOM_ID.RESULT;
-    $resetButton.id = DOM_ID.RESET;
     $success.style.fontWeight = FONT.WEIGHT_BOLD;
+
     return $success;
 }
 
 
 export const emptyElement = () =>{
-    const $empty = document.createElement('div');
-    $empty.id = DOM_ID.RESULT;
+    const $empty = createResultElement();
 
     return $empty;
 }
+
