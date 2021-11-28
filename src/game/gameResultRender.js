a44559436cb8b10fd95e4fac3cdc1cabc0c3dffe
kimsanglok <evertree6031@gmail.com>
2021-11-29 02:20:06 +0900

    Refactor: Strike, Ball의 갯수에 따른 화면출력 함수 -> 파일로 분리(gameResultRender.js)

---
+++ src/game/gameResultRender.js
@@ -0,0 +1,17 @@
+import { $ } from "../util/index.js";
+
+const gameResultRender = (game_result) => {
+  if (game_result === "정답") {
+    $("#result").innerHTML = `
+      <div>정답을 맞추셨습니다!</div>
+      게임을 새로 시작하시겠습니까? 
+      <button id="game-restart-button">재시작</button>
+    `;
+    return;
+  }
+
+  $("#result").innerText = game_result;
+  return;
+};
+
+export default gameResultRender;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import gameResult from "./game/gameResult.js";
+import gameResultRender from "./game/gameResultRender.js";
 import getComputerNumber from "./number/getComputerNumber.js";
 import getUserNumber from "./number/getUserNumber.js";
 import { $ } from "./util/index.js";
@@ -17,18 +18,7 @@ export default function BaseballGame() {
 
     if (userInputNumbers) {
       const game_result = gameResult(computerInputNumbers, userInputNumbers);
-
-      console.log(computerInputNumbers, userInputNumbers);
-      if (game_result === "정답") {
-        $("#result").innerHTML = `
-          <div>정답을 맞추셨습니다!</div>
-          게임을 새로 시작하시겠습니까? 
-          <button id="game-restart-button">재시작</button>
-        `;
-        return;
-      }
-      $("#result").innerText = game_result;
-      return;
+      gameResultRender(game_result);
     }
   });
 }
