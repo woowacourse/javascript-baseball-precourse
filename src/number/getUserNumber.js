49d895760572f44ae7fe6523be119505aab8cb36
kimsanglok <evertree6031@gmail.com>
2021-11-29 00:45:24 +0900

    Feat: 사용자 input 값 길이체크 로직 추가

--- src/number/getUserNumber.js
+++ src/number/getUserNumber.js
@@ -18,6 +18,11 @@ const checkValidation = (userInputNumbers) => {
   if (!isScope) {
     alert("1~9 사이의 숫자를 입력해주세요");
   }
+
+  const isLength = userInputNumbers.length === 3 ? true : false;
+  if (!isLength) {
+    alert("3자리의 수를 입력해주세요");
+  }
 };
 
 export default getUserNumber;
