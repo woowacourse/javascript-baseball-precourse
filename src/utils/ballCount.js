422275779fb7436f88072e38002c674c9cf0efb8
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-11-28 23:18:02 +0900

    refactor: BallCount계산 함수 분리
    - strike 와 일치하는 수 계산 함수 분리

--- src/utils/ballCount.js
+++ src/utils/ballCount.js
@@ -1,15 +1,27 @@
+const caculateStrike = (target, input) => {
+    const strike = target.reduce((ac,cur,index) =>{
+        return cur === input[index] ? ac+1 : ac;
+    },0);
+
+    return strike;
+}
+
+const calculateNumberOfMatches = (target, input) => {
+    const numberOfMatches = target.reduce((ac,cur,index)=>{
+        return input.includes(cur) ? ac + 1 : ac;
+    },0);
+
+    return numberOfMatches;
+}
+
 export const caculateBallCount = (target, input) => {
     const result = {
         "strike": 0,
         "ball" : 0
     };
-    const numberOfMatches = target.reduce((ac,cur,index)=>{
-        return input.includes(cur) ? ac + 1 : ac;
-    },0)
-    result.strike = target.reduce((ac,cur,index) =>{
-        return cur === input[index] ? ac+1 : ac;
-    },0);
-    result.ball = numberOfMatches - result.strike;
+
+    result.strike = caculateStrike(target, input);
+    result.ball = calculateNumberOfMatches(target, input) - result.strike;
 
     return result;
 }
