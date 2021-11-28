aea7fbbf0b697c0beeccdb18466f28ba4241e9da
sangminsong <ssmin0606@hanmail.net>
2021-11-29 00:35:08 +0900

    feat: 입력받은 값에 중복된 숫자가 있는지 검사하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import { isNumber } from "./util/validator.js";
+import { isNumber, isNotDuplicateExist } from "./util/validator.js";
 
 export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
-    if (isNumber(userInputNumbers)) {
+    if (isNumber(userInputNumbers) && isNotDuplicateExist(userInputNumbers)) {
       return "결과 값 String";
     }
-    alert("숫자를 입력하세요");
+    alert("입력값을 다시 확인해주세요");
   }
 }
 
 const baseBallGame = new BaseballGame();
+
+// 테스트용 콘솔 로그
+// console.log(baseBallGame.play(123, 456), "통과");
+// console.log(baseBallGame.play(123, "--"), "숫자아님-실패");
+// console.log(baseBallGame.play(123, 123451), "중복있음-실패");
--- src/util/validator.js
+++ src/util/validator.js
@@ -1,3 +1,14 @@
 export const isNumber = (value) => {
   return !isNaN(value);
 };
+
+export const isNotDuplicateExist = (value) => {
+  const numsExist = new Set();
+  const valueArray = Array.from(String(value));
+  for (const [idx, char] of valueArray.entries()) {
+    const res = idx && numsExist.has(char);
+    if (!!res) return false;
+    numsExist.add(char);
+  }
+  return true;
+};
