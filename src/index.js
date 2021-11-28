f4b2abb1f5086d2cff475df571fc7d66aac02cfe
ronci <jyj9647@naver.com>
2021-11-29 01:13:34 +0900

    feat: check computer input and user input are same
    Show the end of the game screen.
    Restart the game when restart button is clicked

--- src/index.js
+++ src/index.js
@@ -19,10 +19,14 @@ export default class BaseballGame {
     if (!checkCorrectInput(this.userInputNumbers)) {
       alert('올바르지 않은 입력입니다. 다시 입력해주세요');
     } else {
-      document.getElementById('result').innerText = this.play(
+      document.getElementById('result').innerHTML = this.play(
         this.computerInputNumbers,
         this.userInputNumbers
       );
+      if (document.getElementById('game-restart-button')) {
+        document.getElementById('game-restart-button').onclick =
+          this.gameRestart.bind(this);
+      }
     }
   }
 
@@ -43,12 +47,21 @@ export default class BaseballGame {
 
   playResult(strike, ball) {
     if (strike === 0 && ball === 0) return '낫싱';
-    if (strike === 3) return '정답';
+    if (strike === 3) return this.correctAnswer();
     if (strike === 0) return `${ball}볼`;
     if (ball === 0) return `${strike}스트라이크`;
     return `${ball}볼 ${strike}스트라이크`;
   }
+
+  correctAnswer() {
+    return '<div><strong>🎉 정답을 맞추셨습니다! 🎉</strong><p>게임을 새로 시작하시겠습니까?  <button id="game-restart-button">게임 재시작</button></p></div>';
+  }
+
+  gameRestart() {
+    this.computerInputNumbers = createComputerInputNumber();
+    document.getElementById('result').innerHTML = '';
+    document.getElementById('user-input').value = '';
+  }
 }
 
-const player = new BaseballGame();
-console.log(player);
+new BaseballGame();
