56b99d819aa8df9103a0912ca24dad4cd429dad9
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-28 19:55:33 +0900

    refactor: 일반 메소드로 변경 및 this 바인딩 적용
    변경사항:
    1. 화살표 함수에서 일반 함수로 변경
    2. initEventListeners에서 this binding 적용

--- src/index.js
+++ src/index.js
@@ -15,73 +15,83 @@ import {
 class BaseballGame {
   constructor () {
     this.computerInputNumberArray = [];
-  };
+  }
 
   // 초기화 함수
-  init = () => {
+  init () {
+    this.computerInputNumberArray = pickUniqueThreeNumbers();
+    this.initEventListeners();
+  }
+
+  restart () {
     this.computerInputNumberArray = pickUniqueThreeNumbers();
     $(SELECTOR.RESULT).innerHTML = '';
     $(SELECTOR.INPUT).value = '';
-    this.initEventListeners();
-  };
+  }
 
   // 이벤트리스너 초기화 함수
-  initEventListeners = () => {
-    $(SELECTOR.FORM).addEventListener('submit', this.onSubmitPlayerInput);
-    $(SELECTOR.RESULT).addEventListener('click', this.onClickRestartButton);
-  };
+  initEventListeners () {
+    $(SELECTOR.FORM).addEventListener('submit', this.onSubmitPlayerInput.bind(this));
+    $(SELECTOR.RESULT).addEventListener('click', this.onClickRestartButton.bind(this));
+  }
+
+  // 유저 입력값 검증 함수
+  validatePlayerInput (playerInputArray) {
+    // 길이가 3이 아닐 때 or 중복된 값이 들어있을 때 or 숫자가 아닌 값이 들어있을 때
+    if (
+      playerInputArray.length !== 3
+      || !validateUniqueInArray(playerInputArray)
+      || !validateNumberInArray(playerInputArray)
+    ) {
+      alert('입력 값을 확인해주세요');
+      $(SELECTOR.INPUT).value = '';
+      return false;
+    }
+    return true;
+  }
 
   // 유저 입력값 제출 함수
-  onSubmitPlayerInput = (event) => {
+  onSubmitPlayerInput (event) {
     event.preventDefault();
     const playerInputNumberArray = changeStringToNumberArray($(SELECTOR.INPUT).value);
     if (!this.validatePlayerInput(playerInputNumberArray)) return;
     const result = this.play(this.computerInputNumberArray, playerInputNumberArray);
     $(SELECTOR.RESULT).innerHTML = result;
-  };
+  }
 
   // 재시작 버튼 클릭 함수
-  onClickRestartButton = (event) => {
+  onClickRestartButton (event) {
     if (event.target.id === SELECTOR.RESTART_BUTTON) {
-      this.init();
+      this.restart();
     }
-  };
+  }
 
   // 게임 시작 함수
-  play = (computerInputNumberArray, playerInputNumberArray) => {
+  play (computerInputNumberArray, playerInputNumberArray) {
     const [strikeCount, ballCount] = checkSameOrInclude(computerInputNumberArray, playerInputNumberArray);
     if (strikeCount === 3) return this.createGameRestartButtonTemplate();
     // 볼도 아니고 스트라이크도 아닐 때
     return (!ballCount && !strikeCount)
       ? HINT.NOTHING
       : `${createResultHintString(ballCount, HINT.BALL)} ${createResultHintString(strikeCount, HINT.STRIKE)}`;
-  };
-
-  // 유저 입력값 검증 함수
-  static validatePlayerInput = (playerInputArray) => {
-    // 길이가 3이 아닐 때 or 중복된 값이 들어있을 때 or 숫자가 아닌 값이 들어있을 때
-    if (playerInputArray.length !== 3 || !validateUniqueInArray(playerInputArray) || !validateNumberInArray(playerInputArray)) {
-      alert('입력 값을 확인해주세요');
-      $(SELECTOR.INPUT).value = '';
-      return false;
-    }
-    return true;
-  };
+  }
 
   // 재시작 버튼 템플릿 생성 함수
-  static createGameRestartButtonTemplate = () => (
-    `
-      <div>
-        <p>
-          <strong>🎉 정답을 맞추셨습니다! 🎉</strong>
-        </p>
-        <span게임을 재시작 하시겠습니까?</span>
-        <button id="${SELECTOR.RESTART_BUTTON}">
-          게임 재시작
-        </button>
-      </div>
-    `
-  );
+  createGameRestartButtonTemplate () {
+    return (
+      `
+        <div>
+          <p>
+            <strong>🎉 정답을 맞추셨습니다! 🎉</strong>
+          </p>
+          <span게임을 재시작 하시겠습니까?</span>
+          <button id="${SELECTOR.RESTART_BUTTON}">
+            게임 재시작
+          </button>
+        </div>
+      `
+    );
+  }
 }
 
 const baseballgame = new BaseballGame();
