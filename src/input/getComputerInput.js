98d4d9ee36cba7f274a2d15f024054ee6befa5a9
donglee <zuzudnf@gmail.com>
2021-11-28 17:06:07 +0900

    refactor: COMPUTER_INPUT_DIGIT 상수를 INPUT_DIGIT 으로 이름 수정
    컴퓨터 입력값만 세자리가 아니라 사용자 입력값도 세자리이므로 다른 곳에서도 사용하기 위해 더 적절한 이름으로 수정함

--- src/input/getComputerInput.js
+++ src/input/getComputerInput.js
@@ -1,9 +1,9 @@
-const COMPUTER_INPUT_DIGIT = 3;
+export const INPUT_DIGIT = 3;
 
 export default function getComputerInput() {
   const computerInput = new Set();
 
-  while (computerInput.size < COMPUTER_INPUT_DIGIT) {
+  while (computerInput.size < INPUT_DIGIT) {
     computerInput.add(window.MissionUtils.Random.pickNumberInRange(1, 9));
   }
   return [...computerInput].join("");
