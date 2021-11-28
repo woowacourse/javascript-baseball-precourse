9e64986228664c6eb43d46d351e90384ca4cb144
jhyj0521 <jhyj3636@gmail.com>
2021-11-28 19:25:33 +0900

    fix: Catch randomThreeNumbers function Error
    기존 함수는 세 개의 랜덤한 숫자가 중복될 수 있다는 버그를 가지고 있었습니다.
    이를 숫자가 중복되지 않도록 수정하였습니다.

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,4 +1,11 @@
-export const randomThreeNumbers = () =>
-  MissionUtils.Random.pickNumberInRange(1, 9).toString() +
-  MissionUtils.Random.pickNumberInRange(1, 9).toString() +
-  MissionUtils.Random.pickNumberInRange(1, 9).toString();
+export const randomThreeNumbers = () => {
+  let result = '';
+
+  while (result.length < 3) {
+    const randomNum = MissionUtils.Random.pickNumberInRange(1, 9).toString();
+
+    if (!result.includes(randomNum)) result += randomNum;
+  }
+
+  return result;
+};
