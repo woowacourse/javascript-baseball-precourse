9625b66d205139f3093950316525e233f682359a
donglee <zuzudnf@gmail.com>
2021-11-28 17:43:39 +0900

    refactor: ONE_TO_NINE_ERR_MSG 상수 문자열 적절하게 수정

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -5,7 +5,7 @@ import isValidInput, {
 } from "./isValidInput.js";
 
 const THREE_DIGIT_ERR_MSG = "입력 오류! 세 자리의 숫자만 입력해주세요.";
-const ONE_TO_NINE_ERR_MSG = "입력 오류! 숫자만 입력해주세요.";
+const ONE_TO_NINE_ERR_MSG = "입력 오류! 1~9 까지의 숫자만 입력해주세요.";
 const DUPLICATE_ERR_MSG = "입력 오류! 중복없는 세 자리 숫자만 입력해주세요.";
 
 function alertErrorMessage(error) {
