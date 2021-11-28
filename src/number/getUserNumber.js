e2d15ce325903981c1e89bdcf7801de426643a8c
kimsanglok <evertree6031@gmail.com>
2021-11-29 00:57:29 +0900

    Refactor: 사용자 input 값 유효성 검사 로직 -> 함수로 분리

--- src/number/getUserNumber.js
+++ src/number/getUserNumber.js
@@ -6,30 +6,44 @@ const getUserNumber = () => {
     .split("")
     .map((number) => parseInt(number, 10));
 
-  checkValidation(userInputNumbers);
+  if (checkValidation(userInputNumbers)) {
+    return userInputNumbers.join("");
+  }
+};
 
-  return userInputNumbers;
+const checkScopeValidation = (userInputNumbers) => {
+  const result = userInputNumbers.every((number) =>
+    number >= 1 && number <= 9 ? true : false
+  );
+  return result;
+};
+
+const checkLengthValidation = (userInputNumbers) => {
+  return userInputNumbers.length === 3 ? true : false;
+};
+
+const checkDuplicateValidation = (userInputNumbers) => {
+  return userInputNumbers.length === new Set(userInputNumbers).size
+    ? true
+    : false;
 };
 
 const checkValidation = (userInputNumbers) => {
-  const isScope = userInputNumbers.every((number) =>
-    number >= 1 && number <= 9 ? true : false
-  );
-  if (!isScope) {
+  if (!checkScopeValidation(userInputNumbers)) {
     alert("1~9 사이의 숫자를 입력해주세요");
+    return false;
   }
 
-  const isLength = userInputNumbers.length === 3 ? true : false;
-  if (!isLength) {
+  if (!checkLengthValidation(userInputNumbers)) {
     alert("3자리의 수를 입력해주세요");
+    return false;
   }
 
-  const isDuplicated =
-    userInputNumbers.length === new Set(userInputNumbers).size ? true : false;
-
-  if (!isDuplicated) {
+  if (!checkDuplicateValidation(userInputNumbers)) {
     alert("중복이 없는 서로 다른 숫자를 입력해주세요");
+    return false;
   }
+  return true;
 };
 
 export default getUserNumber;
