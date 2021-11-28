31a190d087a253803fe32cb31c441f9e83bc3a95
donglee <zuzudnf@gmail.com>
2021-11-28 17:09:47 +0900

    feat: 볼 개수 확인하는 기능 추가

--- src/game/getGameResult.js
+++ src/game/getGameResult.js
@@ -1,8 +1,25 @@
+import { INPUT_DIGIT } from "../input/getComputerInput.js";
+
 function isRightAnswer(computerInput, userInput) {
   return computerInput === userInput;
 }
 
+function countBall(computerInput, userInput) {
+  let ballCount = 0;
+
+  for (let i = 0; i < INPUT_DIGIT; i += 1) {
+    if (userInput.find((elem, idx) => idx !== i && elem === computerInput[i])) {
+      ballCount += 1;
+    }
+  }
+  return ballCount !== 0 ? `${ballCount}볼` : "";
+}
+
 export default function getGameResult(computerInput, userInput) {
+  let ballCount = "";
+
   if (isRightAnswer(computerInput, userInput)) return "정답";
+  ballCount = countBall(computerInput.split(""), userInput.split(""));
+
   return "출력할 결과물";
 }
