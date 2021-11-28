a0e2b10c2f74a187e533b77adbf2709a28eeefcd
JuYoung <ken44929@gmail.com>
2021-11-28 20:08:47 +0900

    feat: 사용자가 입력한 값 유효성 체크

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const $ = selector => document.querySelector(selector);
+
 export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
+    const userInput = $('#user-input');
+    const button = $('#submit');
+
     const generateAnswer = () => {
       const answer = [];
 
@@ -10,7 +15,60 @@ export default class BaseballGame {
       return answer;
     };
 
+    const userInputValidation = value => {
+      const validation = {
+        isError: false,
+        inValidText: '',
+      };
+
+      if (!value) {
+        validation.isError = true;
+        validation.inValidText = '값을 입력해주세요.';
+        return validation;
+      }
+
+      if (isNaN(Number(value))) {
+        validation.isError = true;
+        console.log('isError: ', validation.isError);
+        validation.inValidText = '숫자를 입력해주세요.';
+        return validation;
+      }
+
+      if (value.length !== 3) {
+        validation.isError = true;
+        validation.inValidText = '숫자 3개를 입력해주세요.';
+
+        return validation;
+      }
+
+      const set = new Set(value.split(''));
+      if (value.length !== set.size) {
+        validation.isError = true;
+        validation.inValidText =
+          '중복된 숫자를 입력하였습니다. 다시 입력해주세요.';
+        return validation;
+      }
+
+      return validation;
+    };
+
+    const handleSubmitUserInput = e => {
+      e.preventDefault();
+      let val = userInput.value.trim();
+      const { isError, inValidText } = userInputValidation(val);
+
+      if (isError) {
+        alert(inValidText);
+        userInput.value = '';
+        return;
+      }
+
+      console.log('값 입력 성공!: ', userInput.value);
+      userInput.value = '';
+    };
+
     generateAnswer();
+    button.addEventListener('click', handleSubmitUserInput);
   }
 }
 
