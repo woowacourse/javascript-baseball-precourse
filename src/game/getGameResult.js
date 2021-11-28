0d9b2d81e4dcdb5a1c176609d7a07a72cc8a5eb7
donglee <zuzudnf@gmail.com>
2021-11-28 17:24:46 +0900

    feat: 스트라이크 개수 확인하는 기능 추가

--- src/game/getGameResult.js
+++ src/game/getGameResult.js
@@ -15,11 +15,23 @@ function countBall(computerInput, userInput) {
   return ballCount !== 0 ? `${ballCount}볼` : "";
 }
 
+function countStrike(computerInput, userInput) {
+  let strikeCount = 0;
+
+  for (let i = 0; i < INPUT_DIGIT; i += 1) {
+    if (userInput.find((elem, idx) => idx === i && elem === computerInput[i])) {
+      strikeCount += 1;
+    }
+  }
+  return strikeCount !== 0 ? `${strikeCount}스트라이크` : "";
+}
+
 export default function getGameResult(computerInput, userInput) {
   let ballCount = "";
+  let strikeCount = "";
 
   if (isRightAnswer(computerInput, userInput)) return "정답";
   ballCount = countBall(computerInput.split(""), userInput.split(""));
-
+  strikeCount = countStrike(computerInput.split(""), userInput.split(""));
   return "출력할 결과물";
 }
