2445ff4b33c5cd5759ebae27ccc0d1d208d9ec7d
moonnew <b4bynew@gmail.com>
2021-11-28 19:24:30 +0900

    feat: 사용자 입력 유효성 검사하는 기능 추가

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ export default class BaseballGame {
     this.userInput = document.getElementById("user-input");
     this.result = document.getElementById("result");
 
+    document
+      .getElementById("submit")
+      .addEventListener("click", this.userClickBtn);
+
     this.gameInitializing();
   }
 
@@ -24,6 +28,31 @@ export default class BaseballGame {
     document.getElementById("user-input").value = "";
   }
 
+  userClickBtn = () => {
+    if (!this.isCorrectInput(this.userInput.value)) {
+      alert("입력 형식이 틀렸어요. 다시 확인해주세요");
+      return;
+    }
+
+    this.play(this.answer, this.userInput.value);
+  };
+
+  isCorrectInput(inputString) {
+    let inputNumbers = new Set();
+    inputNumbers.add(Number(inputString.charAt(0)));
+    inputNumbers.add(Number(inputString.charAt(1)));
+    inputNumbers.add(Number(inputString.charAt(2)));
+
+    if (
+      inputString.length !== 3 ||
+      isNaN(Number(inputString)) ||
+      inputNumbers.has(0)
+    ) {
+      return false;
+    }
+
+    return true;
+  }
 }
 
 new BaseballGame();
