946482646fee6c26bbab4d93a8cc01ad1d5c809f
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-28 21:07:54 +0900

    chore: ESLint, Prettier 의존성 설치
    eslint, prettier를 설치하고 설정 파일 적용

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true, // string '' 으로
+  semi: true, // 코드 끝에 ; 붙이기
+  useTabs: true, // 탭 사용
+  tabWidth: 2, // 탭 간격 2칸 ( JS, TS 추천 )
+  trailingComma: 'all', // 객체 내부 각 키-값에 , 넣어줌
+  printWidth: 100, // 한 줄 최대 80칸
+  bracketSpacing: true, // 객체 내부 한칸씩 띄워줌
+  arrowParens: 'avoid', // 화살표함수 인자가 1개일시 () 생략
+};
