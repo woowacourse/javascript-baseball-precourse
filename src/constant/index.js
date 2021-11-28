3db31b10d51024db4014afb89ec74d814611620f
woose28 <dldntmdsla@naver.com>
2021-11-28 13:51:10 +0900

    feat: 사용자에게 입력받는 기능 구현
    id가 'submit'인 엘리먼트에 onclick 이벤트 핸들러 등록
    사용자의 입력 값이 유효한 값인지 확인
    사용자가 잘못된 값을 입력할 경우 alert 메서드를 호출하도록 예외 처리
    id가 'submit' DOM 엘리먼트에 접근하기 위해 id를 constatn/index.js에 상수로 정의
    alert 메서드에 보여줄 메시지를 constatn/index.js에 상수로 정의

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,11 +5,16 @@ const LENGTH_NUMBERS = 3;
 
 const ELEMENT_ID_USER_INPUT = 'user-input';
 const ELEMENT_ID_RESULT = 'result';
+const ELEMENT_ID_SUBMIT = 'submit';
+
+const ALERT_MESSAGE = '1부터 9사이의 중복되지 않는 숫자 3개를 입력해 주세요:)';
 
 export {
     MIN_NUMBER_IN_RANGE,
     MAX_NUMBER_IN_RANGE,
     LENGTH_NUMBERS,
     ELEMENT_ID_USER_INPUT,
-    ELEMENT_ID_RESULT
+    ELEMENT_ID_RESULT,
+    ELEMENT_ID_SUBMIT,
+    ALERT_MESSAGE,
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import Computer from './Computer.js';
 import {
+    MIN_NUMBER_IN_RANGE,
+    MAX_NUMBER_IN_RANGE,
+    LENGTH_NUMBERS,
     ELEMENT_ID_USER_INPUT,
     ELEMENT_ID_RESULT,
+    ELEMENT_ID_SUBMIT,
+    ALERT_MESSAGE,
 } from './constant/index.js';
 
 export default class BaseballGame {
     constructor() {
         this.$userInput = document.getElementById(ELEMENT_ID_USER_INPUT);
         this.$result = document.getElementById(ELEMENT_ID_RESULT);
+        this.$submit = document.getElementById(ELEMENT_ID_SUBMIT);
         this.computer = null;
 
         this.initialize();
+        this.enrollSubmitOnclickHandler();
     }
 
     initialize() {
@@ -18,6 +25,62 @@ export default class BaseballGame {
         this.$result.innerHTML = '';
         this.$userInput.focus();
         this.computer = new Computer();
+    }
+    
+    enrollSubmitOnclickHandler() {
+        this.$submit.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.submitUserInput();
+        })
+    }
+
+    submitUserInput() {
+        if (this.checkIsValidInput(this.$userInput.value)) {
+            this.play(this.computer.getComputerNumbers(), this.$userInput.value);
+        } else {
+            this.handleException();
+        }
+    }
+
+    checkIsValidInput(userInputValue) {
+        let isValid = false;
+
+        if (
+            this.checkLength(userInputValue)
+            && this.checkIsAllValidNumber(userInputValue)
+            && this.checkDuplicatedNumber(userInputValue)
+        ) {
+            isValid = true;
+        }
+
+        return isValid;
+    }
+
+    checkLength(userInputValue) {
+        return userInputValue.length === LENGTH_NUMBERS;
+    }
+
+    checkIsAllValidNumber(userInputValue) {
+        const isAllNaturalNumber = userInputValue.split('').every((e)=>{
+            if (!isNaN(e) && MIN_NUMBER_IN_RANGE <= e && MAX_NUMBER_IN_RANGE >= e) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        return isAllNaturalNumber;
+    }
+
+    checkDuplicatedNumber(userInputNumbers) {
+        const { size } = new Set(userInputNumbers.split(''));
+
+        return size === LENGTH_NUMBERS
+    }
+
+    handleException() {
+        alert(ALERT_MESSAGE);
+        this.$userInput.focus();
     }
 
     play(computerInputNumbers, userInputNumbers) {
