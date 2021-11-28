90ce96204e910a8f475bda3fbc9d185c1f05b7a1
jhyj0521 <jhyj3636@gmail.com>
2021-11-28 18:57:58 +0900

    feat: Render result for input numbers
    - submit 기본 기능 막음
    - input value와 저장되어 있는 정답을 비교해 적절한 텍스트 반환
    - 컴퓨터 랜덤 값이 중복된다는 버그 발견

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import BaseballGame from './lib/baseballGame.js';
+
+const baseballGame = new BaseballGame();
+
+const $userInput = document.getElementById('user-input');
+const $submit = document.getElementById('submit');
+const $result = document.getElementById('result');
+
+$submit.onclick = e => {
+  e.preventDefault();
+
+  const userInputNumbers = $userInput.value;
+  const result = baseballGame.play(baseballGame.answer, userInputNumbers);
+
+  $result.textContent = result;
+
+  $userInput.value = '';
+};
--- src/lib/baseballGame.js
+++ src/lib/baseballGame.js
@@ -2,10 +2,28 @@ import { randomThreeNumbers } from '../utils/random.js';
 
 export default class BaseballGame {
   constructor() {
-    this.answer = randomThreeNumbers();
+    this.computerInputNumbers = randomThreeNumbers();
+  }
+
+  get answer() {
+    return this.computerInputNumbers;
   }
 
   play(computerInputNumbers, userInputNumbers) {
-    return '결과 값 String';
+    let strike = 0;
+    let ball = 0;
+
+    [...computerInputNumbers].forEach((computerInputNumber, index) => {
+      if (computerInputNumber === userInputNumbers[index]) strike += 1;
+      else if (computerInputNumbers.includes(userInputNumbers[index])) ball += 1;
+    });
+
+    return strike && ball
+      ? `${ball}볼 ${strike}스트라이크`
+      : !strike && !ball
+      ? `낫싱`
+      : !strike
+      ? `${ball}볼`
+      : `${strike}스트라이크`;
   }
 }
