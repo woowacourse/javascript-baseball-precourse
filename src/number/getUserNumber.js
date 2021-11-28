06987d030aa7969b8edcc32765968753f8489d8e
kimsanglok <evertree6031@gmail.com>
2021-11-29 00:43:25 +0900

    Feat: 사용자 input 값 1~9 사이의 영역체크 로직 추가

--- src/number/getUserNumber.js
+++ src/number/getUserNumber.js
@@ -2,8 +2,22 @@ import { $ } from "../util/index.js";
 
 const getUserNumber = () => {
   const $userInput = $("#user-input").value;
+  const userInputNumbers = $userInput
+    .split("")
+    .map((number) => parseInt(number, 10));
 
-  return $userInput;
+  checkValidation(userInputNumbers);
+
+  return userInputNumbers;
+};
+
+const checkValidation = (userInputNumbers) => {
+  const isScope = userInputNumbers.every((number) =>
+    number >= 1 && number <= 9 ? true : false
+  );
+  if (!isScope) {
+    alert("1~9 사이의 숫자를 입력해주세요");
+  }
 };
 
 export default getUserNumber;
