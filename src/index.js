105daba43c61e5cb832dbd58d2889b3c039c25ad
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-28 21:08:42 +0900

    style: ESLint, Prettier 적용
    모든 구현 파일에 eslint, prettier 적용

--- src/index.js
+++ src/index.js
@@ -1,86 +1,88 @@
 import { $ } from './utils/dom.js';
+import { SELECTOR, HINT } from './constants.js';
 import {
-  pickUniqueThreeNumbers,
-  changeStringToNumberArray,
-  validateUniqueInArray,
-  validateNumberInArray,
-  checkSameOrInclude,
-  createResultHintString,
+	pickUniqueThreeNumbers,
+	changeStringToNumberArray,
+	validateUniqueInArray,
+	validateNumberInArray,
+	checkSameOrInclude,
+	createResultHintString,
 } from './utils/index.js';
-import {
-  SELECTOR,
-  HINT,
-} from './constants.js';
 
 class BaseballGame {
-  constructor () {
-    this.computerInputNumberArray = [];
-  }
+	constructor() {
+		this.computerInputNumberArray = [];
+	}
 
-  // 초기화 함수
-  init () {
-    this.computerInputNumberArray = pickUniqueThreeNumbers();
-    this.initEventListeners();
-  }
+	// 초기화 함수
+	init() {
+		this.computerInputNumberArray = pickUniqueThreeNumbers();
+		this.initEventListeners();
+	}
 
-  // 재시작 함수
-  restart () {
-    this.computerInputNumberArray = pickUniqueThreeNumbers();
-    $(SELECTOR.RESULT).innerHTML = '';
-    $(SELECTOR.INPUT).value = '';
-  }
+	// 재시작 함수
+	restart() {
+		this.computerInputNumberArray = pickUniqueThreeNumbers();
+		$(SELECTOR.RESULT).innerHTML = '';
+		$(SELECTOR.INPUT).value = '';
+	}
 
-  // 이벤트리스너 초기화 함수
-  initEventListeners () {
-    $(SELECTOR.FORM).addEventListener('submit', this.onSubmitPlayerInput.bind(this));
-    $(SELECTOR.RESULT).addEventListener('click', this.onClickRestartButton.bind(this));
-  }
+	// 이벤트리스너 초기화 함수
+	initEventListeners() {
+		$(SELECTOR.FORM).addEventListener('submit', this.onSubmitPlayerInput.bind(this));
+		$(SELECTOR.RESULT).addEventListener('click', this.onClickRestartButton.bind(this));
+	}
 
-  // 유저 입력값 검증 함수
-  validatePlayerInput (playerInputArray) {
-    // 길이가 3이 아닐 때 or 중복된 값이 들어있을 때 or 숫자가 아닌 값이 들어있을 때
-    if (
-      playerInputArray.length !== 3
-      || !validateUniqueInArray(playerInputArray)
-      || !validateNumberInArray(playerInputArray)
-    ) {
-      alert('입력 값을 확인해주세요');
-      $(SELECTOR.INPUT).value = '';
-      return false;
-    }
-    return true;
-  }
+	// 유저 입력값 검증 함수
+	validatePlayerInput(playerInputArray) {
+		// 길이가 3이 아닐 때 or 중복된 값이 들어있을 때 or 숫자가 아닌 값이 들어있을 때
+		if (
+			playerInputArray.length !== 3 ||
+			!validateUniqueInArray(playerInputArray) ||
+			!validateNumberInArray(playerInputArray)
+		) {
+			alert('입력 값을 확인해주세요');
+			$(SELECTOR.INPUT).value = '';
+			return false;
+		}
+		return true;
+	}
 
-  // 유저 입력값 제출 함수
-  onSubmitPlayerInput (event) {
-    event.preventDefault();
-    const playerInputNumberArray = changeStringToNumberArray($(SELECTOR.INPUT).value);
-    if (!this.validatePlayerInput(playerInputNumberArray)) return;
-    const result = this.play(this.computerInputNumberArray, playerInputNumberArray);
-    $(SELECTOR.RESULT).innerHTML = result;
-  }
+	// 유저 입력값 제출 함수
+	onSubmitPlayerInput(event) {
+		event.preventDefault();
+		const playerInputNumberArray = changeStringToNumberArray($(SELECTOR.INPUT).value);
+		if (!this.validatePlayerInput(playerInputNumberArray)) return;
+		const result = this.play(this.computerInputNumberArray, playerInputNumberArray);
+		$(SELECTOR.RESULT).innerHTML = result;
+	}
 
-  // 재시작 버튼 클릭 함수
-  onClickRestartButton (event) {
-    if (event.target.id === SELECTOR.RESTART_BUTTON) {
-      this.restart();
-    }
-  }
+	// 재시작 버튼 클릭 함수
+	onClickRestartButton(event) {
+		if (event.target.id === SELECTOR.RESTART_BUTTON) {
+			this.restart();
+		}
+	}
 
-  // 게임 시작 함수
-  play (computerInputNumberArray, playerInputNumberArray) {
-    const [strikeCount, ballCount] = checkSameOrInclude(computerInputNumberArray, playerInputNumberArray);
-    if (strikeCount === 3) return this.createGameRestartButtonTemplate();
-    // 볼도 아니고 스트라이크도 아닐 때
-    return (!ballCount && !strikeCount)
-      ? HINT.NOTHING
-      : `${createResultHintString(ballCount, HINT.BALL)} ${createResultHintString(strikeCount, HINT.STRIKE)}`;
-  }
+	// 게임 시작 함수
+	play(computerInputNumberArray, playerInputNumberArray) {
+		const [strikeCount, ballCount] = checkSameOrInclude(
+			computerInputNumberArray,
+			playerInputNumberArray,
+		);
+		if (strikeCount === 3) return this.createGameRestartButtonTemplate();
+		// 볼도 아니고 스트라이크도 아닐 때
+		return !ballCount && !strikeCount
+			? HINT.NOTHING
+			: `${createResultHintString(ballCount, HINT.BALL)} ${createResultHintString(
+					strikeCount,
+					HINT.STRIKE,
+			  )}`;
+	}
 
-  // 재시작 버튼 템플릿 생성 함수
-  createGameRestartButtonTemplate () {
-    return (
-      `
+	// 재시작 버튼 템플릿 생성 함수
+	createGameRestartButtonTemplate() {
+		return `
         <div>
           <p>
             <strong>🎉 정답을 맞추셨습니다! 🎉</strong>
@@ -90,9 +92,8 @@ class BaseballGame {
             게임 재시작
           </button>
         </div>
-      `
-    );
-  }
+      `;
+	}
 }
 
 const baseballgame = new BaseballGame();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,41 +1,38 @@
 // 1부터 9까지 3개의 중복되지 않는 수를 뽑는 함수
 export const pickUniqueThreeNumbers = () => {
-  const numberSet = new Set();
-  while (numberSet.size !== 3) {
-    numberSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
-  }
-  return Array.from(numberSet);
+	const numberSet = new Set();
+	while (numberSet.size !== 3) {
+		numberSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
+	}
+	return Array.from(numberSet);
 };
 
 // string으로 된 숫자들을 배열로 리턴하는 함수
-export const changeStringToNumberArray = (string) => {
-  return string.split('').map(item => Number(item));
-};
+export const changeStringToNumberArray = string => string.split('').map(item => Number(item));
 
 // 배열의 요소들이 중복되지 않는지 검증하는 함수
-export const validateUniqueInArray = (array) => {
-  const set = new Set();
-  array.map((item) => set.add(item));
-  return set.size === 3 ? true : false;
+export const validateUniqueInArray = array => {
+	const set = new Set();
+	array.map(item => set.add(item));
+	return set.size === 3;
 };
 
 // 배열의 요소들이 숫자인지 검증하는 함수
-export const validateNumberInArray = (array) => {
-  const isNumberArray = array.map((item) => Number.isNaN(item));
-  return isNumberArray.includes(true) ? false : true;
+export const validateNumberInArray = array => {
+	const isNumberArray = array.map(item => Number.isNaN(item));
+	return !isNumberArray.includes(true);
 };
 
 // 같은 자리에 있는 요소인지 혹은 포함하는 요소인지 확인하는 함수
 export const checkSameOrInclude = (array1, array2) => {
-  let same = 0, include = 0;
-  for (let i = 0; i < 3; i += 1) {
-    if (array1[i] === array2[i]) same += 1;
-    else if (array1.includes(array2[i])) include += 1; 
-  }
-  return [same, include];
+	let same = 0;
+	let include = 0;
+	for (let i = 0; i < 3; i += 1) {
+		if (array1[i] === array2[i]) same += 1;
+		else if (array1.includes(array2[i])) include += 1;
+	}
+	return [same, include];
 };
 
 // 힌트 문자열을 count에 따라 반환하는 함수
-export const createResultHintString = (count, hint) => (
-  count ? `${count}${hint}` : ''
-);
+export const createResultHintString = (count, hint) => (count ? `${count}${hint}` : '');
