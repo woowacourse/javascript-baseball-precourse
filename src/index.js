f7de08a6033348e03c9deb1ea162dbc9cfd2c5a0
kimsanglok <evertree6031@gmail.com>
2021-11-29 02:15:48 +0900

    Feat: Strike와 Ball의 갯수에 따른 화면출력 함수 추가

--- src/index.js
+++ src/index.js
@@ -17,6 +17,18 @@ export default function BaseballGame() {
 
     if (userInputNumbers) {
       const game_result = gameResult(computerInputNumbers, userInputNumbers);
+
+      console.log(computerInputNumbers, userInputNumbers);
+      if (game_result === "정답") {
+        $("#result").innerHTML = `
+          <div>정답을 맞추셨습니다!</div>
+          게임을 새로 시작하시겠습니까? 
+          <button id="game-restart-button">재시작</button>
+        `;
+        return;
+      }
+      $("#result").innerText = game_result;
+      return;
     }
   });
 }
