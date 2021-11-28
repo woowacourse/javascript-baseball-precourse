a0bbed3b1556c3267bee32120dbb725199439679
ronci <jyj9647@naver.com>
2021-11-28 22:52:25 +0900

    feat: compare user data and computer data
    index.js
    Compare user data and computer data.
    Output the return string  according to the output type.
    
    index.html
    Make the initial state of div with the ID 'result' empty.

--- src/index.js
+++ src/index.js
@@ -14,17 +14,39 @@ export default class BaseballGame {
     e.preventDefault();
     this.userInputNumbers = document
       .getElementById('user-input')
-      .value.split('');
+      .value.split('')
+      .map(Number);
     if (!checkCorrectInput(this.userInputNumbers)) {
       alert('올바르지 않은 입력입니다. 다시 입력해주세요');
     } else {
-      console.log(this);
-      this.play(this.computerInputNumbers, this.userInputNumbers);
+      document.getElementById('result').innerText = this.play(
+        this.computerInputNumbers,
+        this.userInputNumbers
+      );
     }
   }
 
   play(computerInputNumbers, userInputNumbers) {
-    return this;
+    let strike = 0;
+    let ball = 0;
+    computerInputNumbers.forEach((number, index) => {
+      const currentNumIdx = userInputNumbers.findIndex(
+        (item) => item === number
+      );
+      if (currentNumIdx !== -1) {
+        if (index === currentNumIdx) strike += 1;
+        else ball += 1;
+      }
+    });
+    return this.playResult(strike, ball);
+  }
+
+  playResult(strike, ball) {
+    if (strike === 0 && ball === 0) return '낫싱';
+    if (strike === 3) return '정답';
+    if (strike === 0) return `${ball}볼`;
+    if (ball === 0) return `${strike}스트라이크`;
+    return `${ball}볼 ${strike}스트라이크`;
   }
 }
 
