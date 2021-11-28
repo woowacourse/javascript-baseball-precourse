ccb9952cad6f0addbbde65979ba4dce3e033fddb
moonheekim0118 <hellomooneekim@gmail.com>
2021-11-28 17:30:15 +0900

    refactor: 변수명을 수정한다.

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const MESSAGE = {
     '🚫 올바르지 않은 입력값 입니다! 1~9 사이의 서로 다른 숫자 3자리를 넣어주세요!',
 };
 
-const RESTART_VIEW = `
+const END = `
   <p>🎉 정답을 맞추셨습니다 🎉</p>
   <span> 게임을 새로 시작하시겠습니까? </span>
   <button id="game-restart-button">게임 재시작</button>
@@ -19,5 +19,5 @@ export const GAME_RESULT = {
   STRIKE: '스트라이크',
   BALL: '볼',
   NOTHING: '낫싱',
-  END: RESTART_VIEW,
+  END,
 };
