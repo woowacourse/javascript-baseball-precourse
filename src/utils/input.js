5567e2d5ae799edc4453bda8144fca558598a7c2
moonheekim0118 <hellomooneekim@gmail.com>
2021-11-28 17:05:30 +0900

    refactor : input을 다루는 함수 내의 변수명을 명시적으로 변경한다.

--- src/utils/input.js
+++ src/utils/input.js
@@ -1,11 +1,11 @@
 import { BASEBALL_RULE } from '../constants.js';
 
-const hasZero = (target) => {
-  return target.indexOf('0') !== -1;
+const hasZero = (numbers) => {
+  return numbers.indexOf('0') !== -1;
 };
 
-const isValidLength = (target) => {
-  return new Set(target.split('')).size === BASEBALL_RULE.DIGITS;
+const isValidLength = (numbers) => {
+  return new Set(numbers.split('')).size === BASEBALL_RULE.DIGITS;
 };
 
 export const isNotValidInput = (input) => {
