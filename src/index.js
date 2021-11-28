2789061a066dda4ddf2664ab8984ae3468165912
heejaykong <heejay.dev@gmail.com>
2021-11-28 18:34:00 +0900

    feat: 사용자로부터 숫자 입력받기 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import INPUT_RULES from './constants';
+import { INPUT_RULES } from './constants.js';
+
+const $ = (selector) => document.querySelector(selector);
 
 function generateRandomNumbers() {
-  const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
+  return MissionUtils.Random.pickUniqueNumbersInRange(
     INPUT_RULES.MIN_OF_RANGE,
     INPUT_RULES.MAX_OF_RANGE,
     INPUT_RULES.NUMBER_OF_DIGITS
   );
-  return randomNumbers;
 }
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const $userInput = $('#user-input');
+
+  const currentUserInput = $userInput.value;
+  console.log(currentUserInput);
+
+  $userInput.value = '';
+}
+
+const $submitBtn = $('#submit');
+$submitBtn.addEventListener('click', handleSubmit);
 
 export default class BaseballGame {
   constructor() {
