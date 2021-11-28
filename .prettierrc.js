4fcfcb059672ed15b108b7ca6fc3586a1576115e
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-28 21:11:38 +0900

    chore: ESLint useTabs option 변경
    useTabs 옵션을 false로 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   singleQuote: true, // string '' 으로
   semi: true, // 코드 끝에 ; 붙이기
-  useTabs: true, // 탭 사용
+  useTabs: false, // 탭 사용 금지
   tabWidth: 2, // 탭 간격 2칸 ( JS, TS 추천 )
   trailingComma: 'all', // 객체 내부 각 키-값에 , 넣어줌
   printWidth: 100, // 한 줄 최대 80칸
