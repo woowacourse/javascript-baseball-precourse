68f55cbb9801f2162da49cb78fe3f04a9838dd7d
mieumje <66072832+mieumje@users.noreply.github.com>
2021-11-28 21:20:18 +0900

    style : 불필요한 주석, 공백 제거, index.html 수정
    index.js -> 불필요한 주석, 공백 제거 / 누락된 (;) 추가 / 개임 재시작 시 input 값 초기화
    
    index.html -> 초기 결과 제거 (1볼 1스트라이크 -> 공백)

--- src/index.js
+++ src/index.js
@@ -19,48 +19,43 @@ export default function BaseballGame() {
 
     const setAnswer = () => {
         let generateRandNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-        computerInputNumbers = generateRandNumbers
+        computerInputNumbers = generateRandNumbers;
         console.log(computerInputNumbers);
         return computerInputNumbers;
     };
     setAnswer();
 
-    //computerInputNumbers = setAnswer();
-
-
-    // checkout Input
     const checkInput = userInputNumbers => {
         if (userInputNumbers.length !== 3) {
             return alert('3자리 숫자를 입력하세요');
-        }
+        };
         if (userInputNumbers[0] == 0 || userInputNumbers[1] == 0 || userInputNumbers[2] == 0) {
             return alert('0이 아닌 숫자를 입력하세요');
-        }
+        };
         if (isNaN(userInputNumbers)) {
             return alert('숫자를 입력하세요');
-        }
+        };
         if (new Set(userInputNumbers).size !== 3) {
-            return alert('중복되지 않은 값을 입력하세요.\n 예) 123(o), 122(x)')
-        }
+            return alert('중복되지 않은 값을 입력하세요.\n 예) 123(o), 122(x)');
+        };
         return true;
     };
-    // checkScore
+
     const CountStrikeAndBall = (computerInputNumbers, userInputNumbers) => {
         StrikeCnt = 0;
         BallCnt = 0;
         for (let i = 0; i < userInputNumbers.length; i++) {
             if (userInputNumbers[i] == computerInputNumbers[i]) StrikeCnt++;
             else if (userInputNumbers[i] == computerInputNumbers[(i + 1) % 3] || userInputNumbers[i] == computerInputNumbers[(i + 2) % 3]) BallCnt++;
-        }
-        //console.log(`${StrikeCnt}스트라이크, ${BallCnt}볼`);
+        };
         let Strike = StrikeCnt;
         let Ball = BallCnt;
         return {
             Strike: Strike,
             Ball: Ball
         };
-    }
-    // Hint 제공
+    };
+
     const Hint = Strike_Ball => {
         if (Strike_Ball.Strike === 3) {
             $result.innerHTML = '<p id="result_txt"><Strong> 정답을 맞추셨습니다 <Strong /> <br/></p>';
@@ -73,23 +68,21 @@ export default function BaseballGame() {
             if (Strike_Ball.Strike === 0) $result.innerText = `${Strike_Ball.Ball}볼`;
             else if (Strike_Ball.Ball === 0) $result.innerText = `${Strike_Ball.Strike}스트라이크`;
             else $result.innerText = `${Strike_Ball.Ball}볼 ${Strike_Ball.Strike}스트라이크`;
-        }
-    }
+        };
+    };
 
-    //endGame, resetGame
     const endGame = () => {
         $result.innerHTML += `<p id="endGame_txt">게임을 새로 시작하시겠습니까?<button id="game-restart-button">재시작</button></p>`;
         let Game_Restart_Button = document.querySelector('#game-restart-button');
         let Result_Txt = document.querySelector('#result_txt');
         let EndGame_Txt = document.querySelector('#endGame_txt');
         Game_Restart_Button.onclick = () => {
-            BallCnt = 0;
-            StrikeCnt = 0;
+            $input.value = '';
             $result.removeChild(Result_Txt);
             $result.removeChild(EndGame_Txt);
             setAnswer();
-        }
-    }
+        };
+    };
 
     $form.addEventListener('submit', (event) => {
         event.preventDefault(); // 제출 시 초기화 되는 것 방지
