aaf1ad0cffce248181401e7313c40522330815a6
ellynhan <wown252@naver.com>
2021-11-29 02:57:21 +0900

    feat: 사용자의 입력값이 유효한지 확인하는 메서드 구현

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 export default class BaseballGame {
-  constructor(){
+  constructor() {
       this.submitButton = document.getElementById('submit');
       this.userInput = document.getElementById('user-input');
+      this.answer = this.randomGenerator();
+      this.checkButtonPressed();
   }
 
-  randomGenerator(){
+  randomGenerator() {
     const answer = MissionUtils.Random.pickUniqueNumbersInRange(1,9,3);
     return answer;
   }
 
-  checkButtonPressed(){
+  checkButtonPressed() {
     this.submitButton.addEventListener('click', (e)=>{
       e.preventDefault();
       const userAnswer = this.userInput.value;
-      return userAnswer;
+      const valid = this.checkAnswerValidation(userAnswer);
+      if (valid) {
+        return userAnswer;
+      } else {
+        alert('잘못된 값을 입력했습니다.');
+      }
     });
+  }
+
+  checkAnswerValidation(userAnswer) {
+    if (userAnswer.length != 3) {
+      return false;
+    }
+    if (new Set(userAnswer).size != 3){
+      return false;
+    }
+    if (Number.isNaN(Number(userAnswer))) {
+      return false;
+    }
+    if (userAnswer.includes(0)) {
+      return false;
+    }
+    return true;
   }
 }
 
 const b = new BaseballGame();
-b.checkButtonPressed();
