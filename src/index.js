448fbad73af53ada638534234779ad9b143fb63b
2doo <2doo@kakao.com>
2021-11-29 13:47:25 +0900

    feat: 입력 문자열 검증 및 파싱 메소드

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 export default class BaseballGame {
+  static validNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  #size; // 추리해야 하는 숫자 개수
+
+  constructor(size) {
+    this.#size = size;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
+  static inValidRange(num) {
+    return BaseballGame.validNumbers.includes(num);
+  }
+
+  isValidInputString(inputString) {
+    const inputNumbers = inputString.split("").map(Number);
+    const hasNoNaN = !inputNumbers.some(Number.isNaN);
+    const isValidSize = inputNumbers.length === this.#size;
+    const isValidValue = inputNumbers.every(BaseballGame.inValidRange);
+    const isUnique = inputNumbers.length === new Set(inputNumbers).size;
+    return hasNoNaN && isValidSize && isValidValue && isUnique;
+  }
+
+  parseInputString(inputString) {
+    if (!this.isValidInputString(inputString)) {
+      return null;
+    }
+    return inputString.split("").map(Number);
+  }
+
   play(computerInputNumbers, userInputNumbers) {
     return "결과 값 String";
   }
