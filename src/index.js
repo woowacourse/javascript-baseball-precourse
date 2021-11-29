1b1f8db7eb079a46146c184f906758b5a2be4371
heejaykong <heejay.dev@gmail.com>
2021-11-29 21:23:00 +0900

    refactor: 객체형 프로그래밍에 걸맞게 함수들을 메소드화

--- src/index.js
+++ src/index.js
@@ -1,52 +1,53 @@
 import { INPUT_RULES, ERROR_MESSAGE } from './constants.js';
-
-const $ = (selector) => document.querySelector(selector);
-
-function generateRandomNumbers() {
-  return MissionUtils.Random.pickUniqueNumbersInRange(
-    INPUT_RULES.MIN_OF_RANGE,
-    INPUT_RULES.MAX_OF_RANGE,
-    INPUT_RULES.NUMBER_OF_DIGITS
-  );
-}
-
-function isValid(input) {
-  const regex = /[^1-9]/g;
-  const notValidChars = input.match(regex);
-  const setString = new Set(input);
-
-  if (input === '') {
-    return false;
-  }
-  if (notValidChars) {
-    return false;
-  }
-  if (input.length !== INPUT_RULES.NUMBER_OF_DIGITS) {
-    return false;
-  }
-  if (setString.size !== input.length) {
-    return false;
-  }
-  return true;
-}
-
-function handleSubmit(e) {
-  e.preventDefault();
-  const $userInput = $('#user-input');
-
-  const currentUserInput = $userInput.value;
-  if (!isValid(currentUserInput)) {
-    alert(ERROR_MESSAGE);
-  }
-
-  $userInput.value = '';
-}
-
-const $submitBtn = $('#submit');
-$submitBtn.addEventListener('click', handleSubmit);
+import { $ } from './utils.js';
 
 export default class BaseballGame {
   constructor() {
-    this.computerInputNumbers = generateRandomNumbers();
+    this.computerInputNumbers = this.generateRandomNumbers();
+    this.attachSubmitHandler();
+  }
+
+  generateRandomNumbers() {
+    const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
+      INPUT_RULES.MIN_OF_RANGE,
+      INPUT_RULES.MAX_OF_RANGE,
+      INPUT_RULES.NUMBER_OF_DIGITS
+    );
+    return randomNumbers;
+  }
+
+  attachSubmitHandler() {
+    const $submitBtn = $('#submit');
+    const $userInput = $('#user-input');
+    $submitBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const currentUserInput = $userInput.value;
+      if (!this.isValid(currentUserInput)) {
+        alert(ERROR_MESSAGE);
+      }
+      $userInput.value = '';
+    });
+  }
+
+  isValid(input) {
+    const regex = /[^1-9]/g;
+    const notValidChars = input.match(regex);
+    const setString = new Set(input);
+
+    if (input === '') {
+      return false;
+    }
+    if (notValidChars) {
+      return false;
+    }
+    if (input.length !== INPUT_RULES.NUMBER_OF_DIGITS) {
+      return false;
+    }
+    if (setString.size !== input.length) {
+      return false;
+    }
+    return true;
   }
 }
+
+new BaseballGame();
