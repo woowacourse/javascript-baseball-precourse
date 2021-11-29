620d654bc954c1d6aebe357059d01857b12dc3ba
jhy979 <jhy979@gmail.com>
2021-11-29 16:04:02 +0900

    feat: answer와 input 비교 후 힌트 반환

--- src/computer/getResult.js
+++ src/computer/getResult.js
@@ -1,10 +1,17 @@
+import { RESTART } from "../constatns/constants.js";
 
 export default function getResult(answer, userInput) {
   console.log(answer, userInput);
   const strike = countStrike(answer, userInput);
   const ball = countBall(answer, userInput);
-
-  console.log(`${ball}볼 ${strike}스트라이크`);
+  
+  if(strike === 3){
+    return RESTART;
+  }
+  if(ball === 0 && strike === 0){
+    return "낫싱";
+  }
+  return `${ball}볼 ${strike}스트라이크`;
 }
 
 const countBall = (answer, userInput) => {
