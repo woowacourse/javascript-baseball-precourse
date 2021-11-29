6ead80c7b582083dee6312758dc673811aebb104
moonheekim0118 <hellomooneekim@gmail.com>
2021-11-29 14:46:24 +0900

    feat: GameResult 컴포넌트를 작성한다.

---
+++ src/components/GameResult.js
@@ -0,0 +1,26 @@
+import component from '../core/component.js';
+import { replaceChild, createElement } from '../utils/dom.js';
+import { GAME_STATUS } from '../constants.js';
+
+export default class GameResult extends component {
+  bindEvent() {
+    this.appendRootEvent('click', ({ target }) => {
+      if (target.id !== 'game-restart-button') return;
+      this.props.onClickRestart();
+    });
+  }
+
+  render() {
+    const { hint, gameStatus } = this.props;
+    const renderByStatus = {
+      [GAME_STATUS.READY]: '',
+      [GAME_STATUS.PLAYING]: hint,
+      [GAME_STATUS.END]: `
+      <p>🎉 정답을 맞추셨습니다 🎉</p>
+        <span> 게임을 새로 시작하시겠습니까? </span>
+      <button id="game-restart-button">게임 재시작</button>
+    `,
+    };
+    replaceChild(this.container, createElement(renderByStatus[gameStatus]));
+  }
+}
--- src/constants.js
+++ src/constants.js
@@ -9,12 +9,6 @@ export const MESSAGE = {
     '🚫 올바르지 않은 입력값 입니다! 1~9 사이의 서로 다른 숫자 3자리를 넣어주세요!',
 };
 
-// const END = `
-//   <p>🎉 정답을 맞추셨습니다 🎉</p>
-//   <span> 게임을 새로 시작하시겠습니까? </span>
-//   <button id="game-restart-button">게임 재시작</button>
-// `;
-
 const END = 'END';
 
 export const GAME_RESULT = {
