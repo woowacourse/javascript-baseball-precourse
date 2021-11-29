d4502630e0afe003c952438b6c2d2657f2755dc8
nan-noo <uni613@naver.com>
2021-11-29 16:50:32 +0900

    feat: add comparing functions
    Added in index.js
    - play: comparing input & answer
    - getCountStrikeAndBall
    - getPlayResult

--- src/index.js
+++ src/index.js
@@ -17,15 +17,40 @@ function generateThreeDigitsNumber() {
     while(!isCorrectAnswerNumber(result)){
         result = generateRandomNumber();
     }
-    return result;
+    return result.toString();
 }
 
 function isCorrectInput(input) {
     return input.length === 3 && Number(input) && isCorrectAnswerNumber(Number(input));
 }
 
+function getCountStrikeAndBall(computerNumbers, userNumbers) {
+    const strike = userNumbers.filter((number, index) => number === computerNumbers[index]);
+    const ball = userNumbers
+        .filter((number, index) => (number !== computerNumbers[index]) && computerNumbers.includes(number));
+    return [strike.length, ball.length];
+}
+
+function getPlayResult(numStrike, numBall) {
+    if(numStrike && numBall) return `${numBall}볼 ${numStrike}스트라이크`;
+    else if(numStrike) return `${numStrike}스트라이크`;
+    else if(numBall) return `${numBall}볼`;
+    else return "낫싱";
+}
+
+function play(computerInputNumbers, userInputNumbers) {
+    if(computerInputNumbers === userInputNumbers) return "정답을 맞추셨습니다!";
+
+    const computerNumArray = computerInputNumbers.split('');
+    const userNumArray = userInputNumbers.split('');
+
+    const [strike, ball] = getCountStrikeAndBall(computerNumArray, userNumArray);
+    return getPlayResult(strike, ball);
+}
+
 // main
 const answer = generateThreeDigitsNumber();
+console.log(answer);
 const form = document.querySelector('form');
 const userInput = document.querySelector('#user-input');
 const result = document.querySelector('#result');
