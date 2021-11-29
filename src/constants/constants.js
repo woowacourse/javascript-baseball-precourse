b07c3c86638f103f4ae60c365bb0ed031bc7e1a8
yongrae <kyr9389@naver.com>
2021-11-29 11:01:03 +0900

    chore: 필요없던 구문 삭제
    - module에 대한 이해 부족으로 작성한  의미 없는 코드 삭제

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,5 +8,3 @@ export const Messages = {
 export const HTML = {
     result: "<h3>" + Messages.correct + "</h3>" + "게임을 새로 시작하시겠습니까? <button id=\"game-restart-button\">게임 재시작</button>"
 };
-
-export default {Messages, HTML};
