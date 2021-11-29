3b13302514698afbb1ade91f94b5e6c3f8cdf93c
sangminsong <ssmin0606@hanmail.net>
2021-11-29 20:41:23 +0900

    refactor: 컴퓨터 입력값 생성하는 기능 리팩토링

--- src/index.js
+++ src/index.js
@@ -9,15 +9,13 @@ import { checkResult } from "./util/checkResult.js";
 
 export default class BaseballGame {
   constructor() {
-    this.computerInput = 0;
+    this.computerInput = generateComputerInput();
     this.handleSubmit = this.handleSubmit.bind(this);
     this.restartGame = this.restartGame.bind(this);
   }
   init() {
-    this.generateComputerInput();
     const $form = $("form");
     $form.addEventListener("submit", this.handleSubmit);
-    console.log(this.computerInput);
   }
 
   generateComputerInput() {
@@ -30,7 +28,7 @@ export default class BaseballGame {
       if (isNotDuplicateExist(randomNum + newRandomNum))
         randomNum += newRandomNum;
     }
-    this.computerInput = randomNum;
+    return randomNum;
   }
 
   handleSubmit(event) {
@@ -78,16 +76,3 @@ export default class BaseballGame {
 
 const baseBallGame = new BaseballGame();
 baseBallGame.init();
-
-// 테스트용 콘솔 로그
-// console.log(checkResult("123", "123"), "3스트라이크");
-// console.log(checkResult("123", "345"), "1볼");
-// console.log(checkResult("123", "456"), "낫싱");
-// console.log(checkResult("123", "134"), "1볼 1스트라이크");
-// console.log(checkResult("987", "879"), "3볼");
-// console.log(checkResult("917", "279"), "2볼");
-// console.log(checkResult("234", "239"), "2스트라이크");
-// console.log(baseBallGame.play(123, 456), "통과");
-// console.log(baseBallGame.play(123, "---"), "숫자아님-실패");
-// console.log(baseBallGame.play(123, 111), "중복있음-실패");
-// console.log(baseBallGame.play(123, 12), "길이3아님-실패");
