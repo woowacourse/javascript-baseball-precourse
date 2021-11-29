b3b3c9f8ca0e7165b76821448ad2722ba33141af
Daye Lee <leeda1801@gmail.com>
2021-11-30 01:28:39 +0900

    feat: add exception for invalid input submit

--- src/Submit.js
+++ src/Submit.js
@@ -1,10 +1,16 @@
 import { createUserInputNumbers } from './Numbers.js';
 import checkInputValidation from './Check.js';
 
+function onInvalidInputSubmit() {
+  // eslint-disable-next-line no-alert
+  alert('잘못 입력하셨습니다. 1~9까지의 수를 중복 없이 3개 입력해 주세요.');
+  document.getElementById('user-input').value = '';
+}
+
 export default function onInputSubmit(e) {
   e.preventDefault();
   const userInputNumbers = createUserInputNumbers();
   const validation = checkInputValidation(userInputNumbers);
 
-  console.log(validation);
+  if (!validation) onInvalidInputSubmit();
 }
