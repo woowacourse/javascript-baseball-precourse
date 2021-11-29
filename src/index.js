b5bd67a9341682bb3b0eb65abca599795ae4f35a
연훈 배 <sbaru1997@naver.com>
2021-11-30 07:12:14 +0900

    feat: 게임 결과 출력 기능 구현
    index.js => play
    printGameResult.js printGameResult, printCorrect, printInCorrect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import getUserInput from './input/getUserInput.js';
 import getComputerInput from './input/getComputerInput.js';
 import getGameResult from './play/getGameResult.js';
+import printGameResult from './play/printGameResult.js';
 
 export default function BaseballGame() {
   const computerInputNumbers = getComputerInput();
 
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return getGameResult(computerInputNumbers, userInputNumbers);
+    return printGameResult(getGameResult(computerInputNumbers, userInputNumbers));
   };
 
   getUserInput(computerInputNumbers, this.play);
---
+++ src/play/printGameResult.js
@@ -0,0 +1,28 @@
+function printCorrect($result) {
+  $result.innerHTML = `
+    <div>
+      <div>
+      🎉<strong>정답을 맞추셨습니다</strong>🎉
+      </div>
+      <br/>
+      <div>
+      <span>게임을 새로 시작하시겠습니까?</span>
+      <button id="game-restart-button">게임 재시작</button>
+      </div>
+     </div>`;
+  return true;
+}
+
+function printInCorrect($result, result) {
+  $result.textContent = result;
+  return false;
+}
+
+export default function printGameResult(result) {
+  const $result = document.querySelector('#result');
+
+  if (result === '정답') {
+    return printCorrect($result);
+  }
+  return printInCorrect($result, result);
+}
