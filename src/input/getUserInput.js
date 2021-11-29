768599d16eff25bac94573effb9b7c1a531b4423
연훈 배 <sbaru1997@naver.com>
2021-11-30 00:13:47 +0900

    feat: 사용자 입력 유효성 검사 기능 구현
    getUserInput.js => getUserInput, getInputValue
    getInputValid.js => checkInputLength, checkInputType, checkInputZero, checkInputDuplication

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -1,14 +1,25 @@
+import getInputValid from './getInputValid.js';
+
 function getInputValue() {
-  const $userInput = document.querySelector("#user-input");
-  const numbers = $userInput.value.split("").map((number) => Number(number));
+  const $userInput = document.querySelector('#user-input');
+
+  const numbers = $userInput.value.split('').map(number => Number(number));
+
+  if (!getInputValid(numbers)) {
+    return false;
+  }
   return numbers;
 }
 
 export default function getUserInput(computerInputNumbers, play) {
-  const $submit = document.querySelector("#submit");
-  $submit.addEventListener("click", (event) => {
+  const $submit = document.querySelector('#submit');
+
+  $submit.addEventListener('click', event => {
     event.preventDefault();
+
     const userInputNumbers = getInputValue();
-    play(computerInputNumbers, userInputNumbers);
+    if (userInputNumbers) {
+      play(computerInputNumbers, userInputNumbers);
+    }
   });
 }
