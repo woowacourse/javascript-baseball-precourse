963259da64d6ac081e76884099022d802f3070c6
DomMorello <zuzudnf@gmail.com>
2021-11-29 20:42:27 +0900

    feat: 정답이 아닌 경우 힌트를 보여주는 기능

--- src/view/showResult.js
+++ src/view/showResult.js
@@ -1,16 +1,19 @@
 import { ANSWER } from "../game/getGameResult.js";
 
 function renderAskRestartView() {
-  const $resultDivElement = document.getElementById("result");
-  $resultDivElement.innerHTML = `
+  const $resultDiv = document.getElementById("result");
+  $resultDiv.innerHTML = `
     <div>🎉<strong>정답을 맞추셨습니다!</strong>🎉</div>
     <span>게임을 새로 시작하시겠습니까?</span>
     <button id="game-restart-button">게임 재시작</button>
-  `
+  `;
 }
 
 export default function showResult(resultString) {
   if (resultString === ANSWER) {
     renderAskRestartView();
+    return;
   }
+  const $resultDiv = document.getElementById("result");
+  $resultDiv.innerText = resultString;
 }
