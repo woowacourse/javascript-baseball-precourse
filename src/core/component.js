b24069aab1d61ebdfcf3d7db7ab3e77acd603414
moonheekim0118 <hellomooneekim@gmail.com>
2021-11-29 13:47:03 +0900

    feat : 공용으로 사용 될 컴포넌트 코어를 작성한다.

---
+++ src/core/component.js
@@ -0,0 +1,38 @@
+export default class Component {
+  constructor(container, props) {
+    this.container = container;
+    this.props = props;
+    this.state = {};
+    this.childrens = [];
+  }
+
+  /**
+   * 컴포넌트 초기화 함수
+   * state 와 childrens를 초기화한다.
+   */
+  init() {}
+
+  /**
+   * 컴포넌트 내 이벤트 생성 함수
+   */
+  bindEvent() {}
+
+  /**
+   * 컴포넌트 상태 및 props 에 따라서 뷰를 렌더링 해주는 함수
+   */
+  render() {}
+
+  setState(nextState) {
+    this.state = nextState;
+    this.updateComponent();
+  }
+
+  updateComponent() {
+    this.childrens.forEach(component => component.updateComponent());
+    this.render();
+  }
+
+  appendRootEvent(eventType, eventHandler) {
+    this.container.addEventListener(eventType, eventHandler);
+  }
+}
