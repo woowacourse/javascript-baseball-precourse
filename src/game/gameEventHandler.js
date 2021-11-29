5554eb155a4447e63b504ea6c22bd2af6c5f5cc3
donglee <zuzudnf@gmail.com>
2021-11-29 18:28:07 +0900

    feat: 게임 재시작 버튼 클릭시 새로운 컴퓨터 입력값을 받는 기능 추가
    onUserSubmit으로 콜백함수 분리함
    게임 재시작 이벤트리스너 등록
    index.html 에서 초기 '1볼 1스트라이크' 예시 텍스트 제거

--- src/game/gameEventHandler.js
+++ src/game/gameEventHandler.js
@@ -1,13 +1,28 @@
 import getComputerInput from "../input/getComputerInput.js";
 import getUserInput from "../input/getUserInput.js";
+import showResult from "../view/showResult.js";
+
+function onUserSubmit(event, play, computerInput) {
+  event.preventDefault();
+  const userInput = getUserInput();
+
+  if (userInput) {
+    showResult(play(computerInput, userInput));
+  }
+}
 
 export default function gameEventHandler(play) {
-  const $userSubmitButton = document.querySelector("#submit");
-  const computerInput = getComputerInput(); //let으로 바꿔야 재시작할 때 재할당할 수 있을 것이다
+  const $userSubmitButton = document.getElementById("submit");
+  const $userResultDiv = document.getElementById("result");
+  let computerInput = getComputerInput();
 
-  $userSubmitButton.addEventListener("click", (event) => {
-    event.preventDefault(); // 개발을 위해서 잠시 페이지 새로고침을 일단 막아둠
-    const userInput = getUserInput();
-    if (userInput) play(computerInput, userInput);
+  $userSubmitButton.addEventListener("click", (event) =>
+    onUserSubmit(event, play, computerInput)
+  );
+  $userResultDiv.addEventListener("click", (event) => {
+    if (event.target.id === "game-restart-button") {
+      computerInput = getComputerInput();
+      // deleteAskRestartView();
+    }
   });
 }
