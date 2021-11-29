1e50c0522b180430ef5133dd14a87d867c06c58d
nan-noo <uni613@naver.com>
2021-11-30 00:00:52 +0900

    feat: create constants.js
    constants file
    - game results, html, texts

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const gameResults = {
+    NOTHING: "낫싱",
+    BALL: "볼",
+    STRIKE: "스트라이크",
+    CORRECT: "정답",
+};
+
+export const NEW_GAME_HTML = 
+    '<p><strong>정답을 맞추셨습니다!</strong></p>' +
+    '<p>게임을 새로 시작하시겠습니까? ' +
+        '<button id="game-restart-button">게임 재시작</button>' +
+    '</p>';
+
+export const ALERT_MESSAGE = '1 ~ 9까지 수를 중복없이 3개 입력해주세요:D';
