cbca9186164d6d5f358eb0e48f264ff6e1af2088
gxxrxn <kkr1226@naver.com>
2021-11-30 19:38:18 +0900

    feat: 사용자에게 입력 받기
    - BaseballGame 클래스에 userInputNumbers 프로퍼티 추가
    - submit 버튼 클릭 시 userInputNumbers 프로퍼티에 사용자 입력값 저장

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import { pickRandomNumbers } from './utils/number-picker.js';
 
+const $userInput = document.getElementById("user-input");
+const $submitBtn = document.getElementById("submit");
+const $resultDiv = document.getElementById("result");
+
 export default class BaseballGame {
     constructor() {
         this.computerInputNumbers = pickRandomNumbers(3);
-        console.log(this.computerInputNumbers);
+        this.userInputNumbers = null;
+
+        this.bindSubmitEvent();
+    }
+
+    bindSubmitEvent() {
+        $submitBtn.addEventListener('click', event => {
+            event.preventDefault();
+            this.userInputNumbers = $userInput.value;
+            console.log(this.userInputNumbers);
+        });
     }
 }
 
