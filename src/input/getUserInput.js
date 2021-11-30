3859d2492c62c83d2c81784d734cf7ba19a66f3a
DomMorello <zuzudnf@gmail.com>
2021-11-30 11:52:21 +0900

    refactor: alertErrorMessage 함수 view 디렉토리로 이동

--- src/input/getUserInput.js
+++ src/input/getUserInput.js
@@ -1,28 +1,5 @@
-import isValidInput, {
-  THREE_DIGIT_ERR,
-  ONE_TO_NINE_ERR,
-  DUPLICATE_ERR,
-} from "./isValidInput.js";
-
-const THREE_DIGIT_ERR_MSG = "입력 오류! 세 자리의 숫자만 입력해주세요.";
-const ONE_TO_NINE_ERR_MSG = "입력 오류! 1~9 까지의 숫자만 입력해주세요.";
-const DUPLICATE_ERR_MSG = "입력 오류! 중복없는 세 자리 숫자만 입력해주세요.";
-
-function alertErrorMessage(error) {
-  if (error === THREE_DIGIT_ERR) {
-    alert(THREE_DIGIT_ERR_MSG);
-    return false;
-  }
-  if (error === ONE_TO_NINE_ERR) {
-    alert(ONE_TO_NINE_ERR_MSG);
-    return false;
-  }
-  if (error === DUPLICATE_ERR) {
-    alert(DUPLICATE_ERR_MSG);
-    return false;
-  }
-  return true;
-}
+import isValidInput from "./isValidInput.js";
+import alertErrorMessage from "../view/alertErrorMessage.js";
 
 export default function getUserInput() {
   let userInput = null;
---
+++ src/view/alertErrorMessage.js
@@ -0,0 +1,25 @@
+import {
+  THREE_DIGIT_ERR,
+  ONE_TO_NINE_ERR,
+  DUPLICATE_ERR,
+} from "../input/isValidInput.js";
+
+const THREE_DIGIT_ERR_MSG = "입력 오류! 세 자리의 숫자만 입력해주세요.";
+const ONE_TO_NINE_ERR_MSG = "입력 오류! 1~9 까지의 숫자만 입력해주세요.";
+const DUPLICATE_ERR_MSG = "입력 오류! 중복없는 세 자리 숫자만 입력해주세요.";
+
+export default function alertErrorMessage(error) {
+  if (error === THREE_DIGIT_ERR) {
+    alert(THREE_DIGIT_ERR_MSG);
+    return false;
+  }
+  if (error === ONE_TO_NINE_ERR) {
+    alert(ONE_TO_NINE_ERR_MSG);
+    return false;
+  }
+  if (error === DUPLICATE_ERR) {
+    alert(DUPLICATE_ERR_MSG);
+    return false;
+  }
+  return true;
+}
