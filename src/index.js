e11cc01928d963a89d89be10cf6ba9d3f5cef6ba
Daye Lee <leeda1801@gmail.com>
2021-11-30 23:34:12 +0900

    refactor: separate BaseballGame into functions

--- src/index.js
+++ src/index.js
@@ -8,20 +8,27 @@ import {
 } from './Result.js';
 import { countStrike } from './Count.js';
 
-window.computerInputNumbers = createComputerInputNumbers();
-document.getElementById('submit').addEventListener('click', onInputSubmit);
+function initialize() {
+  window.computerInputNumbers = createComputerInputNumbers();
+  document.getElementById('submit').addEventListener('click', onInputSubmit);
+}
+
+function getResultString(computerInputNumbers, userInputNumbers) {
+  const nothing = getNothingHint(computerInputNumbers, userInputNumbers);
+  if (nothing) return nothing;
+
+  let result = getBallHint(computerInputNumbers, userInputNumbers);
+  const strike = countStrike(computerInputNumbers, userInputNumbers);
+  if (strike === 3) result = getGameOver();
+  else if (strike) result = getStrikeHint(strike, result);
+
+  return result;
+}
 
 export default function BaseballGame() {
-  window.play = (computerInputNumbers, userInputNumbers) => {
-    const nothing = getNothingHint(computerInputNumbers, userInputNumbers);
-    if (nothing) return nothing;
-
-    let result = getBallHint(computerInputNumbers, userInputNumbers);
-    const strike = countStrike(computerInputNumbers, userInputNumbers);
-    if (strike === 3) result = getGameOver();
-    else if (strike) result = getStrikeHint(strike, result);
-
-    return result;
+  initialize();
+  window.play = function (computerInputNumbers, userInputNumbers) {
+    return getResultString(computerInputNumbers, userInputNumbers);
   };
 }
 
