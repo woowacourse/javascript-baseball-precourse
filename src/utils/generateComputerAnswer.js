185a7113f5c0421e8b58398068bea124d86a0ab8
JuYoung <ken44929@gmail.com>
2021-11-30 22:13:11 +0900

    refactor: 컴퓨터가 입력하는 답안을 생성하는 함수 이름 변경

--- src/utils/generateComputerAnswer.js
+++ src/utils/generateComputerAnswer.js
@@ -1,11 +1,9 @@
-const generateAnswer = () => {
+export const generateComputerAnswer = () => {
   const answer = [];
 
   while (answer.length < 3) {
     const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
     if (!answer.includes(randomNum)) answer.push(randomNum);
   }
-  return answer.join('');
+  return Number(answer.join(''));
 };
-
-export default generateAnswer;
