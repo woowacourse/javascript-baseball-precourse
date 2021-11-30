de114947f66721c1caefa9841c3cf1bc98468298
mieumje <66072832+mieumje@users.noreply.github.com>
2021-11-30 23:41:07 +0900

    style : 불필요한 주석 및 공백 제거

--- src/index.js
+++ src/index.js
@@ -5,36 +5,25 @@ import { checkInput } from './checkInput.js';
 import { CountStrikeAndBall } from './CountStrikeAndBall.js';
 import { endGame } from './endGame.js';
 import { StrikeAndBall } from './ResultText.js';
+
 export default function BaseballGame() {
     let computerInputNumbers = setAnswer();
-
     const play = (computerInputNumbers, userInputNumbers) => {
         let result_game_text = '';
         if (!checkInput(userInputNumbers)) return;
         else {
             console.log(userInputNumbers);
             let Strike_Ball = CountStrikeAndBall(computerInputNumbers, userInputNumbers);
-            //Hint(Strike_Ball);
             result_game_text = Hint(Strike_Ball);
         };
         console.log(result_game_text);
         return result_game_text;
     };
     const Hint = Strike_Ball => {
-        /*if (Strike_Ball.Strike === 3) {
-            $result.innerHTML = CORRECT_ANSWER;
-            computerInputNumbers = setAnswer();
-            endGame();
-            return `${Strike_Ball.Strike}${Strike}`;
-        }
-        else if (Strike_Ball.Strike === 0 && Strike_Ball.Ball === 0) return $result.innerText = NOTHING_ANSWER;
-        else if (Strike_Ball.Strike === 0) return $result.innerText = `${Strike_Ball.Ball}${Ball}`;
-        else if (Strike_Ball.Ball === 0) return $result.innerText = `${Strike_Ball.Strike}${Strike}`;
-        return $result.innerText = `${Strike_Ball.Ball}${Ball} ${Strike_Ball.Strike}${Strike}`;*/
         if (Strike_Ball.Strike === 3) {
             $result.innerHTML = CORRECT_ANSWER;
-            computerInputNumbers = setAnswer();
             endGame();
+            computerInputNumbers = setAnswer();
             return `${Strike_Ball.Strike}${Strike}`;
         }
         return $result.innerText = `${StrikeAndBall[Strike_Ball.Strike][Strike_Ball.Ball]}`;
--- src/setAnswer.js
+++ src/setAnswer.js
@@ -1,9 +1,5 @@
 export const setAnswer = () => {
     let computerInputNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    //let computerInputNumbers = generateRandNumbers;
     console.log(computerInputNumbers);
     return computerInputNumbers;
 };
-
-
-
