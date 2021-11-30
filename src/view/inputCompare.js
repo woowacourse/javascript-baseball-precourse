251d6aac112d3397d0afbdeb26fe1e3419f9c30a
simyeen <simyeen@naver.com>
2021-11-30 22:26:24 +0900

    Feat : 받은 입력과 저장된 답을 비교하고 각 상황을 반환한다.

---
+++ src/view/inputCompare.js
@@ -0,0 +1,48 @@
+function getGameResultText(strikeCount, ballCount) {
+  if (strikeCount === 3) {
+    return "정답";
+  }
+  if (strikeCount === 0 && ballCount) {
+    return `${ballCount}볼`;
+  }
+  if (strikeCount && ballCount === 0) {
+    return `${strikeCount}스트라이크`;
+  }
+  if (strikeCount && ballCount) {
+    return `${ballCount}볼 ${strikeCount}스트라이크`;
+  }
+  if (strikeCount === 0 && ballCount === 0) {
+    return "낫싱";
+  }
+}
+
+function checkStrikeBall(el, index, countArray, computerInputArray) {
+  if (!computerInputArray.includes(el)) {
+    return;
+  } else if (computerInputArray[index] === el) {
+    countArray[0]++;
+  } else {
+    countArray[1]++;
+  }
+}
+
+function checkCount(computerInputNumbers, userInputNumbers) {
+  const countArray = [0, 0];
+  const computerInputArray = String(computerInputNumbers).split("");
+  const userInputArray = String(userInputNumbers).split("");
+
+  userInputArray.forEach((el, index) => {
+    checkStrikeBall(el, index, countArray, computerInputArray);
+  });
+
+  return countArray;
+}
+
+export default function inputCompare(computerInputNumbers, userInputNumbers) {
+  const [strikeCount, ballCount] = checkCount(
+    computerInputNumbers,
+    userInputNumbers
+  );
+
+  return getGameResultText(strikeCount, ballCount);
+}
