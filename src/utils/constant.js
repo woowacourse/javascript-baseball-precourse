f5989664b314a2aaacc5bb72fba63ccbbd0abbb8
moonyerim2 <moonyerim2@gmail.com>
2021-11-30 15:59:50 +0900

    feat: 상수 모듈 생성 - 제약조건 - 볼카운트 출력용 텍스트 - 상황별 출력 메세지

---
+++ src/utils/constant.js
@@ -0,0 +1,17 @@
+export const CONSTRAIN = {
+  MAX_OF_RANGE: 9,
+  MIN_OF_RANGE: 1,
+  INPUT_LENGTH: 3,
+};
+
+export const COUNT = {
+  NOTHING: '낫싱',
+  STRIKE: '스트라이크',
+  BALL: '볼',
+};
+
+export const MESSAGE = {
+  INPUT_ERR: '1~9까지의 수로 이루어진 중복없는 숫자를 입력해 주세요.',
+  WIN: '🎉  정답을 맞추셨습니다!  🎉',
+  RESTART: '게임을 새로 시작하시겠습니까?',
+};
