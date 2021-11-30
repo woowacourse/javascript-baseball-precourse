50d87a85c8868e00be2cd5deea5c400fd8296c2b
simyeen <simyeen@naver.com>
2021-11-30 23:01:54 +0900

    Docs : 파일구조 업데이트

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import startGame from "./view/startGame.js";
 
 export default function BaseballGame() {
   const computerInputNumbers = getComputerInputNumbers();
-  console.log("정답", computerInputNumbers);
 
   this.play = function (computerInputNumbers, userInputNumbers) {
     return inputCompare(computerInputNumbers, userInputNumbers);
--- src/input/getUserInputNumbers.js
+++ src/input/getUserInputNumbers.js
@@ -4,7 +4,6 @@ const userInput = document.getElementById("user-input");
 
 export default function getUserInputNumbers() {
   if (validate(userInput.value)) {
-    console.log("올바른 입력", userInput.value);
     return userInput.value;
   }
   return "";
