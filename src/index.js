a9ea07aac7fb6d1465c8a4a0fa150171001c1222
gxxrxn <kkr1226@naver.com>
2021-11-30 22:00:19 +0900

    feat: 컴퓨터 숫자와 입력된 숫자 볼, 스트라이크 계산 기능 구현
    - utils/ball-counter에 볼, 스트라이크 계산하는 함수 getBallStrikeCount 구현
      - 리턴 값 : [볼 카운트, 스트라이크 카운트]
      - 리턴 타입 : 2개의 숫자로 이뤄진 배열

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { pickRandomNumbers } from './utils/number-picker.js';
-import { removeSpace, isInvalid } from './utils/input-validator.js'
+import { removeSpace, isInvalid } from './utils/input-validator.js';
+import { getBallStrikeCount } from './utils/ball-counter.js';
 
 const $userInput = document.getElementById('user-input');
 const $submitBtn = document.getElementById('submit');
@@ -13,23 +14,28 @@ export default class BaseballGame {
     }
 
     bindSubmitEvent() {
-        $submitBtn.addEventListener('click', event => {
-            event.preventDefault();
-            const inputValue = removeSpace($userInput.value);
-            
-            if (isInvalid(inputValue, 3)) {
-                $userInput.value = '';
-                return;
-            }
+        $submitBtn.addEventListener('click', this.submitEvent.bind(this));
+    }
 
-            this.userInputNumbers = inputValue;
-            this.play(this.computerInputNumbers, this.userInputNumbers);
-        });
+    submitEvent(event) {
+        event.preventDefault();
+        const inputValue = removeSpace($userInput.value);
+        
+        if (isInvalid(inputValue, 3)) {
+            $userInput.value = '';
+            return;
+        }
+
+        this.userInputNumbers = inputValue;
+        this.play(this.computerInputNumbers, this.userInputNumbers);
     }
 
     play(computerInputNumbers, userInputNumbers) {
         console.log(computerInputNumbers);
         console.log(userInputNumbers);
+        
+        const [ball, strike] = getBallStrikeCount(computerInputNumbers, userInputNumbers);
+        console.log(`${ball}볼 ${strike}스트라이크`);
     }
 }
 
---
+++ src/utils/ball-counter.js
@@ -0,0 +1,51 @@
+export const getStrikeCount = (computerInputNumbers, userInputNumbers) => {
+    let count = 0;
+
+    if (computerInputNumbers.length !== userInputNumbers.length) {
+        return 0;
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (computerInputNumbers[i] === userInputNumbers[i]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+export const getBallCount = (computerInputNumbers, userInputNumbers) => {
+    let count = 0;
+
+    if (computerInputNumbers.length !== userInputNumbers.length) {
+        return 0;
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (computerInputNumbers.includes(userInputNumbers[i])) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+export const getBallStrikeCount = (computerInputNumbers, userInputNumbers) => {
+    let ballCount = 0;
+    let strikeCount = 0;
+
+    if (computerInputNumbers.length !== userInputNumbers.length) {
+        return 0;
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (computerInputNumbers[i] === userInputNumbers[i]) {
+            strikeCount++;
+        }
+        else if (computerInputNumbers.includes(userInputNumbers[i])) {
+            ballCount++;
+        }
+    }
+
+    return [ballCount, strikeCount];
+}
