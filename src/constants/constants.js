a849de3761146636bc09b6b2efbb01c030a01cf3
prefer2 <sunho6200@gmail.com>
2021-11-30 15:08:39 +0900

    style: 여러 export 하나의 객체로 구성

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
-export const $userInput = document.getElementById("user-input");
-export const $submit = document.getElementById("submit");
-export const $result = document.getElementById("result");
+const $userInput = document.getElementById("user-input");
+const $submit = document.getElementById("submit");
+const $result = document.getElementById("result");
+
+export { $userInput, $submit, $result };
