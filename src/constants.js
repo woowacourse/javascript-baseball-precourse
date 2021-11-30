2b8c98b0947bc4c3420e1909d2aa6293f14716b6
heejaykong <heejay.dev@gmail.com>
2021-11-30 19:20:05 +0900

    feat: 컴퓨터인풋 사용자인풋 비교해 힌트(또는 정답입니다) 출력 구현

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,11 @@ export const INPUT_RULES = {
 };
 
 export const ERROR_MESSAGE = '잘못 입력하셨습니다. 다시 입력해 주세요.';
+
+export const RESULT_MESSAGE = {
+  NOTHING: '낫싱',
+  CORRECT: '정답을 맞추셨습니다!',
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  ASK_RESTART: '게임을 새로 시작하시겠습니까?',
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { INPUT_RULES, ERROR_MESSAGE } from './constants.js';
+import { INPUT_RULES, ERROR_MESSAGE, RESULT_MESSAGE } from './constants.js';
 import { $ } from './utils.js';
 
 export default class BaseballGame {
@@ -24,6 +24,9 @@ export default class BaseballGame {
       const currentUserInput = $userInput.value;
       if (!this.isValid(currentUserInput)) {
         alert(ERROR_MESSAGE);
+      } else {
+        const parsedUserInput = this.parseStringtoIntArray(currentUserInput);
+        this.play(this.computerInputNumbers, parsedUserInput);
       }
       $userInput.value = '';
     });
@@ -47,6 +50,47 @@ export default class BaseballGame {
       return false;
     }
     return true;
+  }
+
+  parseStringtoIntArray(stringOfInt) {
+    return stringOfInt.split('').map(Number);
+  }
+
+  play(computerInputNumbers, userInputNumbers) {
+    let ballCount = 0;
+    let strikeCount = 0;
+    for (let i = 0; i < INPUT_RULES.NUMBER_OF_DIGITS; i++) {
+      if (computerInputNumbers[i] === userInputNumbers[i]) {
+        strikeCount += 1;
+      } else if (computerInputNumbers.includes(userInputNumbers[i])) {
+        ballCount += 1;
+      }
+    }
+    const resultString = this.createResultMessage(ballCount, strikeCount);
+    this.paintResultMessage(resultString);
+  }
+
+  createResultMessage(ballCount, strikeCount) {
+    if (ballCount === 0 && strikeCount === 0) {
+      return RESULT_MESSAGE.NOTHING;
+    }
+    if (strikeCount === 3) {
+      this.endGame = true;
+      this.askRestartGame();
+      return RESULT_MESSAGE.CORRECT;
+    }
+    if (ballCount && !strikeCount) {
+      return `${ballCount}${RESULT_MESSAGE.BALL}`;
+    }
+    if (!ballCount && strikeCount) {
+      return `${strikeCount}${RESULT_MESSAGE.STRIKE}`;
+    }
+    return `${ballCount}${RESULT_MESSAGE.BALL} ${strikeCount}${RESULT_MESSAGE.STRIKE}`;
+  }
+
+  paintResultMessage(resultString) {
+    const $resultText = $('#result');
+    $resultText.innerHTML = resultString;
   }
 }
 
