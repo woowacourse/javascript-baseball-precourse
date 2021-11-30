1191bde6a03c2e0c0ea4be85b5dadebc5d629139
gytks4 <gytks4@naver.com>
2021-11-30 21:36:45 +0900

    refactor: 조건을 함수화

--- src/BaseballGameView.js
+++ src/BaseballGameView.js
@@ -11,23 +11,31 @@ export class BaseballGame {
   calculateBall(computerInputNumbers, userInputNumbers) {
     let ball = 0;
     for (let i = 0; i < computerInputNumbers.length; i++) {
-      if (
-        computerInputNumbers.includes(userInputNumbers[i]) &&
-        computerInputNumbers[i] !== userInputNumbers[i]
-      ) {
+      if (this.isBall(computerInputNumbers, userInputNumbers, i)) {
         ball++;
       }
     }
     return ball > 0 ? `${ball}볼` : '';
   }
 
+  isBall(computerInputNumbers, userInputNumbers, i) {
+    return (
+      computerInputNumbers.includes(userInputNumbers[i]) &&
+      computerInputNumbers[i] !== userInputNumbers[i]
+    );
+  }
+
   calculateStrike(computerInputNumbers, userInputNumbers) {
     let strike = 0;
     for (let i = 0; i < computerInputNumbers.length; i++) {
-      if (computerInputNumbers[i] === userInputNumbers[i]) {
+      if (this.isStrike(computerInputNumbers, userInputNumbers, i)) {
         strike++;
       }
     }
     return strike > 0 ? `${strike}스트라이크` : '';
+  }
+
+  isStrike(computerInputNumbers, userInputNumbers, i) {
+    return computerInputNumbers[i] === userInputNumbers[i];
   }
 }
