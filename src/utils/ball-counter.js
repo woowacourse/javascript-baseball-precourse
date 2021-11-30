838fded66f4496d856626eeae56059eacc5bc720
gxxrxn <kkr1226@naver.com>
2021-11-30 23:30:59 +0900

    refactor: utils/ball-counter 필요없는 함수 삭제
    - getBallCount, getStrikeCount 함수 삭제

--- src/utils/ball-counter.js
+++ src/utils/ball-counter.js
@@ -1,35 +1,3 @@
-export const getStrikeCount = (computerInputNumbers, userInputNumbers) => {
-    let count = 0;
-
-    if (computerInputNumbers.length !== userInputNumbers.length) {
-        return 0;
-    }
-
-    for (let i = 0; i < 3; i++) {
-        if (computerInputNumbers[i] === userInputNumbers[i]) {
-            count++;
-        }
-    }
-
-    return count;
-}
-
-export const getBallCount = (computerInputNumbers, userInputNumbers) => {
-    let count = 0;
-
-    if (computerInputNumbers.length !== userInputNumbers.length) {
-        return 0;
-    }
-
-    for (let i = 0; i < 3; i++) {
-        if (computerInputNumbers.includes(userInputNumbers[i])) {
-            count++;
-        }
-    }
-
-    return count;
-}
-
 export const getBallStrikeCount = (computerInputNumbers, userInputNumbers) => {
     let ballCount = 0;
     let strikeCount = 0;
