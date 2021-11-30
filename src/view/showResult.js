6b86ddb0ab29e24b5f4a2999cded5dabd8340401
simyeen <simyeen@naver.com>
2021-11-30 22:26:58 +0900

    Feat : 정답과 정답이 아닐 때를 나뉘어서 출력한다.

--- src/view/showResult.js
+++ src/view/showResult.js
@@ -1,1 +1,22 @@
-export default function showResult(params) {}
+function showCorrectResult(resultDiv) {
+  resultDiv.innerHTML = `<div>
+    <strong>🎉정답입니다🎉</strong>
+    <br/>
+    <p>게임을 새로 시작하시겠습니까?</p>
+    <button id="game-restart-button">게임 재시작</button>
+   </div>`;
+}
+
+function showIncorrectResult(resultDiv, result) {
+  resultDiv.textContent = result;
+}
+
+export default function showResult(result) {
+  const resultDiv = document.getElementById("result");
+  if (result === "정답") {
+    showCorrectResult(resultDiv);
+    return true;
+  }
+  showIncorrectResult(resultDiv, result);
+  return false;
+}
