b764438158c124d858f76e4c424400cce56d6023
KIM YEJI <rladpwl0512@gmail.com>
2021-11-30 16:59:03 +0900

    refactor: getNumArr 함수에서 메서드 체이닝 활용

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,11 @@
 export const $ = selector => document.querySelector(selector);
 
-// eslint-disable-next-line prettier/prettier
-export const getNumArr = notNumArr => notNumArr.toString().split('').map(num => Number(num));
+export const getNumArr = notNumArr => {
+  return notNumArr
+    .toString()
+    .split('')
+    .map(num => Number(num));
+};
 
 export const resetOutput = () => {
   $('#result').innerHTML = '';
