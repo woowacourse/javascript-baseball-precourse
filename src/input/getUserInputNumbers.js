42bcc3acdb629477b98adc23898887c5d7d1b18a
simyeen <simyeen@naver.com>
2021-11-30 22:24:14 +0900

    feat : Get uerInput numbers and do it validate

--- src/input/getUserInputNumbers.js
+++ src/input/getUserInputNumbers.js
@@ -4,7 +4,8 @@ const userInput = document.getElementById("user-input");
 
 export default function getUserInputNumbers() {
   if (validate(userInput.value)) {
+    console.log("올바른 입력", userInput.value);
     return userInput.value;
   }
-  return Null;
+  return "";
 }
