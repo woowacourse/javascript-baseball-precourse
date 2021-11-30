01449512f8115b51c1f3a9550a3331ac85b25d4d
junzzi <jjh01027717502@gmail.com>
2021-11-30 17:43:39 +0900

    refactor: add  MIN_RANGE & MAX_RANGE constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,3 +4,5 @@ export const BALL = "볼";
 export const FINISH =
   "🎉 <b>정답을 맞추셨습니다.</b> 🎉 <br/> <br/> 게임을 다시 시작하시겠습니까?";
 export const PLAIN_TEXT = "";
+export const MIN_RANGE = 1;
+export const MAX_RANGE = 9;
