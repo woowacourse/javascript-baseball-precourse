0855ba22e2a5098d0eef960766bcf23618a62b86
simyeen <simyeen@naver.com>
2021-11-30 22:27:20 +0900

    Feat : 버튼을 누르면 게임이 시작한다. 이때 정답일 때는 재시작하게한다.

--- src/view/startGame.js
+++ src/view/startGame.js
@@ -1,1 +1,31 @@
-export default function startGame(params) {}
+import getComputerInputNumbers from "../input/getComputerInputNumbers.js";
+import getUserInputNumbers from "../input/getUserInputNumbers.js";
+import showResult from "./showResult.js";
+
+const userInput = document.getElementById("user-input");
+const submitButton = document.getElementById("submit");
+const resultDiv = document.getElementById("result");
+
+function initForm() {
+  userInput.value = "";
+  resultDiv.textContent = "";
+}
+
+export default function startGame(computerInputNumbers, callbackPlay) {
+  submitButton.addEventListener("click", () => {
+    console.log("버튼 실행", computerInputNumbers);
+    event.preventDefault();
+
+    const userInputNumbers = getUserInputNumbers();
+    if (userInputNumbers !== "") {
+      showResult(callbackPlay(computerInputNumbers, userInputNumbers));
+    }
+  });
+
+  result.addEventListener("click", ({ target }) => {
+    if (target.id === "game-restart-button") {
+      initForm();
+      computerInputNumbers = getComputerInputNumbers();
+    }
+  });
+}
