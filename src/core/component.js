2a27444d62734b0a97f5f7886772fb643f2ee14a
moonheekim0118 <hellomooneekim@gmail.com>
2021-11-30 14:39:40 +0900

    refactor : component core의 메서드 선언 순서 및 주석을 변경한다.

--- src/core/component.js
+++ src/core/component.js
@@ -11,24 +11,24 @@ export default class Component {
     this.bindEvent();
   }
 
-  /**
-   *  컴포넌트 state 초기화 함수
-   */
+  // 컴포넌트 내부의 state를 초기화 한다.
   init() {}
 
+  // 컴포넌트의 자식 컴포넌트 배열을 초기화한다.
   initChildrens() {}
 
+  // 컴포넌트의 DOM 요소를 초기화 한다.
   initDoms() {}
 
-  /**
-   * 컴포넌트 내 이벤트 생성 함수
-   */
+  // 컴포넌트내의 이벤트를 초기화 한다.
   bindEvent() {}
 
-  /**
-   * 컴포넌트 상태 및 props 에 따라서 뷰를 렌더링 해주는 함수
-   */
+  // 컴포넌트 상태 및 props 에 따라서 뷰를 렌더링 해주는 함수
   render() {}
+
+  appendRootEvent(type, handler) {
+    this.container.addEventListener(type, handler);
+  }
 
   setState(nextState) {
     this.state = { ...this.state, ...nextState };
@@ -43,9 +43,5 @@ export default class Component {
   updateComponent() {
     this.render();
     this.childrens.forEach(component => component.setProps(this.state));
-  }
-
-  appendRootEvent(eventType, eventHandler) {
-    this.container.addEventListener(eventType, eventHandler);
   }
 }
