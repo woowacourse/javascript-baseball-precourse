263e74e884554df941e1f152f420c9e863b6bf64
simyeen <simyeen@naver.com>
2021-11-30 22:25:02 +0900

    Feat : input을 받고 올바른 타입, 길이, 범위인지 검사

--- src/input/validate.js
+++ src/input/validate.js
@@ -1,16 +1,16 @@
 function printAlert(state) {
   switch (state) {
     case "NaN":
-      alert("잘못 된 입력입니다! 숫자만 입력 가능합니다! 예)139");
+      alert("숫자만 입력하시오.");
       break;
     case "length":
-      alert("잘못 된 입력입니다! 숫자는 반드시 3개를 입력해야합니다! 예)139");
+      alert("3자리의 숫자만 입력하시오.");
       break;
     case "range":
-      alert("잘못 된 입력입니다! 중복된 숫자는 입력할 수 없습니다! 예)139");
+      alert("0~9까지의 숫자만 입력하시오.");
       break;
     case "duplicate":
-      alert("잘못 된 입력입니다! 중복된 숫자는 입력할 수 없습니다! 예)139");
+      alert("중복되지 않게 입력하시오.");
       break;
     default:
       break;
@@ -19,42 +19,35 @@ function printAlert(state) {
   return false;
 }
 
-function checkType(notCheckedInputs) {
-  [...notCheckedInputs].forEach((num) => {
-    if (typeof num === "number") {
-      return true;
-    }
-  });
-  return false;
+function checkType(userInputs) {
+  if (isNaN(Number(userInputs))) {
+    return false;
+  }
+  return true;
 }
 
-function checkLength(notCheckedInputs) {
-  if (notCheckedInputs.length === 3) {
+function checkLength(userInputs) {
+  if (userInputs.length === 3) {
     return true;
   }
   return false;
 }
 
-function checkDuplicate(notCheckedInputs) {
-  if ([...new Set(notCheckedInputs)].length === 3) {
+function checkDuplicate(userInputs) {
+  if ([...new Set(userInputs)].length === 3) {
     return true;
   }
   return false;
 }
 
 export default function validate(userInputs) {
-  console.log("검증 시작", userInputs);
-
-  const notCheckedInputs = Number(userInputs);
-  console.log(notCheckedInputs);
-
-  if (!checkType(notCheckedInputs)) {
+  if (!checkType(userInputs)) {
     return printAlert("NaN");
   }
-  if (!checkLength(notCheckedInputs)) {
+  if (!checkLength(userInputs)) {
     return printAlert("length");
   }
-  if (!checkDuplicate(notCheckedInputs)) {
+  if (!checkDuplicate(userInputs)) {
     return printAlert("duplicate");
   }
 
