1b6d4dc802ffb1bac91f8c54979fff107a9e4feb
yongrae <kyr9389@naver.com>
2022-01-06 18:04:48 +0900

    refactor: 기타 파일로부터 Model 의존성 끊어내기

--- src/game/baseballGame.js
+++ src/game/baseballGame.js
@@ -1,5 +1,4 @@
 import judgeInputNumber from "../utils/judgeInputNumber.js";
-import resultMessages from "./resultMessages.js";
 
 function uniqueNumberPush(computerNumber, randomNumber) {
     if(!computerNumber.includes(randomNumber)) {
@@ -19,6 +18,7 @@ class BaseballGame {
 
     constructor() {
         this.state.computerNumber = [];
+        this.state.gameState = true;
     }
 
     setComputerNumber() {
@@ -29,13 +29,14 @@ class BaseballGame {
         return this.state;
     }
 
-
     play(userInputNumber) {
         judgeInputNumber(this.state, userInputNumber);
+        if(this.state.strikes === 3) this.state.gameState = false;
         return this.state;
     }
 
     replay() {
+        this.state.gameState = true;
         this.state.computerNumber = [];
         this.setComputerNumber();
     }
