353521fc6f89e716fd7f69b29e9da412d245a926
kknyapple <kknyapple@gmail.com>
2022-07-01 03:48:39 +0900

    입력 숫자 확인 함수 나누기

--- src/validation.js
+++ src/validation.js
@@ -1,22 +1,50 @@
 //2. 입력한 숫자 확인
 
 export default function isValid(input) {
-  if (isNaN(input)) {
-    console.log("숫자를 입력해주세요");
+  if (!isNaN(input)) {
+    checkNumber(input);
   } else {
-    if (input.length !== 3) {
-      console.log("세자리 숫자를 입력해주세요");
-    } else {
-      if (input.includes(0)) {
-        console.log("1부터 9까지 숫자를 입력해 주세요");
-      } else {
-        const inputArray = [...new Set(input)];
-        if (inputArray.length !== 3) {
-          console.log("중복 없이 입력해 주세요");
-        } else {
-          return true;
-        }
-      }
-    }
+    alert("숫자를 입력해주세요");
+  }
+}
+
+function checkDuplication(number) {
+  const numberArray = [...new Set(number)];
+  if (numberArray.length !== 3) {
+    return false;
+  }
+  return true;
+}
+
+function checkNoZero(number) {
+  if (number.includes(0)) {
+    return false;
+  }
+  return true;
+}
+
+function checkThreeDigitNumber(number) {
+  if (number.length !== 3) {
+    return false;
+  }
+  return true;
+}
+
+function checkNumber(number) {
+  if (checkThreeDigitNumber(number) === false) {
+    alert("세자리 숫자를 입력해주세요");
+  } else if (
+    checkNoZero(number) === false &&
+    checkThreeDigitNumber(number) === true
+  ) {
+    alert("1부터 9까지 숫자를 입력해 주세요");
+  } else if (
+    checkDuplication(number) === false &&
+    checkThreeDigitNumber(number) === true &&
+    checkNoZero(number) === true
+  ) {
+    alert("중복 없이 입력해 주세요");
+  } else {
+    return true;
   }
 }
