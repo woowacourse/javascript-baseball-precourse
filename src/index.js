3495fc884f5c0c543f3d2c870f192ed3062ee642
Taeyooooon <eric568@naver.com>
2023-10-06 12:17:17 +0900

    feat: 유저 입력 받기

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { generateComputerNumber } from './utils/generateComputerNumber.js';
 
+const onUserInputSubmit = (event) => {
+  event.preventDefault();
+  const userInput = document.querySelector('#user-input');
+  console.log('userInput : ', userInput);
+};
+
 export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
-    return '결과 값 String';
+    console.log('시작');
   }
 }
 
+const submitButton = document.querySelector('#submit');
+submitButton.addEventListener('click', onUserInputSubmit);
+
 const game = new BaseballGame();
+game.play();
